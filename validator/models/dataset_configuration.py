86a1a885cfd9f5b85bc1e07ec43b6dfe62a630f4
Zoltan Bakcsa <bakcsa@awst.at>
2019-03-27 15:48:20 +0100

    Updating related_names

--- validator/models/dataset_configuration.py
+++ validator/models/dataset_configuration.py
@@ -11,10 +11,10 @@ from validator.models.validation_run import ValidationRun
 class DatasetConfiguration(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     validation = models.ForeignKey(to=ValidationRun, on_delete=models.PROTECT, related_name='validation', null=False)
-    dataset = models.ForeignKey(to=Dataset, on_delete=models.PROTECT, related_name='data_dataset', null=False)
-    version = models.ForeignKey(to=DatasetVersion, on_delete=models.PROTECT, related_name='data_version', null=False)
-    variable = models.ForeignKey(to=DataVariable, on_delete=models.PROTECT, related_name='data_variable', null=False)
-    filters = models.ManyToManyField(DataFilter, related_name='data_filters')
+    dataset = models.ForeignKey(to=Dataset, on_delete=models.PROTECT, related_name='dataset', null=False)
+    version = models.ForeignKey(to=DatasetVersion, on_delete=models.PROTECT, related_name='version', null=False)
+    variable = models.ForeignKey(to=DataVariable, on_delete=models.PROTECT, related_name='variable', null=False)
+    filters = models.ManyToManyField(DataFilter, related_name='filters')
     reference = models.BooleanField()
     scaling_reference = models.BooleanField()
     
