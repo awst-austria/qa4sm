c4e6ffd7113561f94888ca28fb557289a9f2a21a
D. Baum <baum@awst.at>
2019-03-28 12:51:47 +0100

    Remove unused uuid field in CeleryTask

--- validator/models/celery_task.py
+++ validator/models/celery_task.py
@@ -1,12 +1,9 @@
-import uuid
-
 from django.db import models
 
 from validator.models.validation_run import ValidationRun
 
 
 class CeleryTask(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     validation = models.ForeignKey(to=ValidationRun, on_delete=models.PROTECT, related_name='validation', null=False)
     celery_task = models.UUIDField(null=False)
 
