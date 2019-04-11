5da19658d38473270bf45673077f1eb2f63fd9ed
D. Baum <baum@awst.at>
2019-04-11 19:52:40 +0200

    adapt validation run admin interface to use dataset configs

--- validator/admin/validation_run.py
+++ validator/admin/validation_run.py
@@ -1,13 +1,23 @@
-from django.contrib.admin import ModelAdmin
 from django.utils.html import format_html
 from django.urls.base import reverse
+from django.contrib.admin.options import ModelAdmin, TabularInline
+from validator.models.dataset_configuration import DatasetConfiguration
+
+# see https://docs.djangoproject.com/en/2.1/ref/contrib/admin/#inlinemodeladmin-objects
+class DatasetConfigurationInline(TabularInline):
+    model = DatasetConfiguration
+    fk_name = "validation"
+    extra = 0
 
 # for tutorial see https://djangobook.com/customizing-change-lists-forms/
 class ValidationRunAdmin(ModelAdmin):
     list_display = ('id', 'start_time', 'end_time', 'user', 'name_tag', 'open_button', )
+    readonly_fields = ('reference_configuration', 'scaling_ref',)
     search_fields = ('name_tag', )
     list_filter = ('start_time', 'end_time', 'user')
     ordering = ('-start_time', '-end_time',)
+
+    inlines = [DatasetConfigurationInline,]
 
     def __init__(self, model, admin_site):
         super(ValidationRunAdmin, self).__init__(model, admin_site)
