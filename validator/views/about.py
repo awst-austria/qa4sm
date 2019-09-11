1794c2db961ed71eaf5b4827802ba7b68347d315
D. Baum <baum@awst.at>
2019-09-11 17:40:59 +0200

    Remove debug print statement

--- validator/views/about.py
+++ validator/views/about.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from valentina.settings import APP_VERSION
 
 def about(request):
-    print(APP_VERSION)
     return render(request, 'validator/about.html',{'app_version' : APP_VERSION})
