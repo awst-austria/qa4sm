53592328122ae9f86d62fb4a718da97f4fced2cb
D. Baum <baum@awst.at>
2020-07-06 12:48:39 +0200

    Fix environment creation and settings (#25)
    * Unpin cftime version again
    * Move zenodo url to other config file

--- valentina/settings.py
+++ valentina/settings.py
@@ -28,8 +28,6 @@ SITE_URL = "https://qa4sm.eu"
 
 ENV_FILE_URL_TEMPLATE = "https://github.com/awst-austria/qa4sm/blob/v{}/environment/qa4sm_env.yml"
 
-DOI_REGISTRATION_URL="https://zenodo.org/api/deposit/depositions"
-DOI_URL_PREFIX="https://doi.org/"
 ORICD_REGEX = "^([0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9Xx]{3}[0-9Xx])$"
 
 # Application definition
