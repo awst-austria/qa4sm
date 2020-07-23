1a5af637938369bf668e6e384c0867135f5ec183
Monika Tercjak <monika.tercjak@gmail.com>
2020-07-23 10:16:47 +0200

    buttons added to the result view, some improvements of functions added

--- validator/static/js/some_functions.js
+++ validator/static/js/some_functions.js
@@ -1,18 +1,27 @@
 console.log("I'm here")
 
+function start_editing(obj){
+    var target = obj.currentTarget;
+    var target_parent = $(target).parent();
+    target_parent.find('button.edit_name_btn').toggleClass('d-none');
+    target_parent.find('button.save_name_btn').toggleClass('d-none');
+    target_parent.find('span.no_edit_name').toggleClass('d-none');
+    target_parent.find('input.edit_name').toggleClass('d-none');
+    return target_parent
+}
+
 function edit_name(obj){
-
-var target = obj.currentTarget;
-var target_parent = $(target).parent();
-target_parent.find('button.edit_name_btn').toggleClass('d-none')
-target_parent.find('button.save_name_btn').toggleClass('d-none')
-target_parent.find('span.no_edit_name').toggleClass('d-none')
-target_parent.find('input.edit_name').toggleClass('d-none')
-
+    var target_parent = start_editing(obj)
+    var old_name = target_parent.find('span.no_edit_name').html()
+    target_parent.find('input.edit_name').val(old_name)
+    console.log(old_name)
 }
 
 
-function monika(obj, result_id){
-edit_name(obj)
+function save_name(obj, result_id){
+    var target_parent = start_editing(obj)
+    var new_name = target_parent.find('input.edit_name').val()
+
+    console.log(new_name)
 
 }
