9591f9d0d36264fa0614c2de0802ff15fdb521c0
Monika Tercjak <monika.tercjak@gmail.com>
2020-08-03 16:53:26 +0200

    functions connected to edit, save and cancel button finished and saved in result_buttons.js file

--- validator/static/js/some_functions.js
+++ validator/static/js/some_functions.js
@@ -1,33 +1,31 @@
 console.log("I'm here")
 
-function start_editing(obj){
+
+
+function toggle_editing(obj){
     var target = obj.currentTarget;
     var target_parent = $(target).parent();
     target_parent.find('button.edit_name_btn').toggleClass('d-none');
     target_parent.find('button.save_name_btn').toggleClass('d-none');
-    target_parent.find('button.cancel_saving_btn').toggleClass('d-none');
+    target_parent.find('button.cancel_editing_btn').toggleClass('d-none');
     target_parent.find('span.no_edit_name').toggleClass('d-none');
     target_parent.find('input.edit_name').toggleClass('d-none');
     return target_parent
 }
 
 function edit_name(obj){
-    var target_parent = start_editing(obj)
+    var target_parent = toggle_editing(obj)
     var old_name = target_parent.find('span.no_edit_name').html()
     target_parent.find('input.edit_name').val(old_name)
-    console.log(old_name)
 }
 
 function cancel_editing(obj){
-    var target_parent = start_editing(obj)
-    var old_name = target_parent.find('span.no_edit_name').html()
-    target_parent.find('input.edit_name').val(old_name)
-    console.log(old_name)
+    toggle_editing(obj)
 }
 
 
 function ajax_save_name(obj, result_id){
-    var target_parent = start_editing(obj)
+    var target_parent = toggle_editing(obj)
     var new_name = target_parent.find('input.edit_name').val()
 
     var url =  result_url.replace('00000000-0000-0000-0000-000000000000', result_id);
@@ -37,7 +35,7 @@ function ajax_save_name(obj, result_id){
     $.ajaxSetup({
         headers : { "X-CSRFToken" : csrf_token }
     });
-//
+
     $.ajax({
         url: url,
         type: 'PATCH',
