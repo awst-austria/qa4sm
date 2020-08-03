0a89ba0d6df4abbf512d920df90fba06245ed43b
Monika Tercjak <monika.tercjak@gmail.com>
2020-08-03 11:34:35 +0200

    save_name function changed name to ajax_save_name, cancel_saving function added, cancel button added to user_runs.html

--- validator/static/js/some_functions.js
+++ validator/static/js/some_functions.js
@@ -5,6 +5,7 @@ function start_editing(obj){
     var target_parent = $(target).parent();
     target_parent.find('button.edit_name_btn').toggleClass('d-none');
     target_parent.find('button.save_name_btn').toggleClass('d-none');
+    target_parent.find('button.cancel_saving_btn').toggleClass('d-none');
     target_parent.find('span.no_edit_name').toggleClass('d-none');
     target_parent.find('input.edit_name').toggleClass('d-none');
     return target_parent
@@ -17,8 +18,15 @@ function edit_name(obj){
     console.log(old_name)
 }
 
+function cancel_editing(obj){
+    var target_parent = start_editing(obj)
+    var old_name = target_parent.find('span.no_edit_name').html()
+    target_parent.find('input.edit_name').val(old_name)
+    console.log(old_name)
+}
 
-function save_name(obj, result_id){
+
+function ajax_save_name(obj, result_id){
     var target_parent = start_editing(obj)
     var new_name = target_parent.find('input.edit_name').val()
 
