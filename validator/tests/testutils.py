411f543ccadfab51530533f85d387bb1944861bf
Zoltan Bakcsa <zoltan@bakcsa.hu>
2020-08-14 13:11:13 +0200

    test-commit

--- validator/tests/testutils.py
+++ validator/tests/testutils.py
@@ -1,6 +1,7 @@
 from validator.models import Dataset
 from os import path
 
+
 def set_dataset_paths():
     for dataset in Dataset.objects.all():
         dataset.storage_path = path.join('testdata/input_data', dataset.short_name)
