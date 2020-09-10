4e59f89d49188224091bf2d405b8e5fe8a80c00d
Monika Tercjak <monika.tercjak@gmail.com>
2020-09-10 14:11:48 +0200

    number of station in particular network added to info that is passed to the website

--- validator/views/validation.py
+++ validator/views/validation.py
@@ -206,7 +206,7 @@ def ajax_get_version_id(request):
         version = DatasetVersion.objects.get(pk=int(version_id))
         networks = version.network_version.all()
         continents = networks.distinct('continent').values_list('continent', flat=True)
-        network_dict =  {continent: [(network.name, network.country) for network in networks.filter(continent=continent)] for continent in continents}
+        network_dict =  {continent: [(network.name, network.country, network.number_of_stations) for network in networks.filter(continent=continent)] for continent in continents}
     except:
         return HttpResponseBadRequest("Not a valid dataset version")
 
