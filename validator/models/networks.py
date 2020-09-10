c3dca1239ec4df50c23ea749ff039dbf7bf17b50
Monika Tercjak <monika.tercjak@gmail.com>
2020-09-10 12:59:26 +0200

    related name changed in the networks model, some small improvements according to continents name introduced in the fixture

--- validator/models/networks.py
+++ validator/models/networks.py
@@ -7,7 +7,7 @@ class ISMNNetworks(models.Model):
     continent = models.CharField(max_length=24)
     country = models.CharField(max_length=60)
     number_of_stations = models.IntegerField()
-    versions = models.ManyToManyField(DatasetVersion, related_name='dataset_version')
+    versions = models.ManyToManyField(DatasetVersion, related_name='network_version')
 
     def __str__(self):
         return self.name
