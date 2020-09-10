15d6b17cf7b066859a36ba00853f722c85d781d1
Monika Tercjak <monika.tercjak@gmail.com>
2020-09-10 13:13:23 +0200

    function for displaying network info depending on version id added together with some auxiliary functions

--- validator/static/js/test.js
+++ validator/static/js/test.js
@@ -1,121 +1,132 @@
 console.log("I'm here")
 
-function removeTab(tabContentClass, tabLinkClass, formsetPrefix, addButton, removeButton){
-// taking tabs (referred as tabLinkClass in validate.html) and containers (referred as tabContentClass)
-// and choosing the ones to be removed (the active ones)
-    var tabs = $(tabLinkClass)
-    var containers = $(tabContentClass)
-
-    // removal button is on the right-hand side of the tab to be remove, therefore I'm using prev()
-    var tab_to_remove = $(removeButton.prev())
-    var container_to_remove = $('#'+tab_to_remove.attr('aria-controls'))
-
-// taking totalNoForms so that plus sign can be displayed again
-    var totalSelector = '#id_' + formsetPrefix + '-TOTAL_FORMS';
-    var totalNoForms = $(totalSelector).val();
-    var maxSelector = '#id_' + formsetPrefix + '-MAX_NUM_FORMS';
-
-// this condition enables removing datasets unless there is only one left
-// additionally it's checked if there is the same number of tabs and containers to prevent unexpected errors
-    if (tabs.length>1 && containers.length>1 && tabs.length == containers.length){
-
-    // taking next tabs and containers, to change their id's
-        var all_next_tabs = tab_to_remove.nextAll(tabLinkClass)
-        var all_next_containers = container_to_remove.nextAll(tabContentClass)
-
-        console.log('All next tabs: ',all_next_tabs)
-        console.log('All next containers: ', all_next_containers)
-
-    //taking next and previous tabs and containers, to make them active after removal current items
-    // since the remove button is placed after each tab then removeButton.next() should be taken
-        var next_tab = removeButton.next()
-        var prev_tab = tab_to_remove.prev()
-
-        var next_container = container_to_remove.next()
-        var prev_container = container_to_remove.prev()
-
-    //removing current tab and container
-        tab_to_remove.remove()
-        container_to_remove.remove()
-
-    // showing add button again
-        totalNoForms--
-         $(totalSelector).val(totalNoForms);
-         if (totalNoForms < $(maxSelector).val()) {
-                $(removeButton).css('margin-left', '0.3rem')
-                addButton.show();
-            }
-         if (totalNoForms == 1) {
-                removeButton.hide();
-            }
-
-        // changing attributes of following items
-        for (var i=0; i < all_next_tabs.length; i++){
-
-            var current_tab = $(all_next_tabs[i])
-            var current_container = $(all_next_containers[i])
-            console.log('current container', current_container)
-
-            var old_id = current_container.attr('id')
-
-            var new_num = parseInt(old_id.slice(-1)) - 1
-            var new_id = old_id.slice(0,-1) + new_num
-            var new_tab_id = new_id + '-tab'
-
-            // setting new values
-            current_tab.attr('id', new_tab_id)
-            current_tab.attr('href', '#' + new_id)
-            current_tab.attr('aria-controls', new_id)
-
-            current_container.attr('id', new_id)
-            current_container.attr('aria-labelledby', new_tab_id)
-
-            current_container.find(':input').each(function() {
-                var id = $(this).attr('id').replace(old_id, new_id);
-                var name = id.replace(/^id_/, '')
-                $(this).attr({'name': name, 'id': id});
-            });
-
-            current_container.find('label').each(function() {
-                var newFor = $(this).attr('for').replace(old_id, new_id);
-                $(this).attr('for', newFor);
-            });
-
-            current_container.find('.tabSpecific').each(function() {
-                var id = $(this).attr('id').replace(old_id, new_id);
-                $(this).attr('id', id);
-            });
-
-           $('#' + new_id + '-dataset').change()
-        }
-
-// checking if there is a tab and a container after the ones removed and set them to be active;
-// if the last one is being removed, the previous ones are set to be the active
-// tabLinkClass and tabContentClass are sliced to remove the dot at the beginning
-
-        if (next_tab.hasClass(tabLinkClass.slice(1, ))){
-            next_tab.toggleClass('active')
-            $(next_tab).after(removeButton)
-        } else {
-            prev_tab.toggleClass('active')
-//            prev_tab.after(removeButton)
-        }
-
-        if (next_container.hasClass(tabContentClass.slice(1, ))){
-            next_container.toggleClass('active show')
-        } else {
-            prev_container.toggleClass('active show')
-        }
-    }
+function toggle_children_class(element, class_name){
+    $(element).toggleClass(class_name)
+    $.each($(element).find("*"), function(id, child){
+        $(child).toggleClass(class_name)
+    })
 }
 
-$('#remove_dc_form').click(function() {
-    removeTab('.dc_form', '.dc_form_link', 'datasets',  $('#add_dc_form'), $(this));
-});
 
-function shiftTheButton(obj, buttonSelector){
-    var currentObject = obj.currentTarget
-    var removeButton = $(buttonSelector)
-    $(currentObject).after(removeButton)
+function verify_html_element(element_class){
+        var selector = '.'+element_class
+        if  ($(selector).length>1){
+            $.each($(selector), function(ind, element){
+                if (!$(element).hasClass('d-none')){
+                    $(element).empty()
+                    $(element).remove()
+                } else {
+                    toggle_children_class(element, 'd-none')
+                }
+            })
+        }
+
+
 }
 
+
+//            if ($(".continent_container").length>1){
+//                $.each($(".continent_container"), function(ind, element){
+//                    if (!$(element).hasClass('d-none')){
+//                        $(element).empty()
+//                        $(element).remove()
+//                    } else {
+//                        $(element).toggleClass('d-none')
+//                        $.each($(element).children(), function(id, child){
+//                            $(child).toggleClass('d-none')
+//                            $.each($(child).children(), function(id, grandchild){
+//                                $(grandchild).toggleClass('d-none')
+//                            })
+//                        })
+//                    }
+//
+//                })
+
+
+
+function get_version_id(){
+       var versionId = $(this).val();
+       $('#id_target_hidden_input_version').val(versionId)
+       console.log(versionId)
+}
+//
+//        function ajax_get_default_version_id() {
+//            var url = $("#validation_form").attr("data-options-url");
+//            var datasetId = $(this).val();
+//            var widgetId = $(this).attr('id');
+//            var filterWidgetId = widgetId.replace(/dataset$/, "filters");
+//            var paramFilterWidgetId = widgetId.replace(/dataset$/, "parametrised_filters");
+//            $.ajax({
+//                url: url,
+//                data: { 'dataset_id': datasetId, 'filter_widget_id': filterWidgetId, 'param_filter_widget_id': paramFilterWidgetId },
+//                success: function (return_data) {
+//                    var dataset_widget = "#" + widgetId;
+//                    var version_widget = dataset_widget.replace(/dataset$/, "version");
+//                    console.log(return_data['versions'])
+////                    var versions_id =
+////                    $(version_widget).(return_data['versions']);
+//
+//                }
+//            });
+//        }
+
+
+
+
+
+//for (var i=0; i<versions.length; i++){
+//    console.log(versions[i])
+//}
+
+
+
+//function get_version_id(){
+//       var datasetName = $(this).find('option:selected').text();
+//       var versionId = '#' + $(this).attr('id').replace(/dataset$/, 'version');
+//       console.log($(versionId).val());
+
+//    var versionId = $(this).val();
+//    console.log(versionId)
+//    var url = $("#validation_form").attr("data-options-url");
+//            $.ajax({
+//                url: url,
+//                success: function (return_data) {
+//                    var dataset_widget = "#" + widgetId;
+//                    var version_widget = dataset_widget.replace(/dataset$/, "version");
+//                    var variable_widget = dataset_widget.replace(/dataset$/, "variable");
+//                    var filter_widget = "#" + filterWidgetId;
+//                    var param_filter_widget = "#" + paramFilterWidgetId;
+//                    $(version_widget).html(return_data['versions']);
+//                    $(variable_widget).html(return_data['variables']);
+//                    $(filter_widget).html(return_data['filters']);
+//                    $(param_filter_widget).html(return_data['paramfilters']);
+//                }
+//            });
+//        }
+//
+
+
+//}
+
+//       function ajax_change_dataset() {
+//            var url = $("#validation_form").attr("data-options-url");
+//            var datasetId = $(this).val();
+//            var widgetId = $(this).attr('id');
+//            var filterWidgetId = widgetId.replace(/dataset$/, "filters");
+//            var paramFilterWidgetId = widgetId.replace(/dataset$/, "parametrised_filters");
+//            $.ajax({
+//                url: url,
+//                data: { 'dataset_id': datasetId, 'filter_widget_id': filterWidgetId, 'param_filter_widget_id': paramFilterWidgetId },
+//                success: function (return_data) {
+//                    var dataset_widget = "#" + widgetId;
+//                    var version_widget = dataset_widget.replace(/dataset$/, "version");
+//                    var variable_widget = dataset_widget.replace(/dataset$/, "variable");
+//                    var filter_widget = "#" + filterWidgetId;
+//                    var param_filter_widget = "#" + paramFilterWidgetId;
+//                    $(version_widget).html(return_data['versions']);
+//                    $(variable_widget).html(return_data['variables']);
+//                    $(filter_widget).html(return_data['filters']);
+//                    $(param_filter_widget).html(return_data['paramfilters']);
+//                }
+//            });
+//        }
