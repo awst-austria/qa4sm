bae8bf3246da4230e666be45827864f94868c902
sheenaze <tercjak@awst>
2020-10-29 18:02:08 +0100

    plot class added to css file; number of validations in time added to the statistics

--- validator/admin/qa4sm_statistics.py
+++ validator/admin/qa4sm_statistics.py
@@ -28,22 +28,12 @@ class StatisticsAdmin(ModelAdmin):
         return new_urls + urls
 
     @staticmethod
-    def active_users_number():
-        users = User.objects.filter(is_active=True)
-        return users.count()
-
-    @staticmethod
-    def number_of_run_validations():
-        validations = ValidationRun.objects.all()
-        return validations.count()
-
-    @staticmethod
     def sorted_users_validation_query():
         return ValidationRun.objects.values('user').annotate(count=Count('user')).order_by('-count')
     
     @staticmethod
-    def users_dict():
-        users = User.objects.filter(is_active=True)
+    def users_info_for_plot():
+        users = User.objects.filter(is_active=True).order_by('pk')
         users_names = [user.username for user in users]
         validations_num = [user.validationrun_set.all().count() for user in users]
         users_dict = {'users': users_names,
@@ -56,19 +46,39 @@ class StatisticsAdmin(ModelAdmin):
         validations_num = self.sorted_users_validation_query().first()['count']
         return {'name': user_name, 'validation_num': validations_num}
 
+    @staticmethod
+    def validation_info_for_plot():
+        validations_sorted_by_time = ValidationRun.objects.order_by('start_time')
+        validations_time = list(validations_sorted_by_time.values_list('start_time', flat=True))
+        # here the date is converted to the format 'YYYY-MM-DD HH:MM' needed to plotply histogram
+        time_list = []
+        for time in validations_time:
+            # if the minute part is a single digit then 0 has to be added, otherwise it's not converted correctly
+            minute = time.minute if time.minute > 9 else f'0{time.minute}'
+            time_new_format = f'{time.year}-{time.month}-{time.day} {time.hour}:{minute}'
+            time_list.append(time_new_format)
+
+        first = validations_sorted_by_time.values_list('start_time', flat=True).first()
+        first_time = [f'{first.year}-{first.month}-{first.day} 0:00:00']
+        last = validations_sorted_by_time.values_list('start_time', flat=True).last()
+        last_time = [f'{last.year}-{last.month}-{last.day}']
+
+        validation_dict = {'validations': validations_sorted_by_time,
+                           'validations_time': time_list,
+                           'first_validation': first_time,
+                           'last_validation': last_time}
+        return validation_dict
+
     # @csrf_protect_m
     def statistics(self, request):
         if not request.user.is_superuser:
             raise PermissionDenied
 
         if request.method == "GET":
-            users = User.objects.filter(is_active=True)
-            sorted_users = self.sorted_users_validation_query()
-            stats = {'users': users,
-                     'number_of_users': self.active_users_number(),
-                     'number_of_validations': self.number_of_run_validations(),
+            stats = {'number_of_users':  User.objects.filter(is_active=True).count(),
+                     'number_of_validations':  ValidationRun.objects.all().count(),
                      'most_frequent_user': self.most_frequent_user_info(),
-                     'sorted_users': sorted_users,
-                     'val_num_by_user_data': self.users_dict()}
+                     'val_num_by_user_data': self.users_info_for_plot(),
+                     'validations_for_plot': self.validation_info_for_plot()}
 
             return render(request, 'admin/qa4sm_statistics.html', {'stats': stats})
