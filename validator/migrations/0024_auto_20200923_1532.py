8715f59e4ba5b149c7b6eb92dc3938d013104cd4
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2020-11-11 14:04:18 +0100

    Update and rename 0022_auto_20200923_1532.py to 0024_auto_20200923_1532.py
    file name changed as well as the name of the file in the dependencies so to avoid conflicts while merging with other branches

--- validator/migrations/0024_auto_20200923_1532.py
+++ validator/migrations/0024_auto_20200923_1532.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('validator', '0021_ismnnetworks'),
+        ('validator', '0023_ismnnetworks'),
     ]
 
     operations = [
