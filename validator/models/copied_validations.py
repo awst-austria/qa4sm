269533546a2f87a03e5c721e7b4cb6e1ef6da176
sheenaze <tercjak@awst>
2021-01-14 13:08:47 +0100

    field used_by_user changed so that the CopiedValidation record is removed when the used_by_user user is removed

--- validator/models/copied_validations.py
+++ validator/models/copied_validations.py
@@ -6,7 +6,7 @@ from validator.models.validation_run import ValidationRun
 class CopiedValidations(models.Model):
 
     copied_run = models.ForeignKey(ValidationRun, on_delete=models.CASCADE, related_name='copied_run', null=True)
-    used_by_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    used_by_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     original_run = models.ForeignKey(ValidationRun, on_delete=models.SET_NULL, null=True, related_name='original_run')
 
     def __str__(self):
