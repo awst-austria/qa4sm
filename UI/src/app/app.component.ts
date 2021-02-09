ae0545dad965e60fc8c5e63eea2961eee08e4ad0
bakcsa <zoltan@bakcsa.hu>
2021-02-09 16:52:51 +0100

    fixing routes

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -17,8 +17,8 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.items = [
-      {label: 'Home', icon: 'pi pi-fw pi-home', routerLink: ['/']},
-      {label: 'Validate', icon: 'pi pi-fw pi-check-square', routerLink: ['/validate']},
+      {label: 'Home', icon: 'pi pi-fw pi-home', routerLink: ['']},
+      {label: 'Validate', icon: 'pi pi-fw pi-check-square', routerLink: ['validate']},
       {label: 'My validations', icon: 'pi pi-fw pi-folder'},
       {label: 'Published validations', icon: 'pi pi-fw pi-globe'},
       {label: 'Profile', icon: 'pi pi-fw pi-user'}
