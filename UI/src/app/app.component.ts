359345b72e4b3d11060231141e0fa8f0df4d57b6
bakcsa <zoltan@bakcsa.hu>
2021-02-09 19:15:16 +0100

    extend menu

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {MenuItem} from 'primeng/api';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'qa4sm-ui';
   items: MenuItem[];
 
@@ -21,7 +21,17 @@ export class AppComponent implements OnInit{
       {label: 'Validate', icon: 'pi pi-fw pi-check-square', routerLink: ['validate']},
       {label: 'My validations', icon: 'pi pi-fw pi-folder'},
       {label: 'Published validations', icon: 'pi pi-fw pi-globe'},
-      {label: 'Profile', icon: 'pi pi-fw pi-user'}
+      {label: 'Info', icon: 'pi pi-fw pi-info-circle', routerLink: ['validate'], items: [
+          {label: 'About', icon: 'pi pi-fw pi-info'},
+          {label: 'Help', icon: 'pi pi-fw pi-question'},
+          {label: 'Datasets', icon: 'pi pi-fw pi-save'},
+          {label: 'Terms', icon: 'pi pi-fw pi-briefcase'},
+        ]},
+      {label: 'Profile', icon: 'pi pi-fw pi-user',items:[
+          {label: 'My account', icon: 'pi pi-fw pi-user'},
+          {label: 'Logout', icon: 'pi pi-fw pi-sign-out'},
+          {label: 'Login', icon: 'pi pi-fw pi-sign-in'},
+        ]}
     ];
   }
 }
