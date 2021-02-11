d1250b92916f6d05e7048ba7694f07c8e218dada
bakcsa <zoltan@bakcsa.hu>
2021-02-11 20:07:23 +0100

    worked on build script

--- UI/scripts/build_run.sh
+++ UI/scripts/build_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+npm install
 ng build --prod --deploy-url /ui/ --base-href /ui/
 cp ../dist/UI ./ui
 docker stop qa4sm_ui
 docker rm qa4sm_ui --force
 docker image rm qa4sm_ui
 docker build -t qa4sm_ui
+docker run --name qa4sm_ui qa4sm_ui -p 8013:80 -d
