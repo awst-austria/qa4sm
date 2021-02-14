b53dc94b68b20d87635216ff35f2e0cb83e15ef0
bakcsa <zoltan@bakcsa.hu>
2021-02-14 15:09:27 +0100

    fine-tuning custom_exception_handler

--- api/api_exception_handler.py
+++ api/api_exception_handler.py
@@ -1,10 +1,28 @@
 import logging
 
 from django.http import JsonResponse
+from rest_framework.views import exception_handler
 
 
 def custom_exception_handler(exc, context):
+    """
+    Exception handler to catch unhandled exceptions.
+    Default behavior is to send back the whole stack trace to the rest client which is a security issue so we need to
+    override it.
+
+    Parameters
+    ----------
+    exc
+    context
+
+    Returns
+    -------
+    response
+    """
     __logger = logging.getLogger(__name__)
-    # response = exception_handler(exc, context)
-    __logger.error('Unhandled exception: ', exc)
-    return JsonResponse({'message': 'Unexpected error'}, status=500)
+    response = exception_handler(exc, context)
+    if response is None:
+        __logger.error('Unhandled exception: ', exc)
+        return JsonResponse({'message': 'Unexpected error'}, status=500)
+
+    return response
