4b33ee6597f57e47be76e1afc9dede7b616da740
bakcsa <zoltan@bakcsa.hu>
2021-02-14 15:08:30 +0100

    cleaning up URLs

--- api/urls.py
+++ api/urls.py
@@ -1,40 +1,36 @@
 from django.conf.urls import url
 from django.urls import path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
 
 from api.endpoints.LoginView import api_login
 from api.endpoints.PathVarTestEndpoint import path_var_get
-from api.endpoints.UsersView import users_get
+from api.endpoints.UsersView import users
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Snippets API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@snippets.local"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 urlpatterns = [
-    # path('swagger-ui/', TemplateView.as_view(template_name='swagger.html',
-    #                                          extra_context={'schema_url': 'openapi-schema'}), name='swagger-ui'),
-    # path('openapi', get_schema_view(
-    #     title="Your Project",
-    #     description="API for all things …",
-    #     version="1.0.0"
-    # ), name='openapi-schema'),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    url(r'^test/$', users_get),
-    url(r'^path_test/(?P<username>.+)/$', path_var_get),
+    # url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # IMPORTANT
+    # URLs should not end with a slash. Ig you add a new endpoint, do not add a trailing slash:
+    #   GOOD:  path('my-new-endpoint', my_new_view, name='fancy stuff'),
+    #   WRONG: path('my-new-endpoint/', my_new_view, name='fancy stuff'),
+    #
+    url(r'^test$', users),
+    url(r'^path_test/(?P<username>.+)$', path_var_get),
     path('auth', api_login, name='login'),
 
 
