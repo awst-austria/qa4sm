975d184a1744f196ca0409c49d68bd048802086a
bakcsa <zoltan@bakcsa.hu>
2021-02-27 17:07:20 +0100

    add validation period module

---
+++ UI/src/app/modules/validation-period/components/validation-period/validation-period.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qa-validation-period',
+  templateUrl: './validation-period.component.html',
+  styleUrls: ['./validation-period.component.scss']
+})
+export class ValidationPeriodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ UI/src/app/modules/validation-period/validation-period.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ValidationPeriodComponent } from './components/validation-period/validation-period.component';
+
+
+
+@NgModule({
+  declarations: [ValidationPeriodComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ValidationPeriodModule { }
