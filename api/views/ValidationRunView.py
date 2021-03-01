f032c1e32d3631c3e374aba1878b00ab73fc351a
bakcsa <zoltan@bakcsa.hu>
2021-03-01 22:01:43 +0100

    setting correct permission class on  published_results view

--- api/views/ValidationRunView.py
+++ api/views/ValidationRunView.py
@@ -1,15 +1,16 @@
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.serializers import ModelSerializer
-from validator.models import ValidationRun
+
 from api.views.auxiliary_functions import get_fields_as_list
+from validator.models import ValidationRun
 
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def published_results(request):
-
     val_runs = ValidationRun.objects.exclude(doi='')
     serializer = ResultsSerializer(val_runs, many=True)
     return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
@@ -22,8 +23,8 @@ def my_results(request):
     serializer = ResultsSerializer(val_runs, many=True)
     return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
+
 class ResultsSerializer(ModelSerializer):
     class Meta:
         model = ValidationRun
         fields = get_fields_as_list(ValidationRun)
-
