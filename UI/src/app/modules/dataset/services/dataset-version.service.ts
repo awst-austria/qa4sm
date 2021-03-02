9efd8aa6ea005b632bd2be0bcaef4402bbd328d2
sheenaze <tercjak@awst>
2021-03-02 16:20:28 +0100

    method for taking dataset version by its id added

--- UI/src/app/modules/dataset/services/dataset-version.service.ts
+++ UI/src/app/modules/dataset/services/dataset-version.service.ts
@@ -8,6 +8,7 @@ import {shareReplay} from 'rxjs/operators';
 const datasetVersionUrl: string = environment.API_URL + 'api/dataset-version';
 const CACHE_LIFETIME: number = 5 * 60 * 1000; // 5 minutes
 const CACHE_KEY_ALL_VERSIONS = -1;
+const CACHE_KEY_VERSION = -1;
 
 export class DatasetVersionCacheItem {
   constructor(public lastFetched: Date, public datasetVersions$: Observable<DatasetVersionDto[]>) {
@@ -19,12 +20,16 @@ export class DatasetVersionCacheItem {
 })
 export class DatasetVersionService {
 
-  requestCache = new Map<number, DatasetVersionCacheItem>();
+  requestCacheAll = new Map<number, DatasetVersionCacheItem>();
+  requestCacheOne = new Map<number, DatasetVersionCacheItem>();
 
 
   constructor(private httpClient: HttpClient) {
-    this.requestCache.set(
+    this.requestCacheAll.set(
       CACHE_KEY_ALL_VERSIONS,
+      new DatasetVersionCacheItem(new Date(), this.httpClient.get<DatasetVersionDto[]>(datasetVersionUrl).pipe(shareReplay())));
+    this.requestCacheOne.set(
+      CACHE_KEY_VERSION,
       new DatasetVersionCacheItem(new Date(), this.httpClient.get<DatasetVersionDto[]>(datasetVersionUrl).pipe(shareReplay())));
   }
 
@@ -34,7 +39,7 @@ export class DatasetVersionService {
 
   getVersionsByDataset(datasetId: number): Observable<DatasetVersionDto[]> {
     if (this.isCached(datasetId)) {
-      return this.requestCache.get(datasetId).datasetVersions$;
+      return this.requestCacheAll.get(datasetId).datasetVersions$;
     }
 
     let request;
@@ -46,9 +51,25 @@ export class DatasetVersionService {
     }
 
     let cacheItem = new DatasetVersionCacheItem(new Date(), request);
-    this.requestCache.set(datasetId, cacheItem);
+    this.requestCacheAll.set(datasetId, cacheItem);
 
     return request;
+  }
+
+  getVersionById(versionId: number): Observable<DatasetVersionDto>{
+    let request;
+    if (versionId === CACHE_KEY_VERSION) {
+      request = this.httpClient.get<DatasetVersionDto>(datasetVersionUrl).pipe(shareReplay());
+    } else {
+      let params = new HttpParams().set('version_id', String(versionId));
+      request = this.httpClient.get<DatasetVersionDto>(datasetVersionUrl, {params: params}).pipe(shareReplay());
+    }
+
+    let cacheItem = new DatasetVersionCacheItem(new Date(), request);
+    this.requestCacheOne.set(versionId, cacheItem);
+
+    return request;
+
   }
 
   /**
@@ -57,8 +78,8 @@ export class DatasetVersionService {
    * @private
    */
   private isCached(datasetId: number): boolean {
-    if (this.requestCache.has(datasetId)) {
-      let cacheItem = this.requestCache.get(datasetId);
+    if (this.requestCacheAll.has(datasetId)) {
+      let cacheItem = this.requestCacheAll.get(datasetId);
       if (((new Date()).getTime() - cacheItem.lastFetched.getTime()) < CACHE_LIFETIME) {
         return true;
       }
