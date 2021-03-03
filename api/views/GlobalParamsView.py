20f752f460c8e149a77a1e1f85108abe4d512d22
sheenaze <tercjak@awst>
2021-03-03 14:24:22 +0100

    unnecessary files removed

--- api/views/GlobalParamsView.py
+++
@@ -1,8 +0,0 @@
-from validator.context_processors import globals_processor
-from django.http import JsonResponse
-from rest_framework import status
-
-
-def global_params(request):
-    data = globals_processor(request)
-    return JsonResponse(data, status=status.HTTP_200_OK)
