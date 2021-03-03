dd8924e4f9b315a0562cb38e6f84abeaa219b5d6
sheenaze <tercjak@awst>
2021-03-03 15:52:44 +0100

    preriliminary code added to validations component so that there is no empty page

--- UI/src/app/pages/validations/validations.component.ts
+++ UI/src/app/pages/validations/validations.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {ValidationrunDto} from '../../modules/validation-result/services/validationrun.dto';
+import {DatasetDto} from '../../modules/dataset/services/dataset.dto';
+import {DatasetVersionDto} from '../../modules/dataset/services/dataset-version.dto';
+import {DatasetVariableDto} from '../../modules/dataset/services/dataset-variable.dto';
+import {ValidationrunService} from '../../modules/validation-result/services/validationrun.service';
 
 @Component({
   selector: 'app-validations',
@@ -6,10 +12,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./validations.component.scss']
 })
 export class ValidationsComponent implements OnInit {
+  myValidation$: Observable<ValidationrunDto[]>;
+  datasets?: DatasetDto[] = [];
+  versions?: DatasetVersionDto[] = [];
+  variables?: DatasetVariableDto[] = [];
 
-  constructor() { }
+  constructor(private validationrunService: ValidationrunService) { }
 
   ngOnInit(): void {
+    this.myValidation$ = this.validationrunService.getMyValidationruns();
+    // this.getDatasetsInfo();
   }
 
+  getDatasetsInfo(){
+    this.validationrunService.getAllDatasets(this.datasets);
+    this.validationrunService.getAllVersions(this.versions);
+    this.validationrunService.getAllVariables(this.variables);
+  }
 }
