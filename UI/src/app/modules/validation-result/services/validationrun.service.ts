709eeb656a7833ab5704c9f1e2c68495b0794892
sheenaze <tercjak@awst>
2021-03-03 14:36:56 +0100

    Methods for retrievieng dataset info moved to validationrun.service.ts

--- UI/src/app/modules/validation-result/services/validationrun.service.ts
+++ UI/src/app/modules/validation-result/services/validationrun.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ValidationrunDto} from './validationrun.dto';
 import {Observable} from 'rxjs';
 import {environment} from '../../../../environments/environment';
+import {DatasetDto} from '../../dataset/services/dataset.dto';
+import {DatasetVersionDto} from '../../dataset/services/dataset-version.dto';
+import {DatasetVariableDto} from '../../dataset/services/dataset-variable.dto';
+import {DatasetService} from '../../dataset/services/dataset.service';
+import {DatasetVersionService} from '../../dataset/services/dataset-version.service';
+import {DatasetVariableService} from '../../dataset/services/dataset-variable.service';
 
 const publishedValidationRunUrl: string = environment.API_URL + 'api/published-results';
 const customValidationRunUrl: string = environment.API_URL + 'api/my-results';
@@ -15,7 +21,10 @@ export class ValidationrunService {
   customValidationrun$: Observable<ValidationrunDto[]>;
   publishedValidationrun$: Observable<ValidationrunDto[]>;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient,
+              private datasetService: DatasetService,
+              private datasetVersionService: DatasetVersionService,
+              private datasetVariableService: DatasetVariableService) {
     this.publishedValidationrun$ = this.httpClient.get<ValidationrunDto[]>(publishedValidationRunUrl);
   }
 
@@ -27,6 +36,30 @@ export class ValidationrunService {
   getMyValidationruns(): Observable<ValidationrunDto[]>{
     this.customValidationrun$ = this.httpClient.get<ValidationrunDto[]>(customValidationRunUrl);
     return  this.customValidationrun$;
+  }
+
+  getAllDatasets(targetArray: DatasetDto[]) {
+    this.datasetService.getAllDatasets().subscribe(datasets => {
+      datasets.forEach(dataset => {
+        targetArray.push(dataset);
+      });
+    });
+  }
+
+  getAllVersions(targetArray: DatasetVersionDto[]) {
+    this.datasetVersionService.getAllVersions().subscribe(versions => {
+      versions.forEach(version => {
+        targetArray.push(version);
+      });
+    });
+  }
+
+  getAllVariables(targetArray: DatasetVariableDto[]) {
+    this.datasetVariableService.getAllVariables().subscribe(variables => {
+      variables.forEach(variable => {
+        targetArray.push(variable);
+      });
+    });
   }
 
 }
--- UI/src/app/pages/published-validations/published-validations.component.ts
+++ UI/src/app/pages/published-validations/published-validations.component.ts
@@ -3,11 +3,8 @@ import {Observable} from 'rxjs';
 import {ValidationrunDto} from '../../modules/validation-result/services/validationrun.dto';
 import {ValidationrunService} from '../../modules/validation-result/services/validationrun.service';
 import {DatasetDto} from '../../modules/dataset/services/dataset.dto';
-import {DatasetService} from '../../modules/dataset/services/dataset.service';
 import {DatasetVersionDto} from '../../modules/dataset/services/dataset-version.dto';
-import {DatasetVersionService} from '../../modules/dataset/services/dataset-version.service';
 import {DatasetVariableDto} from '../../modules/dataset/services/dataset-variable.dto';
-import {DatasetVariableService} from '../../modules/dataset/services/dataset-variable.service';
 
 @Component({
   selector: 'qa-published-validations',
@@ -20,11 +17,7 @@ export class PublishedValidationsComponent implements OnInit {
   versions?: DatasetVersionDto[] = [];
   variables?: DatasetVariableDto[] = [];
 
-  constructor(private validationrunService: ValidationrunService,
-              private datasetService: DatasetService,
-              private datasetVersionService: DatasetVersionService,
-              private datasetVariableService: DatasetVariableService) {
-    // this.getAllDatasets();
+  constructor(private validationrunService: ValidationrunService) {
   }
 
   ngOnInit(): void {
@@ -34,33 +27,8 @@ export class PublishedValidationsComponent implements OnInit {
   }
 
   getDatasetsInfo(){
-    this.getAllDatasets(this.datasets);
-    this.getAllVersions(this.versions);
-    this.getAllVariables(this.variables);
+    this.validationrunService.getAllDatasets(this.datasets);
+    this.validationrunService.getAllVersions(this.versions);
+    this.validationrunService.getAllVariables(this.variables);
   }
-
-  private getAllDatasets(targetArray: DatasetDto[]) {
-    this.datasetService.getAllDatasets().subscribe(datasets => {
-      datasets.forEach(dataset => {
-        targetArray.push(dataset);
-      });
-    });
-  }
-
-  private getAllVersions(targetArray: DatasetVersionDto[]) {
-    this.datasetVersionService.getAllVersions().subscribe(versions => {
-      versions.forEach(version => {
-        targetArray.push(version);
-      });
-    });
-  }
-
-  private getAllVariables(targetArray: DatasetVariableDto[]) {
-    this.datasetVariableService.getAllVariables().subscribe(variables => {
-      variables.forEach(variable => {
-        targetArray.push(variable);
-      });
-    });
-  }
-
 }
