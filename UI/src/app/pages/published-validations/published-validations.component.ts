245a98d8116b3628effacafe47008700a24595d1
sheenaze <tercjak@awst>
2021-03-03 12:19:00 +0100

    All datasets, versions and variables taken and passed to validation-row component

--- UI/src/app/pages/published-validations/published-validations.component.ts
+++ UI/src/app/pages/published-validations/published-validations.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {ValidationrunDto} from '../../modules/validation-result/services/validationrun.dto';
 import {ValidationrunService} from '../../modules/validation-result/services/validationrun.service';
+import {DatasetDto} from '../../modules/dataset/services/dataset.dto';
+import {DatasetService} from '../../modules/dataset/services/dataset.service';
+import {DatasetVersionDto} from '../../modules/dataset/services/dataset-version.dto';
+import {DatasetVersionService} from '../../modules/dataset/services/dataset-version.service';
+import {DatasetVariableDto} from '../../modules/dataset/services/dataset-variable.dto';
+import {DatasetVariableService} from '../../modules/dataset/services/dataset-variable.service';
 
 @Component({
   selector: 'qa-published-validations',
@@ -10,11 +16,51 @@ import {ValidationrunService} from '../../modules/validation-result/services/val
 })
 export class PublishedValidationsComponent implements OnInit {
   publishedValidation$: Observable<ValidationrunDto[]>;
+  datasets?: DatasetDto[] = [];
+  versions?: DatasetVersionDto[] = [];
+  variables?: DatasetVariableDto[] = [];
 
-  constructor(private validationrunService: ValidationrunService) { }
+  constructor(private validationrunService: ValidationrunService,
+              private datasetService: DatasetService,
+              private datasetVersionService: DatasetVersionService,
+              private datasetVariableService: DatasetVariableService) {
+    // this.getAllDatasets();
+  }
 
   ngOnInit(): void {
     this.publishedValidation$ = this.validationrunService.getPublishedValidationruns();
+    this.getDatasetsInfo();
+
+  }
+
+  getDatasetsInfo(){
+    this.getAllDatasets(this.datasets);
+    this.getAllVersions(this.versions);
+    this.getAllVariables(this.variables);
+  }
+
+  private getAllDatasets(targetArray: DatasetDto[]) {
+    this.datasetService.getAllDatasets().subscribe(datasets => {
+      datasets.forEach(dataset => {
+        targetArray.push(dataset);
+      });
+    });
+  }
+
+  private getAllVersions(targetArray: DatasetVersionDto[]) {
+    this.datasetVersionService.getAllVersions().subscribe(versions => {
+      versions.forEach(version => {
+        targetArray.push(version);
+      });
+    });
+  }
+
+  private getAllVariables(targetArray: DatasetVariableDto[]) {
+    this.datasetVariableService.getAllVariables().subscribe(variables => {
+      variables.forEach(variable => {
+        targetArray.push(variable);
+      });
+    });
   }
 
 }
