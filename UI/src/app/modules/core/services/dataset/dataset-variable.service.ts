5211e16ded472a791e9343908db52fe9377739d5
bakcsa <zoltan@bakcsa.hu>
2021-03-06 15:59:19 +0100

    improve caching in dataset variables service

--- UI/src/app/modules/core/services/dataset/dataset-variable.service.ts
+++ UI/src/app/modules/core/services/dataset/dataset-variable.service.ts
@@ -1,69 +1,47 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {shareReplay} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {environment} from '../../../../../environments/environment';
 import {DatasetVariableDto} from './dataset-variable.dto';
+import {DataCache} from '../../tools/DataCache';
 
 const DATASET_VARIABLE_URL: string = environment.API_URL + 'api/dataset-variable';
-const CACHE_LIFETIME: number = 5 * 60 * 1000; // 5 minutes
 const CACHE_KEY_ALL_VERSIONS = -1;
 
-export class DatasetVariableCacheItem {
-  constructor(public lastFetched: Date, public datasetVariables$: Observable<DatasetVariableDto[]>) {
-  }
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatasetVariableService {
 
-  requestCache = new Map<number, DatasetVariableCacheItem>();
+  //cache for dataset arrays
+  arrayRequestCache = new DataCache<Observable<DatasetVariableDto[]>>(5);
+  //cache for single dataset dtos
+  singleRequestCache = new DataCache<Observable<DatasetVariableDto>>(5);
 
 
   constructor(private httpClient: HttpClient) {
-    this.requestCache.set(
-      CACHE_KEY_ALL_VERSIONS,
-      new DatasetVariableCacheItem(new Date(), this.httpClient.get<DatasetVariableDto[]>(DATASET_VARIABLE_URL).pipe(shareReplay())));
   }
 
   getAllVersions(): Observable<DatasetVariableDto[]> {
-    return this.getVariablesByDataset(CACHE_KEY_ALL_VERSIONS);
+    if (this.arrayRequestCache.isCached(CACHE_KEY_ALL_VERSIONS)) {
+      return this.arrayRequestCache.get(CACHE_KEY_ALL_VERSIONS);
+    } else {
+      let datasetVariables$ = this.httpClient.get<DatasetVariableDto[]>(DATASET_VARIABLE_URL).pipe(shareReplay());
+      this.arrayRequestCache.push(CACHE_KEY_ALL_VERSIONS, datasetVariables$);
+      return datasetVariables$;
+    }
+
   }
 
   getVariablesByDataset(datasetId: number): Observable<DatasetVariableDto[]> {
-    if (this.isCached(datasetId)) {
-      return this.requestCache.get(datasetId).datasetVariables$;
-    }
-
-    let request;
-    if (datasetId == CACHE_KEY_ALL_VERSIONS) {
-      request = this.httpClient.get<DatasetVariableDto[]>(DATASET_VARIABLE_URL).pipe(shareReplay());
+    if (this.arrayRequestCache.isCached(datasetId)) {
+      return this.arrayRequestCache.get(datasetId);
     } else {
-      let params = new HttpParams().set('dataset', String(datasetId));
-      request = this.httpClient.get<DatasetVariableDto[]>(DATASET_VARIABLE_URL, {params: params}).pipe(shareReplay());
+      let datasetVariables$ = this.httpClient.get<DatasetVariableDto[]>(DATASET_VARIABLE_URL).pipe(shareReplay());
+      this.arrayRequestCache.push(datasetId, datasetVariables$);
+      return datasetVariables$;
     }
-
-    let cacheItem = new DatasetVariableCacheItem(new Date(), request);
-    this.requestCache.set(datasetId, cacheItem);
-
-    return request;
-  }
-
-  /**
-   * Checks whether there is valid cache item for the specified datasetId
-   * @param datasetId
-   * @private
-   */
-  private isCached(datasetId: number): boolean {
-    if (this.requestCache.has(datasetId)) {
-      let cacheItem = this.requestCache.get(datasetId);
-      if (((new Date()).getTime() - cacheItem.lastFetched.getTime()) < CACHE_LIFETIME) {
-        return true;
-      }
-    }
-
-    return false;
   }
 }
