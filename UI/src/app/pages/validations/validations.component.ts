3422eb42f71e35a7ceae7df1c07e60670212b73c
sheenaze <monika.tercjak@gmail.com>
2021-03-08 14:56:56 +0100

    validations page improved according to changes in validation-row component

--- UI/src/app/pages/validations/validations.component.ts
+++ UI/src/app/pages/validations/validations.component.ts
@@ -1,9 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {ValidationrunDto} from '../../modules/validation-result/services/validationrun.dto';
-import {DatasetDto} from '../../modules/core/services/dataset/dataset.dto';
-import {DatasetVersionDto} from '../../modules/core/services/dataset/dataset-version.dto';
-import {DatasetVariableDto} from '../../modules/core/services/dataset/dataset-variable.dto';
 import {ValidationrunService} from '../../modules/validation-result/services/validationrun.service';
 
 @Component({
@@ -13,20 +10,10 @@ import {ValidationrunService} from '../../modules/validation-result/services/val
 })
 export class ValidationsComponent implements OnInit {
   myValidation$: Observable<ValidationrunDto[]>;
-  datasets?: DatasetDto[] = [];
-  versions?: DatasetVersionDto[] = [];
-  variables?: DatasetVariableDto[] = [];
 
   constructor(private validationrunService: ValidationrunService) { }
 
   ngOnInit(): void {
     this.myValidation$ = this.validationrunService.getMyValidationruns();
-    // this.getDatasetsInfo();
-  }
-
-  getDatasetsInfo(){
-    this.validationrunService.getAllDatasets(this.datasets);
-    this.validationrunService.getAllVersions(this.versions);
-    // this.validationrunService.getAllVariables(this.variables);
   }
 }
