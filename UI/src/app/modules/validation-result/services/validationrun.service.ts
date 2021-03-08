90d2495552388c34e4208c56c177afeda93bc626
sheenaze <monika.tercjak@gmail.com>
2021-03-08 15:00:12 +0100

    Retrievieng info on datasets (versions and variables) removed

--- UI/src/app/modules/validation-result/services/validationrun.service.ts
+++ UI/src/app/modules/validation-result/services/validationrun.service.ts
@@ -3,11 +3,6 @@ import {HttpClient} from '@angular/common/http';
 import {ValidationrunDto} from './validationrun.dto';
 import {Observable} from 'rxjs';
 import {environment} from '../../../../environments/environment';
-import {DatasetDto} from '../../core/services/dataset/dataset.dto';
-import {DatasetVersionDto} from '../../core/services/dataset/dataset-version.dto';
-import {DatasetService} from '../../core/services/dataset/dataset.service';
-import {DatasetVersionService} from '../../core/services/dataset/dataset-version.service';
-import {DatasetVariableService} from '../../core/services/dataset/dataset-variable.service';
 
 const publishedValidationRunUrl: string = environment.API_URL + 'api/published-results';
 const customValidationRunUrl: string = environment.API_URL + 'api/my-results';
@@ -20,10 +15,7 @@ export class ValidationrunService {
   customValidationrun$: Observable<ValidationrunDto[]>;
   publishedValidationrun$: Observable<ValidationrunDto[]>;
 
-  constructor(private httpClient: HttpClient,
-              private datasetService: DatasetService,
-              private datasetVersionService: DatasetVersionService,
-              private datasetVariableService: DatasetVariableService) {
+  constructor(private httpClient: HttpClient) {
     this.publishedValidationrun$ = this.httpClient.get<ValidationrunDto[]>(publishedValidationRunUrl);
   }
 
@@ -36,29 +28,4 @@ export class ValidationrunService {
     this.customValidationrun$ = this.httpClient.get<ValidationrunDto[]>(customValidationRunUrl);
     return  this.customValidationrun$;
   }
-
-  getAllDatasets(targetArray: DatasetDto[]) {
-    this.datasetService.getAllDatasets().subscribe(datasets => {
-      datasets.forEach(dataset => {
-        targetArray.push(dataset);
-      });
-    });
-  }
-
-  getAllVersions(targetArray: DatasetVersionDto[]) {
-    this.datasetVersionService.getAllVersions().subscribe(versions => {
-      versions.forEach(version => {
-        targetArray.push(version);
-      });
-    });
-  }
-
-  // getAllVariables(targetArray: DatasetVariableDto[]) {
-  //   this.datasetVariableService.getAllVariables().subscribe(variables => {
-  //     variables.forEach(variable => {
-  //       targetArray.push(variable);
-  //     });
-  //   });
-  // }
-
 }
