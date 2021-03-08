b7032365450b018303009a76f74c01830247661c
sheenaze <monika.tercjak@gmail.com>
2021-03-08 14:25:48 +0100

    Retrieving info on datasets removed from publishe-validationrun page

--- UI/src/app/pages/published-validations/published-validations.component.ts
+++ UI/src/app/pages/published-validations/published-validations.component.ts
@@ -2,9 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {ValidationrunDto} from '../../modules/validation-result/services/validationrun.dto';
 import {ValidationrunService} from '../../modules/validation-result/services/validationrun.service';
-import {DatasetDto} from '../../modules/core/services/dataset/dataset.dto';
-import {DatasetVersionDto} from '../../modules/core/services/dataset/dataset-version.dto';
-import {DatasetVariableDto} from '../../modules/core/services/dataset/dataset-variable.dto';
 
 @Component({
   selector: 'qa-published-validations',
@@ -13,22 +10,11 @@ import {DatasetVariableDto} from '../../modules/core/services/dataset/dataset-va
 })
 export class PublishedValidationsComponent implements OnInit {
   publishedValidation$: Observable<ValidationrunDto[]>;
-  datasets?: DatasetDto[] = [];
-  versions?: DatasetVersionDto[] = [];
-  variables?: DatasetVariableDto[] = [];
 
   constructor(private validationrunService: ValidationrunService) {
   }
 
   ngOnInit(): void {
     this.publishedValidation$ = this.validationrunService.getPublishedValidationruns();
-    this.getDatasetsInfo();
-
-  }
-
-  getDatasetsInfo(){
-    this.validationrunService.getAllDatasets(this.datasets);
-    this.validationrunService.getAllVersions(this.versions);
-    // this.validationrunService.getAllVariables(this.variables);
   }
 }
