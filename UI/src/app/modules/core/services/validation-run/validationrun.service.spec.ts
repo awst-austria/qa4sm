053fba52cabdb5e84960fc803b4b49b0f8481c68
bakcsa <zoltan@bakcsa.hu>
2021-03-09 00:35:37 +0100

    move validationrun.service to core

--- UI/src/app/modules/core/services/validation-run/validationrun.service.spec.ts
+++ UI/src/app/modules/core/services/validation-run/validationrun.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ValidationrunService } from './validationrun.service';
+import {ValidationrunService} from './validationrun.service';
 
 describe('ValidationrunService', () => {
   let service: ValidationrunService;
