18dd43aa7c018c7bb4fe750d2177d91733ddb501
sheenaze <monika.tercjak@gmail.com>
2021-03-09 10:57:03 +0100

    Tooltip module loaded

--- UI/src/app/modules/validation-result/validation-result.module.ts
+++ UI/src/app/modules/validation-result/validation-result.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ValidationrunRowComponent } from './components/validationrun-row/validationrun-row.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ValidationrunRowComponent} from './components/validationrun-row/validationrun-row.component';
 import {PanelModule} from 'primeng/panel';
+import {TooltipModule} from 'primeng/tooltip';
 
 
 @NgModule({
@@ -9,9 +10,10 @@ import {PanelModule} from 'primeng/panel';
     exports: [
         ValidationrunRowComponent
     ],
-  imports: [
-    CommonModule,
-    PanelModule
-  ]
+    imports: [
+        CommonModule,
+        PanelModule,
+        TooltipModule
+    ]
 })
 export class ValidationResultModule { }
