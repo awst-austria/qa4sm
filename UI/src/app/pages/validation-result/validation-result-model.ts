d8ce0707ad4bc7beffc4310348e9e56af3fe019b
bakcsa <zoltan@bakcsa.hu>
2021-03-09 23:01:17 +0100

    worked on validation-result component

--- UI/src/app/pages/validation-result/validation-result-model.ts
+++ UI/src/app/pages/validation-result/validation-result-model.ts
@@ -1,12 +1,11 @@
 import {ValidationrunDto} from '../../modules/core/services/validation-run/validationrun.dto';
 import {DatasetConfigurationDto} from '../../modules/validation-result/services/dataset-configuration.dto';
+import {Observable} from 'rxjs';
 
 export class ValidationResultModel {
-  constructor(public validationRun: ValidationrunDto,
-              public datasetConfigs: DatasetConfigurationDto[]) {
+  constructor(public validationRun: Observable<ValidationrunDto>,
+              public datasetConfigs: Observable<DatasetConfigurationDto[]>) {
   }
 
-  getComparedDatasets(): number {
-    return this.datasetConfigs.length;
-  }
+
 }
