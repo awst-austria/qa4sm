fe7dc5352d0c4e633a36e35f7249084d1ce7ac91
sheenaze <monika.tercjak@gmail.com>
2021-03-09 14:42:54 +0100

    font awesome added

--- UI/src/app/modules/validation-result/validation-result.module.ts
+++ UI/src/app/modules/validation-result/validation-result.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {ValidationrunRowComponent} from './components/validationrun-row/validationrun-row.component';
 import {PanelModule} from 'primeng/panel';
 import {TooltipModule} from 'primeng/tooltip';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
@@ -11,9 +12,10 @@ import {TooltipModule} from 'primeng/tooltip';
         ValidationrunRowComponent
     ],
     imports: [
-        CommonModule,
-        PanelModule,
-        TooltipModule
+      CommonModule,
+      PanelModule,
+      TooltipModule,
+      FontAwesomeModule
     ]
 })
 export class ValidationResultModule { }
