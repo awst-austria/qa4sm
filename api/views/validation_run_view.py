9dfe6bd14af3e6f6aee38d8d5ca3e2c910f087e8
sheenaze <monika.tercjak@gmail.com>
2021-03-11 16:00:51 +0100

    api view changed for validationruns so now it sends validationruns at sliced with given offset and limit and the total number of appropriate validations

--- api/views/validation_run_view.py
+++ api/views/validation_run_view.py
@@ -12,41 +12,43 @@ from validator.models import ValidationRun
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def published_results(request):
-    page = request.GET.get('page', 1)
+def published_results_paginated(request):
+    limit = int(request.query_params.get('limit', 10))
+    offset = int(request.query_params.get('offset', 0))
 
     val_runs = ValidationRun.objects.exclude(doi='').order_by('-start_time')
 
-    paginator = Paginator(val_runs, 10)
-    try:
-        paginated_runs = paginator.page(page)
-    except PageNotAnInteger:
-        paginated_runs = paginator.page(1)
-    except EmptyPage:
-        paginated_runs = paginator.page(paginator.num_pages)
+    serializer = ResultsSerializer(val_runs[offset:(offset+limit)], many=True)
+    return JsonResponse(({'validations': serializer.data, 'length': len(val_runs)}), status=status.HTTP_200_OK, safe=False)
 
-    serializer = ResultsSerializer(paginated_runs, many=True)
-    return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def published_results(request):
+    limit = int(request.query_params.get('limit', None))
+    offset = int(request.query_params.get('offset', None))
+
+    val_runs = ValidationRun.objects.exclude(doi='').order_by('-start_time')
+
+    serializer = ResultsSerializer(val_runs[offset:(offset+limit)], many=True)
+    response = {'validations': serializer.data, 'length': len(val_runs)}
+    return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
 
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def my_results(request):
-    page = request.GET.get('page', 1)
+    current_user = request.user
+    limit = int(request.query_params.get('limit', None))
+    offset = int(request.query_params.get('offset', None))
 
-    # toDO: val_runs should be filtered by user
-    val_runs = ValidationRun.objects.all().order_by('-start_time')
+    val_runs = ValidationRun.objects.filter(user=current_user).order_by('-start_time')
 
-    paginator = Paginator(val_runs, 10)
-    try:
-        paginated_runs = paginator.page(page)
-    except PageNotAnInteger:
-        paginated_runs = paginator.page(1)
-    except EmptyPage:
-        paginated_runs = paginator.page(paginator.num_pages)
+    serializer = ResultsSerializer(val_runs[offset:(offset+limit)], many=True)
+    response = {'validations': serializer.data, 'length': len(val_runs)}
 
-    serializer = ResultsSerializer(paginated_runs, many=True)
-    return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
+    return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
 
 
 class ResultsSerializer(ModelSerializer):
