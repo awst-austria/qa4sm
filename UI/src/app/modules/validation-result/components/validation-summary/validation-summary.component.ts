3d650627ba376426c2e639839f95fc666fc2bae0
sheenaze <monika.tercjak@gmail.com>
2021-03-19 12:34:21 +0100

    valrun passed as an input

--- UI/src/app/modules/validation-result/components/validation-summary/validation-summary.component.ts
+++ UI/src/app/modules/validation-result/components/validation-summary/validation-summary.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ValidationrunDto} from '../../../core/services/validation-run/validationrun.dto';
 
 @Component({
   selector: 'qa-validation-summary',
@@ -7,6 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ValidationSummaryComponent implements OnInit {
 
+  @Input() valrun: ValidationrunDto;
   constructor() { }
 
   ngOnInit(): void {
