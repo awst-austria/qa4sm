9f21db112cdcbaa6ff996a61df3b765301398233
bakcsa <zoltan@bakcsa.hu>
2021-03-22 18:50:10 +0100

    worked on map integration

--- UI/src/app/modules/map/components/map/map.component.ts
+++ UI/src/app/modules/map/components/map/map.component.ts
@@ -1,7 +1,7 @@
-import {AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, NgZone, Output} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, NgZone, Output} from '@angular/core';
 import {Map, View} from 'ol';
 import {Coordinate} from 'ol/coordinate';
-import {Attribution, Control} from 'ol/control';
+import {Attribution} from 'ol/control';
 import Projection from 'ol/proj/Projection';
 import {register} from 'ol/proj/proj4';
 import {get as GetProjection} from 'ol/proj';
@@ -9,13 +9,9 @@ import {Extent} from 'ol/extent';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
-// import * as proj4 from 'proj4';
-// import {proj4} from 'proj4';
 import * as proj4x from 'proj4';
-import Draw, {createBox} from 'ol/interaction/Draw';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import GeometryType from 'ol/geom/GeometryType';
+import {BoundingBoxControl} from './bounding-box-control';
+import {SpatialSubsetModel} from '../../../spatial-subset/components/spatial-subset/spatial-subset-model';
 
 const proj4 = (proj4x as any).default;
 
@@ -24,10 +20,11 @@ const proj4 = (proj4x as any).default;
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements AfterViewInit, AfterViewChecked {
+export class MapComponent implements AfterViewInit {
 
   @Input() center: Coordinate;
   @Input() zoom: number;
+  @Input() spatialSubset: SpatialSubsetModel;
   view: View;
   projection: Projection;
   extent: Extent = [-20026376.39, -20048966.10,
@@ -38,7 +35,6 @@ export class MapComponent implements AfterViewInit, AfterViewChecked {
   constructor(private zone: NgZone, private cd: ChangeDetectorRef) {
   }
 
-
   ngAfterViewInit(): void {
     console.log('View init');
     if (!this.Map) {
@@ -47,16 +43,13 @@ export class MapComponent implements AfterViewInit, AfterViewChecked {
     setTimeout(() => this.mapReady.emit(this.Map));
   }
 
-  ngAfterViewChecked(): void {
-    console.log('View checked');
-  }
-
   private initMap(): void {
     proj4.defs('EPSG:3857', '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs');
     proj4.defs('EPSG:4326', '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs');
 
     register(proj4);
-    this.projection = GetProjection('EPSG:3857');
+    let mapProjectionString = 'EPSG:3857';
+    this.projection = GetProjection(mapProjectionString);
     this.projection.setExtent(this.extent);
     this.view = new View({
       center: this.center,
@@ -70,50 +63,13 @@ export class MapComponent implements AfterViewInit, AfterViewChecked {
       })],
       target: 'map',
       // controls: DefaultControls({attribution: false}).extend([new MyControl(),new Attribution({collapsible: true,})]),
-      controls: [new MyControl(this.Map), new Attribution({collapsible: true,})],
+      controls: [new Attribution({collapsible: true,})],
       view: this.view,
     });
+    this.Map.addControl(new BoundingBoxControl(this.Map, this.spatialSubset, this.zone));
   }
 
-  // controls: DefaultControls().extend([
-  //                                      new ScaleLine({}),
-  //                                    ]),
+
 }
 
-export class MyControl extends Control {
-  constructor(private map) {
-    super({});
-    let button = document.createElement('button');
-    button.type = 'button';
-    button.innerHTML = '<i class="pi pi-pencil"></i>';
-    button.title = 'Select region';
 
-    let element = document.createElement('div');
-    element.className = 'rotate-north ol-unselectable ol-control';
-    element.appendChild(button);
-    Control.call(this, {
-      element: element
-    });
-    button.addEventListener('click', () => this.click());
-  }
-
-  click() {
-    console.log('click');
-    console.log(this.getMap());
-    var source = new VectorSource({wrapX: false});
-
-    var vector = new VectorLayer({
-      source: source,
-    });
-    let geomFunction = createBox();
-    let draw = new Draw({
-      source: source,
-      type: GeometryType.CIRCLE,
-      geometryFunction: geomFunction,
-    });
-    this.getMap().addLayer(vector);
-    this.getMap().addInteraction(draw);
-
-  }
-
-}
