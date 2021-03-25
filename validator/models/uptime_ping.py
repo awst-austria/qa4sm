37c3a09ee8d73ad745630bfe76e8021983c09469
bakcsa <zoltan@bakcsa.hu>
2021-03-26 00:10:19 +0100

    add models and migration

---
+++ validator/models/uptime_ping.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class UptimePing(models.Model):
+    id = models.AutoField(primary_key=True)
+    agent_key = models.CharField(max_length=200)
+    time = models.DateTimeField(auto_now_add=True)
