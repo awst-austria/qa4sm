6b202bcc0f8e652a1dbbc300caf6153c436aed61
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2021-04-15 15:53:14 +0200

    New UI fixing files downloading (#193)
    * a view for serving files added, a function for downloading files added, an appropriate url added
    
    * updated files serving module
    
    * functions and buttons for downloading files updated
    
    * unnecessary code removed
    
    * response changed to httpresponse to check if it's quicker
    
    * function for downloading data modified
    
    * getting back to FileResponse

--- api/views/serving_file_view.py
+++ api/views/serving_file_view.py
@@ -1,10 +1,5 @@
-import mimetypes
-import os
-from wsgiref.util import FileWrapper
-
-from django.http import FileResponse, HttpResponse
+from django.http import FileResponse
 from django.shortcuts import get_object_or_404
-from django.utils.encoding import smart_str
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -24,20 +19,5 @@ def download_results(request):
         filename = file_path + valrun.output_file_name
     else:
         filename = file_path + 'graphs.zip'
-    file_wrapper = FileWrapper(open(filename, 'rb'))
-    file_mimetype = mimetypes.guess_type(filename)
-    response = HttpResponse(file_wrapper, content_type=file_mimetype)
+    response = FileResponse(open(filename, 'rb'))
     return response
-
-# def download_results(request):
-#     validation_id = request.query_params.get('validationId', None)
-#     file_type = request.query_params.get('fileType', None)
-#     valrun = get_object_or_404(ValidationRun, pk=validation_id)
-#     file_path = valrun.output_dir_url.replace(settings.MEDIA_URL, settings.MEDIA_ROOT)
-#     if file_type == 'netCDF':
-#         filename = file_path + valrun.output_file_name
-#     else:
-#         filename = file_path + 'graphs.zip'
-#     response = FileResponse(open(filename, 'rb'))
-#     return response
-
