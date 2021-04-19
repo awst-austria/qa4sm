0a1b85553c8c6912deae97f4d825568811274beb
sheenaze <monika.tercjak@gmail.com>
2021-04-19 09:27:49 +0200

    summary statistics table added to the appropriate component

--- UI/src/app/modules/validation-result/components/summary-statistics/summary-statistics.component.ts
+++ UI/src/app/modules/validation-result/components/summary-statistics/summary-statistics.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ValidationrunService} from '../../../core/services/validation-run/validationrun.service';
+import {HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
 
 @Component({
   selector: 'qa-summary-statistics',
@@ -6,10 +10,30 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./summary-statistics.component.scss']
 })
 export class SummaryStatisticsComponent implements OnInit {
+  @Input() validationRunId: any;
+  @Input() referenceValidation: object;
+  table$: Observable<string>;
 
-  constructor() { }
+  constructor(private validationService: ValidationrunService) {
+  }
 
   ngOnInit(): void {
+    console.log(this.validationRunId);
+    this.getSummaryStatistics();
   }
+
+  getSummaryStatistics(): void {
+    const parameters = new HttpParams().set('id', this.validationRunId);
+    this.table$ = this.validationService.getSummaryStatistics(parameters);
+    // this.validationService.getSummaryStatistics(parameters).subscribe(data => {
+    //   this.table = data;
+    // });
+  }
+  // getSummaryStatistics(): void {
+  //   const summaryStatisticsUrl = `/api/summary-statistics?id=${this.validationRunId}`;
+  //   const params = new HttpParams().set('id', this.validationRunId);
+  //   this.httpClient.get(summaryStatisticsUrl).subscribe(data => console.log(data));
+  //   // this.httpClient.get(summaryStatisticsUrl, {params}).map(r => r.text()).subscribe(v => this.table = v);
+  // }
 
 }
