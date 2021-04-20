8102ef8d5220a0ddb953c716fbb16b8379406ccb
sheenaze <monika.tercjak@gmail.com>
2021-04-20 13:40:12 +0200

    dropdown added to display metrics

--- UI/src/app/modules/validation-result/components/result-files/result-files.component.ts
+++ UI/src/app/modules/validation-result/components/result-files/result-files.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {MetricsPlotsDto} from '../../../core/services/validation-run/metrics-plots.dto';
+import {ValidationrunService} from '../../../core/services/validation-run/validationrun.service';
+import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'qa-result-files',
@@ -6,10 +10,22 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./result-files.component.scss']
 })
 export class ResultFilesComponent implements OnInit {
+  @Input() validationId: string;
+  metricsPlotsNames$: Observable<MetricsPlotsDto[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private validationService: ValidationrunService) {
   }
 
+  ngOnInit(): void {
+    this.getMetricsAndPlotsNames();
+  }
+
+  getMetricsAndPlotsNames(): void {
+    const params = new HttpParams().set('validationId', this.validationId);
+    this.metricsPlotsNames$ = this.validationService.getMetricsAndPlotsNames(params);
+  }
+
+  onMetricChange(): void {
+    console.log('Metric has changed');
+  }
 }
