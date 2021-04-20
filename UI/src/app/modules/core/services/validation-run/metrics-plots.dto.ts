e68b997a2f361eb64a64a2c140e39c05183e6c5c
sheenaze <monika.tercjak@gmail.com>
2021-04-20 12:43:35 +0200

    metrics-plots.dto.ts added to get metrics names and associated plots

---
+++ UI/src/app/modules/core/services/validation-run/metrics-plots.dto.ts
@@ -0,0 +1,7 @@
+export class MetricsPlotsDto{
+  constructor(public metric_query_name: string,
+              public metric_pretty_name: string,
+              public boxplot_file: string,
+              public overview_files: []) {
+  }
+}
