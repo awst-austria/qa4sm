b094a3aee979ceec1a7045808bbf7a62e7dd68a5
sheenaze <monika.tercjak@gmail.com>
2021-04-22 09:15:31 +0200

    metric plots dto added

--- UI/src/app/modules/core/services/validation-run/metrics-plots.dto.ts
+++ UI/src/app/modules/core/services/validation-run/metrics-plots.dto.ts
@@ -2,6 +2,6 @@ export class MetricsPlotsDto{
   constructor(public metric_query_name: string,
               public metric_pretty_name: string,
               public boxplot_file: string,
-              public overview_files: []) {
+              public overview_files: string[]) {
   }
 }
