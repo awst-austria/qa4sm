7146f22ad9c6e1b284381470bae3752af001f7d9
sheenaze <monika.tercjak@gmail.com>
2021-04-23 13:12:48 +0200

    ind added to metrics-plots-dto

--- UI/src/app/modules/core/services/validation-run/metrics-plots.dto.ts
+++ UI/src/app/modules/core/services/validation-run/metrics-plots.dto.ts
@@ -2,6 +2,7 @@ export class MetricsPlotsDto{
   constructor(public metric_query_name: string,
               public metric_pretty_name: string,
               public boxplot_file: string,
-              public overview_files: string[]) {
+              public overview_files: string[],
+              public ind: number) {
   }
 }
