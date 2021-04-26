f299eaf4b92a7794f0e36d94a8ee3d73d0fbe054
Pietro Stradiotti <pietro.stradiotti@tuwien.ac.at>
2021-04-26 15:12:02 +0200

    Fixed link to section in help page

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -47,6 +47,7 @@ import {MapModule} from './modules/map/map.module';
 import {AboutComponent} from './pages/about/about.component';
 import {TermsComponent} from './pages/terms/terms.component';
 import {DatasetInfoComponent} from './pages/dataset-info/dataset-info.component';
+import {NgxPageScrollModule} from 'ngx-page-scroll';
 import { HelpComponent } from './pages/help/help.component';
 
 
@@ -96,7 +97,8 @@ import { HelpComponent } from './pages/help/help.component';
     AnomaliesModule,
     ScalingModule,
     NgxPaginationModule,
-    MapModule
+    MapModule,
+    NgxPageScrollModule,
 
   ],
   providers: [
