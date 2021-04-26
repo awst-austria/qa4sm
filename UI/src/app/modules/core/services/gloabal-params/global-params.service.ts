5e3c07d439a19899cb172e580f25cc2c5672951f
Pietro Stradiotti <pietro.stradiotti@tuwien.ac.at>
2021-04-26 14:15:47 +0200

    Connected component to view

--- UI/src/app/modules/core/services/gloabal-params/global-params.service.ts
+++ UI/src/app/modules/core/services/gloabal-params/global-params.service.ts
@@ -11,7 +11,14 @@ const globalContextUrl: string = environment.API_URL + 'api/globals';
 export class GlobalParamsService {
 
   // globalContext$: Observable<GlobalContextDto[]>;
-  public globalContext: GlobalParamsDto = {admin_mail: '', doi_prefix: '', site_url: '', app_version: ''};
+  public globalContext: GlobalParamsDto = {
+  admin_mail: '',
+  doi_prefix: '',
+  site_url: '',
+  app_version: '',
+  expiry_period: '',
+  warning_period: ''
+  };
 
   constructor(private httpClient: HttpClient) {
     this.init();
