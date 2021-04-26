9d05ab8b4f30bdc53df9e105882536522ffffac8
sheenaze <monika.tercjak@gmail.com>
2021-04-26 13:27:53 +0200

    plots on home page taken using website-graphics service

--- UI/src/app/pages/home/home.component.ts
+++ UI/src/app/pages/home/home.component.ts
@@ -1,30 +1,48 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../modules/core/services/auth/auth.service';
+import {WebsiteGraphicsService} from '../../modules/core/services/global/website-graphics.service';
+import {Observable} from 'rxjs';
+import {PlotDto} from '../../modules/core/services/global/plot.dto';
+import {HttpParams} from '@angular/common/http';
+import {SafeUrl} from '@angular/platform-browser';
 
 interface City {
-  name: string,
-  code: string
+  name: string;
+  code: string;
 }
-
-@Component({
+const homeUrlPrefix = '/static/images/home/';
+  @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   landingPageImages = [
-    'assets/landing_page_images/map_us_spearman.png',
-    'assets/landing_page_images/smos.jpg',
-    'assets/landing_page_images/root-zone_soil_moisture_may_2016.jpg',
+    homeUrlPrefix + 'map_us_spearman.png',
+    homeUrlPrefix + 'smos.jpg',
+    homeUrlPrefix + 'root-zone_soil_moisture_may_2016.jpg',
   ];
+  images$: Observable<PlotDto[]>;
 
   loginButtonDisabled: boolean = false;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              public plotService: WebsiteGraphicsService) {
   }
 
   ngOnInit(): void {
+    this. images$ = this.getPictures(this.landingPageImages);
     this.authService.authenticated.subscribe(authenticated => this.loginButtonDisabled = authenticated);
   }
 
+  getPictures(files: any): Observable<PlotDto[]> {
+    let params = new HttpParams();
+    files.forEach(file => {
+      params = params.append('file', file);
+    });
+    return this.plotService.getPlots(params);
+  }
+  getListOfPlots(listOfPlotDto: PlotDto[]): SafeUrl[]{
+    return this.plotService.sanitizeManyPlotUrls(listOfPlotDto);
+  }
 }
