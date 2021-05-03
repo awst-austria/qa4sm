3d3a6ffe3bcacfdb464cd6d6b6ea90346a1d1060
sheenaze <monika.tercjak@gmail.com>
2021-05-03 17:59:36 +0200

    changes I introduced here have been accepted by Zoltan and will be merged to angular_ui today

--- UI/src/app/modules/dataset/components/dataset/dataset.component.ts
+++ UI/src/app/modules/dataset/components/dataset/dataset.component.ts
@@ -55,24 +55,46 @@ export class DatasetComponent implements OnInit {
     this.selectableDatasetVariables$ = this.datasetVariableService.getVariablesByDataset(this.selectionModel.selectedDataset.id);
   }
 
-  private updateSelectableVersionsAndVariable() {
-    if (this.selectionModel.selectedDataset == undefined || this.selectionModel.selectedDataset.versions.length == 0) {
+  // private updateSelectableVersionsAndVariable() {
+  //   if (this.selectionModel.selectedDataset == undefined || this.selectionModel.selectedDataset.versions.length == 0) {
+  //     return;
+  //   }
+  //
+  //   this.selectableDatasetVersions$ = this.datasetVersionService.getVersionsByDataset(this.selectionModel.selectedDataset.id);
+  //   this.selectableDatasetVersions$.subscribe(versions => {
+  //     this.selectionModel.selectedVersion = versions[0];
+  //   });
+  //
+  //   this.selectableDatasetVariables$ = this.datasetVariableService.getVariablesByDataset(this.selectionModel.selectedDataset.id);
+  //   this.selectableDatasetVariables$.subscribe(variables => {
+  //     this.selectionModel.selectedVariable = variables[0];
+  //   });
+  // }
+
+  private updateSelectableVersionsAndVariableAndEmmit(): void{
+    if (this.selectionModel.selectedDataset === undefined || this.selectionModel.selectedDataset.versions.length == 0) {
       return;
     }
 
     this.selectableDatasetVersions$ = this.datasetVersionService.getVersionsByDataset(this.selectionModel.selectedDataset.id);
     this.selectableDatasetVersions$.subscribe(versions => {
-      this.selectionModel.selectedVersion = versions[0];
-    });
-
-    this.selectableDatasetVariables$ = this.datasetVariableService.getVariablesByDataset(this.selectionModel.selectedDataset.id);
-    this.selectableDatasetVariables$.subscribe(variables => {
-      this.selectionModel.selectedVariable = variables[0];
-    });
+        this.selectionModel.selectedVersion = versions[0];
+      },
+      () => {},
+      () => {
+        this.selectableDatasetVariables$ = this.datasetVariableService.getVariablesByDataset(this.selectionModel.selectedDataset.id);
+        this.selectableDatasetVariables$.subscribe(variables => {
+            this.selectionModel.selectedVariable = variables[0];
+          },
+          () => {},
+          () => this.changeDataset.emit(this.selectionModel));
+      });
   }
 
-  onDatasetChange() {
-    this.updateSelectableVersionsAndVariable();
+  onDatasetChange(): void{
+    this.updateSelectableVersionsAndVariableAndEmmit();
+  }
+  onVersionChange(): void{
     this.changeDataset.emit(this.selectionModel);
   }
 }
