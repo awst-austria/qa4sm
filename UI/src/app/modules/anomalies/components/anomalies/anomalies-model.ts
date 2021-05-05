975de88d851feb7decea67a0033bb99cb54d171b
sheenaze <monika.tercjak@gmail.com>
2021-05-05 12:26:22 +0200

    setAnomalies method added

--- UI/src/app/modules/anomalies/components/anomalies/anomalies-model.ts
+++ UI/src/app/modules/anomalies/components/anomalies/anomalies-model.ts
@@ -12,9 +12,10 @@ export class AnomaliesModel {
     return new NewValidationRunAnomaliesDto(this.method, this.anomaliesFrom, this.anomaliesTo);
   }
 
-  // public setAnomalies(method: string, anomaliesFrom: Date, anomaliesTo: Date): void{
-  //   this.method = method;
-  //   this.anomaliesFrom = anomaliesFrom;
-  //   this.anomaliesTo = anomaliesTo;
-  // }
+  public setAnomalies(method: string, anomaliesFrom: Date, anomaliesTo: Date): void{
+    this.method = method;
+    this.anomaliesFrom = anomaliesFrom;
+    this.anomaliesTo = anomaliesTo;
+    this.selected = true;
+  }
 }
