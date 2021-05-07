5778e04f7355dd36f893736287ff7d1d5a626161
sheenaze <monika.tercjak@gmail.com>
2021-05-07 12:00:09 +0200

    local api view created to serve general data locally

--- api/views/global_params_view.py
+++ api/views/global_params_view.py
@@ -5,7 +5,6 @@ from validator.context_processors import globals_processor
 from django.http import JsonResponse
 from rest_framework import status
 from django.conf import settings
-from django_countries import countries
 
 
 @api_view(['GET'])
@@ -21,11 +20,3 @@ def global_params(request):
     data.update(help_context)
 
     return JsonResponse(data, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def get_list_of_countries(request):
-    country_dict = countries.countries
-    response = [{'abbreviation': key, 'name': country_dict[key]} for key in country_dict]
-    return JsonResponse(response, safe=False)
