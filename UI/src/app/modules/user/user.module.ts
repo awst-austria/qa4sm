65e61f04043960d996a54684a2ba20dd5acc7e2d
sheenaze <monika.tercjak@gmail.com>
2021-05-07 12:36:54 +0200

    needed modules imported

--- UI/src/app/modules/user/user.module.ts
+++ UI/src/app/modules/user/user.module.ts
@@ -1,12 +1,22 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {UserFormComponent} from './user-form/user-form.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {InputTextModule} from 'primeng/inputtext';
+import {DropdownModule} from 'primeng/dropdown';
 
 
 @NgModule({
   declarations: [UserFormComponent],
+  exports: [
+    UserFormComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    DropdownModule,
+    FormsModule
   ]
 })
 export class UserModule { }
