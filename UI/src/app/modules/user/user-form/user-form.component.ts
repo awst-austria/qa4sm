ebefad3c05a012758b3e0475b475a08eb2251bc4
sheenaze <monika.tercjak@gmail.com>
2021-05-07 08:44:04 +0200

    user module with user-form component added

---
+++ UI/src/app/modules/user/user-form/user-form.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'qa-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.scss']
+})
+export class UserFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ UI/src/app/modules/user/user.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {UserFormComponent} from './user-form/user-form.component';
+
+
+@NgModule({
+  declarations: [UserFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
