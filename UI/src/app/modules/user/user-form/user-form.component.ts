6eca5e38465fa4858f298288484f8719a5a00979
sheenaze <monika.tercjak@gmail.com>
2021-05-07 12:37:31 +0200

    a basic form created

--- UI/src/app/modules/user/user-form/user-form.component.ts
+++ UI/src/app/modules/user/user-form/user-form.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {LocalApiService} from '../../core/services/global/local-api.service';
+import {CountryDto} from '../../core/services/global/country.dto';
 
 @Component({
   selector: 'qa-user-form',
@@ -6,10 +9,36 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+  userForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirmation: new FormControl(''),
+    email: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    organisation: new FormControl(''),
+    country: new FormControl(''),
+    orcid: new FormControl(''),
+  });
+  countries: CountryDto[];
+  selectedCountry: CountryDto;
 
-  constructor() { }
+  constructor(private localApiService: LocalApiService) { }
 
   ngOnInit(): void {
+    this.getListOfCountries();
+  }
+
+  onSubmit(): void{
+    console.log('Monika');
+  }
+
+  getListOfCountries(): void{
+    this.localApiService.getCountryList().subscribe(countries => {
+      console.log(countries);
+      this.countries = countries;
+      this.selectedCountry = countries[0];
+    });
   }
 
 }
