c96f3ce695c70df96c2219c8fdfd5693beb3c3c9
sheenaze <monika.tercjak@gmail.com>
2021-05-12 14:57:54 +0200

    controls names changed to be the same as in the User model

--- UI/src/app/modules/user/user-form/user-form.component.ts
+++ UI/src/app/modules/user/user-form/user-form.component.ts
@@ -1,8 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, Validators} from '@angular/forms';
 import {LocalApiService} from '../../core/services/global/local-api.service';
 import {CountryDto} from '../../core/services/global/country.dto';
 import {UserDto} from '../../core/services/auth/user.dto';
+import {AuthService} from '../../core/services/auth/auth.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'qa-user-form',
@@ -11,55 +13,56 @@ import {UserDto} from '../../core/services/auth/user.dto';
 })
 export class UserFormComponent implements OnInit {
   userForm = this.formBuilder.group({
-      username: [''],
+      username: ['', Validators.required],
       password: [''],
-      passwordConfirmation: [''],
+      password2: [''],
       email: [''],
-      firstName: [''],
-      lastName: [''],
+      first_name: [''],
+      last_name: [''],
       organisation: [''],
       country: [null],
       orcid: [''],
   });
 
   countries: CountryDto[];
+  countries$: Observable<CountryDto[]>;
   selectedCountry: CountryDto;
 
   @Input() userData: UserDto;
 
   constructor(private localApiService: LocalApiService,
-              private formBuilder: FormBuilder) { }
+              private formBuilder: FormBuilder,
+              private userService: AuthService) { }
 
   ngOnInit(): void {
-    this.getListOfCountries();
+    // this.getListOfCountries();
+    this.countries$ = this.localApiService.getCountryList();
     if (this.userData){
       this.setDefaultValues();
     }
   }
 
   onSubmit(): void{
-    console.log('Monika');
+    // const selectedCountryCode = this.userForm.value.country.code;
+    // this.userForm.controls.country.setValue(selectedCountryCode);
     console.warn(this.userForm.value);
+    this.userService.signUp(this.userForm.value);
   }
 
   getListOfCountries(): void{
     this.localApiService.getCountryList().subscribe(countries => {
       this.countries = countries;
-      this.selectedCountry = countries[0];
     });
   }
 
   setDefaultValues(): void{
     this.userForm.controls.username.setValue(this.userData.username);
     this.userForm.controls.email.setValue(this.userData.email);
-    this.userForm.controls.firstName.setValue(this.userData.first_name);
-    this.userForm.controls.lastName.setValue(this.userData.last_name);
+    this.userForm.controls.first_name.setValue(this.userData.first_name);
+    this.userForm.controls.last_name.setValue(this.userData.last_name);
     this.userForm.controls.organisation.setValue(this.userData.organisation);
-    this.userForm.controls.orcid.setValue(this.userData.orcid);
     this.userForm.controls.country.setValue(this.userData.country);
-    console.log(this.countries.find(country => country.name === this.userData.country));
-    // this.selectedCountry.name = this.userData.country;
-    console.log(this.userForm.controls.country);
+    this.userForm.controls.orcid.setValue(this.userData.orcid);
   }
 
 }
