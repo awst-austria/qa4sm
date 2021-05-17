f5db79e8fd5722dade3fd2f0a42d88981ca2673e
sheenaze <monika.tercjak@gmail.com>
2021-05-17 15:40:29 +0200

    first attribute changed from a list to a single plotDto element

--- UI/src/app/pages/home/homepage-images-model.ts
+++ UI/src/app/pages/home/homepage-images-model.ts
@@ -2,7 +2,7 @@ import {PlotDto} from '../../modules/core/services/global/plot.dto';
 import {Observable} from 'rxjs';
 
 export class HomepageImagesModel{
-  constructor(public plot: Observable<PlotDto[]>,
+  constructor(public plot: Observable<PlotDto>,
               public link: string,
               public description: string) {
   }
