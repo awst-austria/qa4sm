19e96186c616df6ad9948aba4848e8fc3ecf140c
sheenaze <monika.tercjak@gmail.com>
2021-05-17 15:35:49 +0200

    model added to be used as an object in carousel

---
+++ UI/src/app/pages/home/homepage-images-model.ts
@@ -0,0 +1,9 @@
+import {PlotDto} from '../../modules/core/services/global/plot.dto';
+import {Observable} from 'rxjs';
+
+export class HomepageImagesModel{
+  constructor(public plot: Observable<PlotDto[]>,
+              public link: string,
+              public description: string) {
+  }
+}
