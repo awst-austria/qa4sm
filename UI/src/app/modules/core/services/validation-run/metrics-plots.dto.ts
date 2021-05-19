44166180edb68032be67eb05f84108b224c84bd9
sheenaze <monika.tercjak@gmail.com>
2021-05-19 15:57:09 +0200

    get back to overview_files name

--- UI/src/app/modules/core/services/validation-run/metrics-plots.dto.ts
+++ UI/src/app/modules/core/services/validation-run/metrics-plots.dto.ts
@@ -2,7 +2,7 @@ export class MetricsPlotsDto{
   constructor(public metric_query_name: string,
               public metric_pretty_name: string,
               public boxplot_file: string,
-              public overview_plots: string[],
+              public overview_files: string[],
               public ind: number,
               public datasets: string[]) {
   }
