8646ea1c0fda40362d224a6e71247e5face2f168
sheenaze <monika.tercjak@gmail.com>
2021-05-19 15:55:48 +0200

    datasets attribute added

--- UI/src/app/modules/core/services/validation-run/metrics-plots.dto.ts
+++ UI/src/app/modules/core/services/validation-run/metrics-plots.dto.ts
@@ -2,7 +2,8 @@ export class MetricsPlotsDto{
   constructor(public metric_query_name: string,
               public metric_pretty_name: string,
               public boxplot_file: string,
-              public overview_files: string[],
-              public ind: number) {
+              public overview_plots: string[],
+              public ind: number,
+              public datasets: string[]) {
   }
 }
