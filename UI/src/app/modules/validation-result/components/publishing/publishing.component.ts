53f765494a7482d3e3ab416810c9221d8240ce31
sheenaze <monika.tercjak@gmail.com>
2021-07-07 15:26:36 +0200

    basic styling of the publishing form added

--- UI/src/app/modules/validation-result/components/publishing/publishing.component.ts
+++ UI/src/app/modules/validation-result/components/publishing/publishing.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {ModalWindowService} from '../../../core/services/global/modal-window.service';
+import {PublishingFormDto} from '../../../core/services/validation-run/publishing-form.dto';
+import {ValidationrunService} from '../../../core/services/validation-run/validationrun.service';
+import {HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'qa-publishing',
@@ -8,15 +11,28 @@ import {ModalWindowService} from '../../../core/services/global/modal-window.ser
   styleUrls: ['./publishing.component.scss']
 })
 export class PublishingComponent implements OnInit {
+  publishingForm$: Observable<PublishingFormDto>;
   display$: Observable<'open' | 'close'>;
+  @Input() validationId: string;
+
   constructor(
-    private modalService: ModalWindowService
+    private modalService: ModalWindowService,
+    private validationrunService: ValidationrunService
   ) { }
 
   ngOnInit(): void {
     this.display$ = this.modalService.watch();
+    this.getPublishingForm();
   }
   close(): void{
     this.modalService.close();
+    // this.publishingOn.emit(false);
+  }
+  publish(): void{
+    console.log('Monika');
+  }
+  getPublishingForm(): void{
+    const params = new HttpParams().set('id', this.validationId);
+    this.publishingForm$ = this.validationrunService.getPublishingFormData(params);
   }
 }
