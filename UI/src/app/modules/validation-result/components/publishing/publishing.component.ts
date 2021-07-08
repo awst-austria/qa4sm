ae99d1f219ec3169d9e761d10526d18a5386e529
sheenaze <monika.tercjak@gmail.com>
2021-07-08 09:23:22 +0200

    publishing form added and styled

--- UI/src/app/modules/validation-result/components/publishing/publishing.component.ts
+++ UI/src/app/modules/validation-result/components/publishing/publishing.component.ts
@@ -4,6 +4,7 @@ import {ModalWindowService} from '../../../core/services/global/modal-window.ser
 import {PublishingFormDto} from '../../../core/services/validation-run/publishing-form.dto';
 import {ValidationrunService} from '../../../core/services/validation-run/validationrun.service';
 import {HttpParams} from '@angular/common/http';
+import {FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'qa-publishing',
@@ -15,9 +16,19 @@ export class PublishingComponent implements OnInit {
   display$: Observable<'open' | 'close'>;
   @Input() validationId: string;
 
+  publishingForm = this.formBuilder.group({
+    title: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    keywords: ['', [Validators.required]],
+    name: ['', [Validators.required]],
+    affiliation: [''],
+    orcid: ['', [Validators.maxLength(25)]]
+  });
+
   constructor(
     private modalService: ModalWindowService,
-    private validationrunService: ValidationrunService
+    private validationrunService: ValidationrunService,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
