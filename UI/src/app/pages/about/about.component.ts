93b6197ade510483ec00ab80fcea92350f0d967c
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2021-07-16 16:13:24 +0200

    Reloding improvements (#246)
    * picture added as a background
    
    * picture added based on its url
    
    * getting back to base64 and image tag
    
    * bug fixed
    
    * url once more time for tests
    
    * source changed
    
    * jpg file added to replace the same png one
    
    * file size changed
    
    * width and height added to image classes
    
    * unnecessary a tag removed
    
    * tag meta added
    
    * aria label added
    
    * components width added
    
    * attribute rel added to links
    
    * some improvements done, e.g. alt texts added
    
    * rel attribute added to all links
    
    * a bug fixed, descriptions were inappropriately assigned
    
    * unnecessary link removed
    
    * rel attribute added
    
    * rel attribute added
    
    * aria-label added to a button
    
    * pictures on the main page set with their url not with base64 encoding
    
    * plots taken by their url
    
    * logos taken by their url
    
    * some code removed

--- UI/src/app/pages/about/about.component.ts
+++ UI/src/app/pages/about/about.component.ts
@@ -1,10 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {GlobalParamsService} from '../../modules/core/services/global/global-params.service';
 import {WebsiteGraphicsService} from '../../modules/core/services/global/website-graphics.service';
-import {Observable} from 'rxjs';
-import {PlotDto} from '../../modules/core/services/global/plot.dto';
-import {HttpParams} from '@angular/common/http';
-import {SafeUrl} from '@angular/platform-browser';
 
 const logoUrlPrefix = '/static/images/logo/';
 const logoAwstUrl = logoUrlPrefix + 'logo_awst.png';
@@ -19,33 +15,22 @@ const logoTuWienUrl = logoUrlPrefix + 'logo_tuwien_geo.png';
 })
 
 export class AboutComponent implements OnInit {
-  logoAwst$: Observable<PlotDto[]>;
-  logoEodc$: Observable<PlotDto[]>;
-  logoFfg$: Observable<PlotDto[]>;
-  logoTuWien$: Observable<PlotDto[]>;
+  logoAwst: string;
+  logoEodc: string;
+  logoFfg: string;
+  logoTuWien: string;
 
   constructor(private globalParamsService: GlobalParamsService,
               public plotService: WebsiteGraphicsService) { }
 
   ngOnInit(): void {
-    this.logoAwst$ = this.getLogos([logoAwstUrl]);
-    this.logoEodc$ = this.getLogos([logoEodcUrl]);
-    this.logoFfg$ = this.getLogos([logoFfgUrl]);
-    this.logoTuWien$ = this.getLogos([logoTuWienUrl]);
+    this.logoAwst = logoAwstUrl;
+    this.logoEodc = logoEodcUrl;
+    this.logoFfg = logoFfgUrl;
+    this.logoTuWien = logoTuWienUrl;
   }
   getAppVersion(): string {
     return this.globalParamsService.globalContext.app_version;
    }
-
-  getLogos(files: any): Observable<PlotDto[]> {
-    let params = new HttpParams();
-    files.forEach(file => {
-      params = params.append('file', file);
-    });
-    return this.plotService.getPlots(params);
-  }
-  sanitizePlotUrl(plotUrl: string): SafeUrl{
-    return this.plotService.sanitizePlotUrl(plotUrl);
-  }
 
 }
