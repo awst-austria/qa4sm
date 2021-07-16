b75e6e3d060a8dfffb9c1558748c658850eef1a5
sheenaze <monika.tercjak@gmail.com>
2021-07-16 15:47:32 +0200

    plots taken by their url

--- UI/src/app/pages/help/help.component.ts
+++ UI/src/app/pages/help/help.component.ts
@@ -1,13 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {GlobalParamsService} from '../../modules/core/services/global/global-params.service';
 import {fas} from '@fortawesome/free-solid-svg-icons';
-import {WebsiteGraphicsService} from '../../modules/core/services/global/website-graphics.service';
-import {Observable} from 'rxjs';
-import {HttpParams} from '@angular/common/http';
-import {PlotDto} from '../../modules/core/services/global/plot.dto';
-import {SafeUrl} from '@angular/platform-browser';
 
 const plotsUrlPrefix = '/static/images/help/';
+
 @Component({
   selector: 'qa-help',
   templateUrl: './help.component.html',
@@ -15,73 +11,87 @@ const plotsUrlPrefix = '/static/images/help/';
 })
 export class HelpComponent implements OnInit {
   // Icons for bullet points
-  faIcons = {faArchive: fas.faArchive,
+  faIcons = {
+    faArchive: fas.faArchive,
     faStop: fas.faStop,
     faFileDownload: fas.faFileDownload,
-    faRedo: fas.faRedo};
+    faRedo: fas.faRedo
+  };
 
 
-  menuMinus$: Observable<PlotDto>;
-  menuPlus$: Observable<PlotDto>;
-  datasetSelections$: Observable<PlotDto>;
-  intercomparison$: Observable<PlotDto>;
-  referenceDatasetSelection$: Observable<PlotDto>;
-  anomalies$: Observable<PlotDto>;
-  spatialSubsetting$: Observable<PlotDto>;
-  mapSelection$: Observable<PlotDto>;
-  validationPeriod$: Observable<PlotDto>;
-  tca$: Observable<PlotDto>;
-  scaling$: Observable<PlotDto>;
-  nameYourValidation$: Observable<PlotDto>;
-  validateButton$: Observable<PlotDto>;
-  myValidations$: Observable<PlotDto>;
-  resultsOverview$: Observable<PlotDto>;
-  resultsGraphs$: Observable<PlotDto>;
-  publicationDialog$: Observable<PlotDto>;
+  // menuMinus$: Observable<PlotDto>;
+  // menuPlus$: Observable<PlotDto>;
+  // datasetSelections$: Observable<PlotDto>;
+  // intercomparison$: Observable<PlotDto>;
+  // referenceDatasetSelection$: Observable<PlotDto>;
+  // anomalies$: Observable<PlotDto>;
+  // spatialSubsetting$: Observable<PlotDto>;
+  // mapSelection$: Observable<PlotDto>;
+  // validationPeriod$: Observable<PlotDto>;
+  // tca$: Observable<PlotDto>;
+  // scaling$: Observable<PlotDto>;
+  // nameYourValidation$: Observable<PlotDto>;
+  // validateButton$: Observable<PlotDto>;
+  // myValidations$: Observable<PlotDto>;
+  // resultsOverview$: Observable<PlotDto>;
+  // resultsGraphs$: Observable<PlotDto>;
+  // publicationDialog$: Observable<PlotDto>;
 
-  constructor(private globalParamsService: GlobalParamsService,
-              private plotService: WebsiteGraphicsService) { }
+  menuMinus: string;
+  menuPlus: string;
+  datasetSelections: string;
+  intercomparison: string;
+  referenceDatasetSelection: string;
+  anomalies: string;
+  spatialSubsetting: string;
+  mapSelection: string;
+  validationPeriod: string;
+  tca: string;
+  scaling: string;
+  nameYourValidation: string;
+  validateButton: string;
+  myValidations: string;
+  resultsOverview: string;
+  resultsGraphs: string;
+  publicationDialog: string;
+
+  constructor(private globalParamsService: GlobalParamsService) {
+  }
 
   ngOnInit(): void {
-    this.getHelpPagePlots();
-  }
-  getHelpPagePlots(): void{
-    this.menuMinus$ = this.getPlot(plotsUrlPrefix + 'menu_minus.png');
-    this.menuPlus$ = this.getPlot(plotsUrlPrefix + 'menu_plus.png');
-    this.datasetSelections$ = this.getPlot(plotsUrlPrefix + 'data_set_selections.png');
-    this.intercomparison$ = this.getPlot(plotsUrlPrefix + 'intercomparison.png');
-    this.referenceDatasetSelection$ = this.getPlot(plotsUrlPrefix + 'reference_data_set_selections.png');
-    this.anomalies$ = this.getPlot(plotsUrlPrefix + 'anomalies.png');
-    this.spatialSubsetting$ = this.getPlot(plotsUrlPrefix + 'spatial_subsetting.png');
-    this.mapSelection$ = this.getPlot(plotsUrlPrefix + 'map_selection.png');
-    this.validationPeriod$ = this.getPlot(plotsUrlPrefix + 'validation_period.png');
-    this.tca$ = this.getPlot(plotsUrlPrefix + 'tca.png');
-    this.scaling$ = this.getPlot(plotsUrlPrefix + 'scaling.png');
-    this.nameYourValidation$ = this.getPlot(plotsUrlPrefix + 'name_your_validation.png');
-    this.validateButton$ = this.getPlot(plotsUrlPrefix + 'validate_button.png');
-    this.myValidations$ = this.getPlot(plotsUrlPrefix + 'my_validations.png');
-    this.resultsOverview$ = this.getPlot(plotsUrlPrefix + 'results_overview.png');
-    this.resultsGraphs$ = this.getPlot(plotsUrlPrefix + 'results_graphs.png');
-    this.publicationDialog$ = this.getPlot(plotsUrlPrefix + 'publication_dialog.png');
+    this.helpPagePlots();
   }
 
-  getPlot(plot): Observable<any>{
-    const params = new HttpParams().set('file', plot);
-    return this.plotService.getSinglePlot(params);
+  helpPagePlots(): void {
+    this.menuMinus = plotsUrlPrefix + 'menu_minus.png';
+    this.menuPlus = plotsUrlPrefix + 'menu_plus.png';
+    this.datasetSelections = plotsUrlPrefix + 'data_set_selections.png';
+    this.intercomparison = plotsUrlPrefix + 'intercomparison.png';
+    this.referenceDatasetSelection = plotsUrlPrefix + 'reference_data_set_selections.png';
+    this.anomalies = plotsUrlPrefix + 'anomalies.png';
+    this.spatialSubsetting = plotsUrlPrefix + 'spatial_subsetting.png';
+    this.mapSelection = plotsUrlPrefix + 'map_selection.png';
+    this.validationPeriod = plotsUrlPrefix + 'validation_period.png';
+    this.tca = plotsUrlPrefix + 'tca.png';
+    this.scaling = plotsUrlPrefix + 'scaling.png';
+    this.nameYourValidation = plotsUrlPrefix + 'name_your_validation.png';
+    this.validateButton = plotsUrlPrefix + 'validate_button.png';
+    this.myValidations = plotsUrlPrefix + 'my_validations.png';
+    this.resultsOverview = plotsUrlPrefix + 'results_overview.png';
+    this.resultsGraphs = plotsUrlPrefix + 'results_graphs.png';
+    this.publicationDialog = plotsUrlPrefix + 'publication_dialog.png';
   }
 
   getAdminMail(): string {
     return this.globalParamsService.globalContext.admin_mail;
-   }
+  }
+
   getExpiryPeriod(): string {
     return this.globalParamsService.globalContext.expiry_period;
-   }
+  }
+
   getWarningPeriod(): string {
     return this.globalParamsService.globalContext.warning_period;
-   }
-
-  sanitizePlotUrl(plotUrl: string): SafeUrl{
-    return this.plotService.sanitizePlotUrl(plotUrl);
   }
 
 }
