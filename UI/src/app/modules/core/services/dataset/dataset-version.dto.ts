76f28b4949e26687c445422a8da669ddb3d2a7ab
sheenaze <monika.tercjak@gmail.com>
2021-07-23 15:26:41 +0200

    dataset version dto updated

--- UI/src/app/modules/core/services/dataset/dataset-version.dto.ts
+++ UI/src/app/modules/core/services/dataset/dataset-version.dto.ts
@@ -5,7 +5,8 @@ export class DatasetVersionDto {
     public pretty_name: string,
     public help_text: string,
     public time_range_start: Date,
-    public time_range_end: Date
+    public time_range_end: Date,
+    public geographical_range: object,
   ) {
   }
 }
