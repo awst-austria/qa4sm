733f6cab7c27bc2f79227d5f0136ea789c58b0a5
sheenaze <monika.tercjak@gmail.com>
2021-07-26 13:35:42 +0200

    geographical range attribute changed to be a dict

--- UI/src/app/modules/core/services/dataset/dataset-version.dto.ts
+++ UI/src/app/modules/core/services/dataset/dataset-version.dto.ts
@@ -6,7 +6,7 @@ export class DatasetVersionDto {
     public help_text: string,
     public time_range_start: Date,
     public time_range_end: Date,
-    public geographical_range: object,
+    public geographical_range: {max_lat: number, max_lon: number, min_lat: number, min_lon: number},
   ) {
   }
 }
