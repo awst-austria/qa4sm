449a05ebbc8694c6ad3990f9625841a6e31643c0
Pietro Stradiotti <pietro.stradiotti@tuwien.ac.at>
2021-07-29 14:30:56 +0200

    removed circular import in migrations

--- validator/migrations/0031_auto_20210505_1855.py
+++ validator/migrations/0031_auto_20210505_1855.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('validator', '0031_auto_20210505_1855'),
+        ('validator', '0030_uptimereport_agent_key'),
     ]
 
     operations = [
