5e7a5d1fa078cebc53b57ae5351f074dfea7d176
sheenaze <monika.tercjak@gmail.com>
2021-07-30 13:09:28 +0200

    code refinement done

--- UI/src/app/modules/comparison/components/comparison-summary/comparison-summary.component.ts
+++ UI/src/app/modules/comparison/components/comparison-summary/comparison-summary.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {Validations2CompareModel} from "../validation-selector/validation-selection.model";
+import {Component, OnInit} from '@angular/core';
+import {Validations2CompareModel} from '../validation-selector/validation-selection.model';
 import {ComparisonService} from '../../services/comparison.service';
-import {Observable} from 'rxjs';
-import {PlotDto} from '../../../core/services/global/plot.dto';
-import {ExtentModel} from "../spatial-extent/extent-model";
-import {ValidationResultModel} from "../../../../pages/validation-result/validation-result-model";
-import {ValidationrunService} from "../../../core/services/validation-run/validationrun.service";
-import {DatasetConfigurationService} from "../../../validation-result/services/dataset-configuration.service";
+import {ExtentModel} from '../spatial-extent/extent-model';
+import {ValidationResultModel} from '../../../../pages/validation-result/validation-result-model';
+import {ValidationrunService} from '../../../core/services/validation-run/validationrun.service';
+import {DatasetConfigurationService} from '../../../validation-result/services/dataset-configuration.service';
 
 @Component({
   selector: 'qa-comparison-summary',
@@ -17,13 +15,12 @@ export class ComparisonSummaryComponent implements OnInit {
   comparisonModel: Validations2CompareModel = new Validations2CompareModel(
     [], new ExtentModel(true).getIntersection,
   );
-  isSingle: boolean = true;
+  isSingle = true;
   valResModels: ValidationResultModel[] = [];
-  extentImage$: Observable<PlotDto>;
 
   constructor(private validationRunService: ValidationrunService,
               private datasetConfigurationService: DatasetConfigurationService,
-              private comparisonService: ComparisonService,) {
+              private comparisonService: ComparisonService) {
   }
 
   ngOnInit(): void {
@@ -35,22 +32,22 @@ export class ComparisonSummaryComponent implements OnInit {
     this.comparisonService.currentComparisonModel.subscribe(comparison => {
       if (comparison.selectedValidations.length > 0) {
         this.comparisonModel = comparison;
-        this.getValidationResultModels(comparison)
-        this.isSingle = (this.comparisonModel.selectedValidations.length == 1);
+        this.getValidationResultModels(comparison);
+        this.isSingle = (this.comparisonModel.selectedValidations.length === 1);
       }
     });
   }
 
   getValidationResultModels(comparisonModel: Validations2CompareModel): void {
     // momentarily unused
-    this.valResModels = []
-    for (let val of comparisonModel.selectedValidations) {
+    this.valResModels = [];
+    for (const val of comparisonModel.selectedValidations) {
       this.valResModels.push(
         new ValidationResultModel(
           this.validationRunService.getValidationRunById(val.id),
           this.datasetConfigurationService.getConfigByValidationrun(val.id)
         )
-      )
+      );
     }
   }
 }
