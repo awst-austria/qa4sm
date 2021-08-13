862f34b9247c120e79e1a716b5b0e6c6db41ecd6
sheenaze <monika.tercjak@gmail.com>
2021-08-13 13:24:56 +0200

    EmailAdmin model updated

--- validator/admin/email_admin.py
+++ validator/admin/email_admin.py
@@ -15,21 +15,26 @@ bulk_send_email_to_users.short_description = 'Send an email to the chosen users'
 class EmailAdmin(ModelAdmin):
     __logger = logging.getLogger(__name__)
     model = Email
-
-    list_display = ('date', 'content')
+    fields = ('send_to', 'subject','content', 'date', 'send_this_email' )
+    list_display = ('date', 'subject','content')
 
     def __init__(self, model, admin_site):
         super(EmailAdmin, self).__init__(model, admin_site)
 
+    def save_related(self, request, form, formsets, change):
+        super(EmailAdmin, self).save_related(request, form, formsets, change)
+        if form.instance.send_this_email:
+            form.instance.send_email()
+            form.instance.sent = True
     # def get_urls(self):
-    #     urls = super(SendEmailToUsers, self).get_urls()
+    #     urls = super(EmailAdmin, self).get_urls()
     #     new_urls = [
-    #         path('', self.admin_site.admin_view(self.send_email), name='send-email')
+    #         path('send-email', self.admin_site.admin_view(self.send_email), name='send-email')
     #     ]
     #
     #     return new_urls + urls
-    #
+
     # def send_email(self, request):
     #     # here I'll create a send email form
     #     context = self.admin_site.each_context(request)
-    #     return render(request, 'admin/send_email.html', context)
+    #     return render(request, 'admin/send_email.html')
