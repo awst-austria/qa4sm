4992fb78a42ffbf1be71c44fd1fd0ba6882e2f08
sheenaze <monika.tercjak@gmail.com>
2021-08-13 13:53:11 +0200

    order changed, so it's possible to use content of .email_admin in .custom_user_admin

--- validator/admin/__init__.py
+++ validator/admin/__init__.py
@@ -1,8 +1,8 @@
+from .email_admin import *
 from .custom_user_admin import *
 from .system_settings import *
 from .validation_run import *
 from .qa4sm_statistics import *
-from .email_admin import *
 # anything that is then registered in .general.py must go before!
 from .general import *
 
