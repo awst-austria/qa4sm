89fdc760d7551594feb38cf9c61cdfec2ad9be57
sheenaze <monika.tercjak@gmail.com>
2021-08-13 10:38:15 +0200

    email model updated

--- validator/models/email.py
+++ validator/models/email.py
@@ -1,10 +1,9 @@
 from django.db import models
 from validator.models import User
-import datetime
-
+from django.utils import timezone
 
 class Email(models.Model):
     send_to = models.ManyToManyField(User, related_name='addressees')
     content = models.TextField()
-    date = models.DateTimeField(null=True, default=datetime.date.today)
+    date = models.DateTimeField(null=True, default=timezone.now)
     sent = models.BooleanField(default=False)
