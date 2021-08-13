d30c277a13c32c2777b969c831fcb38eb36aa4ab
sheenaze <monika.tercjak@gmail.com>
2021-08-13 10:28:25 +0200

    email model updated

--- validator/models/email.py
+++ validator/models/email.py
@@ -1,8 +1,10 @@
 from django.db import models
 from validator.models import User
+import datetime
 
 
 class Email(models.Model):
     send_to = models.ManyToManyField(User, related_name='addressees')
     content = models.TextField()
-    date = models.DateTimeField(null=True)
+    date = models.DateTimeField(null=True, default=datetime.date.today)
+    sent = models.BooleanField(default=False)
