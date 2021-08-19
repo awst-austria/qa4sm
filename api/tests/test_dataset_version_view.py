00ad7dba5ae3d4ddc92dc952742b86092c574f37
sheenaze <monika.tercjak@gmail.com>
2021-08-19 10:31:06 +0200

    tests for dataset version view added

---
+++ api/tests/test_dataset_version_view.py
@@ -0,0 +1,55 @@
+import logging
+
+from django.test import TestCase
+from rest_framework.test import APIClient
+from api.tests.test_helper import *
+
+
+class TestDatasetVersionView(TestCase):
+
+    __logger = logging.getLogger(__name__)
+    databases = '__all__'
+    allow_database_queries = True
+    fixtures = ['datasets', 'filters', 'versions', 'variables']
+
+    def setUp(self):
+        self.auth_data, self.test_user = create_test_user()
+        self.client = APIClient()
+        self.client.login(**self.auth_data)
+
+    def test_dataset_version(self):
+        # check all the versions
+        response = self.client.get('/api/dataset-version')
+        assert response.status_code == 200
+        assert len(response.json()) == 25
+
+        # check versions based on the given dataset
+        response = self.client.get('/api/dataset-version?dataset=1')  # C3S
+        assert response.status_code == 200
+        assert len(response.json()) == 4  # there are 4 C3S versions currently
+
+        # check wrong dataset id
+        response = self.client.get('/api/dataset-version?dataset=100')  # wrong
+        assert response.status_code == 404
+
+        # check version id:
+        response = self.client.get('/api/dataset-version?version_id=1')
+        assert response.status_code == 200
+        assert response.json()['short_name'] == 'C3S_V201706'
+
+        response = self.client.get('/api/dataset-version?version_id=1000')  # wrong version id
+        assert response.status_code == 404
+
+        # check for logged out
+        self.client.logout()
+
+        response = self.client.get('/api/dataset-version')
+        assert response.status_code == 200
+        assert len(response.json()) == 25
+
+
+
+
+
+
+
