a7761808afd835af4fa2424f29ad80af518163dc
sheenaze <monika.tercjak@gmail.com>
2021-08-19 13:14:35 +0200

    tests updated according to recent changes

--- api/tests/test_data_filter_view.py
+++ api/tests/test_data_filter_view.py
@@ -25,19 +25,25 @@ class TestDataFilterView(TestCase):
         # currently we have 23 filters, for some reason we don't have a filter with id = 8
         assert len(response.json()) == 23
 
+        # log out to check if it still works
+        self.client.logout()
+
+        response = self.client.get('/api/data-filter')
+        assert response.status_code == 200
+        assert len(response.json()) == 23
+
+    def test_data_filter_by_dataset(self):
         # check filters for C3S (id = 1)
         dataset_id = 1  #
-        response = self.client.get(f'/api/data-filter?dataset={dataset_id}')
+        response = self.client.get(f'/api/data-filter/{dataset_id}')
         assert response.status_code == 200
         assert len(response.json()) == 6
 
         # log out to check if it still works
         self.client.logout()
-        # all filters
-        response = self.client.get('/api/data-filter')
+        response = self.client.get(f'/api/data-filter/{dataset_id}')
         assert response.status_code == 200
-        # currently we have 23 filters, for some reason we don't have a filter with id = 8
-        assert len(response.json()) == 23
+        assert len(response.json()) == 6
 
     def test_data_parameterized_filters(self):
         # here I need a validation to check if there are actually parameterised filters
@@ -46,24 +52,12 @@ class TestDataFilterView(TestCase):
         run_id = run.id
         val.run_validation(run_id)
         new_run = ValidationRun.objects.get(pk=run_id)
-        new_run_ref_config = DatasetConfiguration.objects.get(pk=new_run.reference_configuration_id)
 
         # all filters
         response = self.client.get('/api/param-filter')
         assert response.status_code == 200
-        assert len(
-            response.json()) == 2  # there will be only 2, as there has been only one validation run with 2 param filters applied
-
-        # filters assigned to the particular config
-        response = self.client.get(f'/api/param-filter?config={new_run_ref_config.id}')
-        assert response.status_code == 200
-        assert len(response.json()) == 2  # there will be only 2, as there are 2 assigned to this particular config
-        assert response.json()[0]['parameters'] == 'SCAN'
-        assert response.json()[1]['parameters'] == '0.0,0.1'
-
-        # checking for non-existing config
-        response = self.client.get(f'/api/param-filter?config={100}')
-        assert response.status_code == 404
+        # there will be only 2, as there has been only one validation run with 2 param filters applied
+        assert len(response.json()) == 2
 
         # now I log out the user to check if I can still get the data (assertion as above)
         self.client.logout()
