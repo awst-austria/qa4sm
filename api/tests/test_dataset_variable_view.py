1dc2cadedccac1ecf2dae644b57c0a219fec0591
sheenaze <monika.tercjak@gmail.com>
2021-08-20 12:38:20 +0200

    unnecessary code removed

--- api/tests/test_dataset_variable_view.py
+++ api/tests/test_dataset_variable_view.py
@@ -7,8 +7,6 @@ from api.tests.test_helper import *
 
 class TestDatasetVariableView(TestCase):
     __logger = logging.getLogger(__name__)
-    databases = '__all__'
-    allow_database_queries = True
     fixtures = ['datasets', 'filters', 'versions', 'variables']
 
     def setUp(self):
