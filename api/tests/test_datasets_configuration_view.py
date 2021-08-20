cc30c6c6b48e3abde786768cc57d0383f9a6e779
sheenaze <monika.tercjak@gmail.com>
2021-08-20 14:26:25 +0200

    address replaced with reverse function

--- api/tests/test_datasets_configuration_view.py
+++ api/tests/test_datasets_configuration_view.py
@@ -1,6 +1,8 @@
 import logging
 
 from django.test.utils import override_settings
+from django.urls import reverse
+
 import validator.validation as val
 from django.test import TestCase
 from rest_framework.test import APIClient
@@ -27,26 +29,27 @@ class TestDatasetConfigurationView(TestCase):
         run_id = run.id
         val.run_validation(run_id)
 
+        dataset_configuration_url = reverse('Configuration')
         # dataset_configuration view
-        response = self.client.get('/api/dataset-configuration')
+        response = self.client.get(dataset_configuration_url)
         assert response.status_code == 200
         assert len(
             response.json()) == 2  # there should be 2, because there was only one validation with 2 datasets used
 
         # dataset_configuration_by_dataset
-        response = self.client.get(f'/api/dataset-configuration/{run_id}')
+        response = self.client.get(f'{dataset_configuration_url}/{run_id}')
         assert response.status_code == 200
         assert len(response.json()) == 2  # there should be 2, there are 2 datasets in this validation
 
         # non-existing validation
         wrong_id = 'f0000000-a000-b000-c000-d00000000000'
-        response = self.client.get(f'/api/dataset-configuration/{wrong_id}')
+        response = self.client.get(f'{dataset_configuration_url}/{wrong_id}')
         assert response.status_code == 200
         assert len(response.json()) == 0
 
         # now I log out the user to check if I can still get the data (assertion as above)
         self.client.logout()
-        response = self.client.get('/api/dataset-configuration')
+        response = self.client.get(dataset_configuration_url)
         assert response.status_code == 200
         assert len(response.json()) == 2
 
