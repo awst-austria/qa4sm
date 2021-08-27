b0049866a882c6ac263f907ec0113dc62c67826b
sheenaze <monika.tercjak@gmail.com>
2021-08-27 15:20:42 +0200

    some not used imports removed

--- api/views/modify_validation_view.py
+++ api/views/modify_validation_view.py
@@ -6,11 +6,8 @@ from validator.forms import PublishingForm
 from validator.models import ValidationRun, CopiedValidations
 from validator.validation.validation import stop_running_validation
 from django.http.response import HttpResponse, JsonResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 from rest_framework import status
-
-# from validator.views.results import _copy_validationrun
-from api.views.validation_run_view import _copy_validationrun
 
 
 @api_view(['DELETE'])
