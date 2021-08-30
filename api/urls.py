efb32c52a1c249d91ad26d9fe76b3354ee32203f
sheenaze <monika.tercjak@gmail.com>
2021-08-30 13:00:27 +0200

    urls updated

--- api/urls.py
+++ api/urls.py
@@ -14,13 +14,13 @@ from api.views.validation_config_view import start_validation, get_validation_co
 from api.views.uptime_view import uptime_ping, get_uptime
 from api.views.user_view import signup_post, user_update, user_delete, users
 from api.views.validation_run_view import published_results, my_results, validation_run_by_id, \
-    custom_tracked_validation_runs, get_summary_statistics, get_publishing_form, copy_validation_results
+    custom_tracked_validation_runs
 from api.views.dataset_configuration_view import dataset_configuration, dataset_configuration_by_dataset
 from api.views.global_params_view import global_params
 from api.views.modify_validation_view import stop_validation, delete_result, change_name, archive_result, \
-    extend_result, publish_result, add_validation, remove_validation
+    extend_result, publish_result, add_validation, remove_validation, get_publishing_form, copy_validation_results
 from api.views.serving_file_view import get_results, get_csv_with_statistics, get_graphic_files, \
-    get_metric_names_and_associated_files, get_graphic_file
+    get_metric_names_and_associated_files, get_graphic_file, get_summary_statistics
 from api.views.local_api_view import get_list_of_countries
 from api.views.settings_view import settings
 
@@ -52,7 +52,7 @@ urlpatterns = [
     url(r'^test$', users), # are we using this one somehow?
     url(r'^path_test/(?P<username>.+)$', path_var_get),
     path('auth/login', api_login, name='api-login'), # checked
-    path('auth/logout', api_logout, name='api-logout'),
+    path('auth/logout', api_logout, name='api-logout'), # checked
     path('dataset', dataset, name='Datasets'),  # checked
     url(r'^dataset/(?P<id>.+)$', dataset_by_id),  # checked
     path('dataset-version', dataset_version, name='Dataset version'),  # checked
@@ -61,14 +61,14 @@ urlpatterns = [
     path('dataset-variable', dataset_variable, name='Dataset variable'), # checked
     url(r'^dataset-variable/(?P<variable_id>.+)$', dataset_variable_by_id), # checked
     url(r'^dataset-variable-by-dataset/(?P<dataset_id>.+)$', dataset_variable_by_dataset, name='Dataset_variable_by_dataset'),
-    path('published-results', published_results, name='Published results'),
-    url(r'^validation-runs/(?P<id>.+)$', validation_run_by_id),
+    path('published-results', published_results, name='Published results'),# checked
+    url(r'^validation-runs/(?P<id>.+)$', validation_run_by_id, name='Validation run by id'),
     path('dataset-configuration', dataset_configuration, name='Configuration'), # checked
     url(r'^dataset-configuration/(?P<dataset_id>.+)$', dataset_configuration_by_dataset, name='Configuration'), # checked
     path('data-filter', data_filter, name='Dataset filters'),  # checked
     url(r'^data-filter/(?P<dataset_id>.+)$', data_filter_by_dataset), # checked
     path('globals', global_params, name='Global context'), # checked
-    path('my-results', my_results, name='My results'),
+    path('my-results', my_results, name='My results'), # checked
     url(r'^validation-configuration/(?P<id>.+)$', get_validation_configuration),
     path('validation-configuration', start_validation, name='Run new validation'), # checked
     path('param-filter', data_parameterised_filter, name='Parameterised filter'),  # checked
@@ -80,9 +80,9 @@ urlpatterns = [
     path('publish-result/<uuid:result_uuid>/', publish_result, name='Publish result'), # checked
     path('add-validation/<uuid:result_uuid>/', add_validation, name='Add validation'), # checked
     path('remove-validation/<uuid:result_uuid>/', remove_validation, name='Remove validation'), # checked
-    path('custom-tracked-run', custom_tracked_validation_runs, name='Copied custom run'),
+    path('custom-tracked-run', custom_tracked_validation_runs, name='Tracked custom run'),
     path('download-result', get_results, name='Download results'), # checked
-    path('summary-statistics', get_summary_statistics, name='Summary statistics'),
+    path('summary-statistics', get_summary_statistics, name='Summary statistics'), # checked
     path('download-statistics-csv', get_csv_with_statistics, name='Download statistics csv'),
     path('uptime-ping', uptime_ping),
     path('uptime-report', get_uptime),
