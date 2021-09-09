4cb2a9ba33cbba78fc0e937e836373ad12ff3d8e
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2021-09-09 14:47:14 +0200

    environment updated; along with the ascat reader (#282)

--- validator/validation/readers.py
+++ validator/validation/readers.py
@@ -1,6 +1,6 @@
 from os import path
 
-from ascat import AscatNc
+from ascat.read_native.cdr import AscatGriddedNcTs
 from c3s_sm.interface import C3STs as c3s_read
 from ecmwf_models.interface import ERATs
 from esa_cci_sm.interface import CCITs
@@ -42,7 +42,7 @@ def create_reader(dataset, version):
         ascat_data_folder = path.join(folder_name, 'data')
         ascat_grid_path = first_file_in(path.join(folder_name, 'grid'), '.nc')
         fn_format = "{:04d}"
-        reader = AscatNc(path=ascat_data_folder, fn_format=fn_format, grid_filename=ascat_grid_path,
+        reader = AscatGriddedNcTs(path=ascat_data_folder, fn_format=fn_format, grid_filename=ascat_grid_path,
                          static_layer_path=None, ioclass_kws={'read_bulk':True})
 
     if dataset.short_name == globals.SMOS:
