4c74a1ffffa14c3bd91e260ed2a8daf4551f35d7
sheenaze <monika.tercjak@gmail.com>
2021-10-07 17:39:23 +0200

    handling http requests added

--- UI/src/app/modules/core/services/auth/auth.service.spec.ts
+++ UI/src/app/modules/core/services/auth/auth.service.spec.ts
@@ -1,13 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpClient} from '@angular/common/http';
+import {TestBed} from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import {AuthService} from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule]});
     service = TestBed.inject(AuthService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
