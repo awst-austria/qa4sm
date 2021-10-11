70ce98b2572f66761bfe748e60770d57fae25603
sheenaze <monika.tercjak@gmail.com>
2021-10-11 09:53:17 +0200

    default test of dataset-info.component passes

--- UI/src/app/pages/dataset-info/dataset-info.component.spec.ts
+++ UI/src/app/pages/dataset-info/dataset-info.component.spec.ts
@@ -1,14 +1,29 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {DatasetInfoComponent} from './dataset-info.component';
+import {DatasetService} from '../../modules/core/services/dataset/dataset.service';
+import {DatasetVersionService} from '../../modules/core/services/dataset/dataset-version.service';
+import {FilterService} from '../../modules/core/services/filter/filter.service';
 
 describe('DatasetInfoComponent', () => {
   let component: DatasetInfoComponent;
   let fixture: ComponentFixture<DatasetInfoComponent>;
+  let getAllDatasetsSpy: jasmine.Spy;
+  let getAllDatasetVersionsSpy: jasmine.Spy;
+  let getAllFiltersSpy: jasmine.Spy;
 
   beforeEach(async () => {
+    const datasetService = jasmine.createSpyObj('DatasetService', ['getAllDatasets']);
+    const datasetVersionService = jasmine.createSpyObj('DatasetVersionService', ['getAllVersions']);
+    const filterService = jasmine.createSpyObj('FilterService', ['getAllFilters']);
+
     await TestBed.configureTestingModule({
-      declarations: [ DatasetInfoComponent ]
+      declarations: [ DatasetInfoComponent ],
+      providers: [
+        {provide: DatasetService, useValue: datasetService},
+        {provide: DatasetVersionService, useValue: datasetVersionService},
+        {provide: FilterService, useValue: filterService}
+      ],
     })
     .compileComponents();
   });
