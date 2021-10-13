b50711b6e73dcc557091ee3a8ec85777ac424643
sheenaze <monika.tercjak@gmail.com>
2021-10-13 10:38:24 +0200

    home component tests developed

--- UI/src/app/pages/home/home.component.spec.ts
+++ UI/src/app/pages/home/home.component.spec.ts
@@ -1,16 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import {HomeComponent} from './home.component';
+import {SettingsService} from '../../modules/core/services/global/settings.service';
+import {SettingsDto} from '../../modules/core/services/global/settings.dto';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  // let settingsService: SettingsService;
+  // let settingsServiceStub: Partial<SettingsService>;
+  let getAllSettingsSpy: jasmine.Spy;
+  let fakeSettings: SettingsDto;
+
   beforeEach(async () => {
+    const settingsService = jasmine.createSpyObj('SettingsService', ['getAllSettings']);
+    fakeSettings = {
+      id: 1,
+      maintenance_mode: false,
+      news: 'some news',
+    };
+
+
+    getAllSettingsSpy = settingsService.getAllSettings.and.returnValue(fakeSettings);
+
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [HomeComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
