2fbe6d9d25ecdb96eeccf29d136723ebaaa20b59
Zoltan Bakcsa <bakcsa@awst.at>
2021-10-24 22:30:42 +0200

    install script changes

--- docker/webapp/install.sh
+++ docker/webapp/install.sh
@@ -14,10 +14,10 @@ wget -P /tmp/ https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64
 bash /tmp/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda && ln -s /opt/miniconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && rm /tmp/Miniconda3-latest-Linux-x86_64.sh
 rsync -r "$WEB_VAL_GIT_DIR/" "$APP_DIR/" --exclude="tests" --exclude="db.sqlite3"
 ls "$WEB_VAL_GIT_DIR/docker/"
-ls "$WEB_VAL_GIT_DIR/docker/qa4sm-webapp"
-cp "$WEB_VAL_GIT_DIR/docker/qa4sm-webapp/settings.py" "$APP_DIR/valentina/"
-cp "$WEB_VAL_GIT_DIR/docker/qa4sm-webapp/settings_conf.py" "$APP_DIR/valentina/"
-cp "$WEB_VAL_GIT_DIR/docker/qa4sm-webapp/apache.conf" /etc/apache2/sites-available/qa4sm-app.conf
+ls "$WEB_VAL_GIT_DIR/docker/webapp"
+cp "$WEB_VAL_GIT_DIR/docker/webapp/settings.py" "$APP_DIR/valentina/"
+cp "$WEB_VAL_GIT_DIR/docker/webapp/settings_conf.py" "$APP_DIR/valentina/"
+cp "$WEB_VAL_GIT_DIR/docker/webapp/apache.conf" /etc/apache2/sites-available/qa4sm-app.conf
 chown www-data:www-data -R "$APP_DIR"
 a2dissite 000-default
 a2ensite qa4sm-app
