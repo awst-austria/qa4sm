b30fc83871f9166425a3cad3bd7c2b98d0d63634
sheenaze <monika.tercjak@gmail.com>
2021-10-25 15:59:15 +0200

    dataset-configuration service tests work

--- UI/src/app/modules/validation-result/services/dataset-configuration.service.spec.ts
+++ UI/src/app/modules/validation-result/services/dataset-configuration.service.spec.ts
@@ -1,13 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { DatasetConfigurationService } from './dataset-configuration.service';
+import {DatasetConfigurationService} from './dataset-configuration.service';
+import {HttpClient} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('DatasetConfigurationService', () => {
   let service: DatasetConfigurationService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule]});
     service = TestBed.inject(DatasetConfigurationService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
