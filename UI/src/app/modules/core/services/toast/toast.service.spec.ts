9c2b517bc279c1b073ca9d5b36f65a6ddaad134c
sheenaze <monika.tercjak@gmail.com>
2021-10-25 16:09:38 +0200

    toast service tests work

--- UI/src/app/modules/core/services/toast/toast.service.spec.ts
+++ UI/src/app/modules/core/services/toast/toast.service.spec.ts
@@ -1,13 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ToastService } from './toast.service';
+import {ToastService} from './toast.service';
+import {MessageService} from 'primeng/api';
 
 describe('ToastService', () => {
   let service: ToastService;
+  let messageService: MessageService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ToastService);
+    messageService = TestBed.inject(MessageService);
   });
 
   it('should be created', () => {
