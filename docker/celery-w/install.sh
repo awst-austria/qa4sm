ea1a35a9f2c6f241e08fd8afdd0e8010de86d523
Zoltan Bakcsa <bakcsa@awst.at>
2021-10-27 18:56:37 +0200

    install script update

--- docker/celery-w/install.sh
+++ docker/celery-w/install.sh
@@ -4,6 +4,4 @@ set -x
 WEB_VAL_GIT_DIR="/tmp/qa4sm-git"
 APP_DIR="/var/lib/qa4sm-web-val/valentina"
 mkdir -p "$APP_DIR"
-
 rsync -r "$WEB_VAL_GIT_DIR/" "$APP_DIR/" --exclude="tests" --exclude="db.sqlite3"
-/opt/miniconda/condabin/conda env create --prefix=/var/lib/qa4sm-web-val/virtenv -f "$WEB_VAL_GIT_DIR/environment/qa4sm_env.yml"
