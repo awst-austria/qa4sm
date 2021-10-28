a609d699d43e4ff31dbc9811a959db53070c147e
sheenaze <monika.tercjak@gmail.com>
2021-10-28 08:58:03 +0200

    services mocked

--- UI/src/app/modules/validation-result/components/publishing/publishing.component.spec.ts
+++ UI/src/app/modules/validation-result/components/publishing/publishing.component.spec.ts
@@ -1,14 +1,27 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PublishingComponent} from './publishing.component';
+import {ModalWindowService} from '../../../core/services/global/modal-window.service';
+import {ValidationrunService} from '../../../core/services/validation-run/validationrun.service';
+import {FormBuilder} from '@angular/forms';
 
 describe('PublishingComponent', () => {
   let component: PublishingComponent;
   let fixture: ComponentFixture<PublishingComponent>;
 
   beforeEach(async () => {
+    const modalServiceSpy = jasmine.createSpyObj('ModalWindowService', ['watch', 'close', 'open']);
+    const validationrunServiceSpy = jasmine.createSpyObj('ValidationrunService',
+      ['publishResults', 'getPublishingFormData']);
+    const formBuilderSpy = jasmine.createSpyObj('FormBuilder', ['group']);
+
     await TestBed.configureTestingModule({
-      declarations: [ PublishingComponent ]
+      declarations: [ PublishingComponent ],
+      providers: [
+        {provide: ModalWindowService, useValue: modalServiceSpy},
+        {provide: FormBuilder, useValue: formBuilderSpy},
+        {provide: ValidationrunService, useValue: validationrunServiceSpy}
+      ]
     })
     .compileComponents();
   });
@@ -16,6 +29,7 @@ describe('PublishingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PublishingComponent);
     component = fixture.componentInstance;
+    component.validationId = 'f0000000-a000-b000-c000-d00000000000';
     fixture.detectChanges();
   });
 
