af937ac15ae0992e62fd0328a6d7235621e0d751
Zoltan Bakcsa <bakcsa@awst.at>
2021-11-06 00:16:36 +0100

    celery start script update

--- docker/celery-w/entrypoint.sh
+++ docker/celery-w/entrypoint.sh
@@ -12,11 +12,8 @@ if [ ! -d "run_celery" ]; then
 fi
 
 cd $APP_DIR
-MKL_NUM_THREADS=1
-OMP_NUM_THREADS=1
-MKL_DYNAMIC=FALSE
-OPENBLAS_NUM_THREADS=1
+
 if [ ! -d "/var/log/valentina" ]; then
   mkdir /var/log/valentina
 fi
-celery -A valentina worker --max-tasks-per-child=1 --concurrency=$QA4SM_CELERY_WORKERS -l INFO --time-limit=16000 --prefetch-multiplier=1
+MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 MKL_DYNAMIC=FALSE OPENBLAS_NUM_THREADS=1 celery -A valentina worker --max-tasks-per-child=1 --concurrency=$QA4SM_CELERY_WORKERS -l INFO --time-limit=16000 --prefetch-multiplier=1
