fd0dfb201776168467a26f1c93e949103a73f01d
Zoltan Bakcsa <bakcsa@awst.at>
2021-12-10 22:11:40 +0100

    update compose file

--- UI/scripts/build_run.sh
+++ UI/scripts/build_run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 set -e
-npm install
-ng build --prod --deploy-url /ui/ --base-href /ui/
-cp -r ../dist/UI ../docker/ui
-docker stop qa4sm_ui
-docker rm qa4sm_ui --force
-docker image rm qa4sm_ui
-docker build -t qa4sm_ui ../docker
 docker run --name qa4sm_ui --restart unless-stopped -d -p 8013:80 qa4sm_ui
