a55cb863d82f5c9f1e53dd7ab3665753e6524755
sheenaze <monika.tercjak@gmail.com>
2022-02-08 17:22:55 +0100

    django and ui backends replaced with each other; default path for angular changed

--- UI/compile.sh
+++ UI/compile.sh
@@ -3,5 +3,5 @@ set -e
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 npm install
-ng build --prod --deploy-url /ui/ --base-href /ui/
+ng build --prod --deploy-url / --base-href /
 cp -r dist/UI docker/ui
