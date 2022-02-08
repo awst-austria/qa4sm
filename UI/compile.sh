6123680d082042da54bd2592b3a5d0c3103c4f70
sheenaze <monika.tercjak@gmail.com>
2022-02-08 18:05:22 +0100

    old settings retrieved

--- UI/compile.sh
+++ UI/compile.sh
@@ -3,5 +3,5 @@ set -e
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 npm install
-ng build --prod --deploy-url / --base-href /
+ng build --prod --deploy-url /ui/ --base-href /ui/
 cp -r dist/UI docker/ui
