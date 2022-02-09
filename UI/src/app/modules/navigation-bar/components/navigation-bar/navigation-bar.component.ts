39e86d1440b1e935f27dc5f881067d50138d3701
sheenaze <monika.tercjak@gmail.com>
2022-02-09 14:07:27 +0100

    some code refinement done

--- UI/src/app/modules/navigation-bar/components/navigation-bar/navigation-bar.component.ts
+++ UI/src/app/modules/navigation-bar/components/navigation-bar/navigation-bar.component.ts
@@ -17,19 +17,39 @@ export class NavigationBarComponent implements OnInit {
   userProfileMenuItem: MenuItem;
 
   constructor(private authService: AuthService, private router: Router, private toastService: ToastService) {
-    this.loginMenuItem = {label: 'Log in', icon: 'pi pi-fw pi-sign-in', routerLink: ['login'], command: event => this.setPreviousUrl('user-profile')};
+    this.loginMenuItem = {
+      label: 'Log in',
+      icon: 'pi pi-fw pi-sign-in',
+      routerLink: ['login'],
+      command: event => this.setPreviousUrl('user-profile')
+    };
     this.userProfileMenuItem = {label: 'User profile', icon: 'pi pi-fw pi-user', routerLink: ['user-profile']};
     this.logoutMenuItem = {label: 'Log out', icon: 'pi pi-fw pi-sign-out', command: event => this.logout()};
     this.items = [
       {label: 'Home', icon: 'pi pi-fw pi-home', routerLink: [''], command: event => this.setPreviousUrl('')},
-      {label: 'Validate', icon: 'pi pi-fw pi-check-square', routerLink: ['validate'], command: event => this.setPreviousUrl('validate')},
-      {label: 'My validations', icon: 'pi pi-fw pi-folder', routerLink: ['my-validations'], command: event => this.setPreviousUrl('my-validations')},
+      {
+        label: 'Validate',
+        icon: 'pi pi-fw pi-check-square',
+        routerLink: ['validate'],
+        command: event => this.setPreviousUrl('validate')
+      },
+      {
+        label: 'My validations',
+        icon: 'pi pi-fw pi-folder',
+        routerLink: ['my-validations'],
+        command: event => this.setPreviousUrl('my-validations')
+      },
       {label: 'Published validations', icon: 'pi pi-fw pi-globe', routerLink: ['published-validations']},
       {
         label: 'Info', icon: 'pi pi-fw pi-info-circle', items: [
           {label: 'About', icon: 'pi pi-fw pi-info', routerLink: ['about']},
           {label: 'Help', icon: 'pi pi-fw pi-question', routerLink: ['help']},
-          {label: 'User Manual', icon: 'pi pi-fw pi-book', url: 'https://www.geo.tuwien.ac.at/media/filer_public/15/6c/156cd163-cd29-4dd3-a132-820bcb653817/frm4sm_dt3-1_qa4sm_sum_v11.pdf', target: '_blank'},
+          {
+            label: 'User Manual',
+            icon: 'pi pi-fw pi-book',
+            url: 'https://www.geo.tuwien.ac.at/media/filer_public/15/6c/156cd163-cd29-4dd3-a132-820bcb653817/frm4sm_dt3-1_qa4sm_sum_v11.pdf',
+            target: '_blank'
+          },
           {label: 'Datasets', icon: 'pi pi-fw pi-save', routerLink: ['datasets']},
           {label: 'Terms', icon: 'pi pi-fw pi-briefcase', routerLink: ['terms']},
 
@@ -49,23 +69,23 @@ export class NavigationBarComponent implements OnInit {
     this.authService.authenticated.subscribe(authenticated => this.authStatusChanged(authenticated));
   }
 
-  private authStatusChanged(authenticated: boolean) {
+  private authStatusChanged(authenticated: boolean): void {
     this.logoutMenuItem.disabled = !authenticated;
     this.loginMenuItem.disabled = authenticated;
     this.userProfileMenuItem.disabled = !authenticated;
   }
 
-  logout() {
+  logout(): void {
     this.authService.logout().subscribe(result => {
-      this.setPreviousUrl('');
-        if (result) {//Successful logout
+        this.setPreviousUrl('');
+        if (result) {// Successful logout
           this.router.navigate(['home']).then(value => this.toastService.showSuccessWithHeader('Logout', 'Successful logout'));
         }
       }
     );
   }
 
-  setPreviousUrl(prevUrl: string): void{
+  setPreviousUrl(prevUrl: string): void {
     this.authService.setPreviousUrl(prevUrl);
   }
 
