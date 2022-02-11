d9dcb8755863c78eb4787a26e047c39775c835d3
sheenaze <monika.tercjak@gmail.com>
2022-02-11 13:30:35 +0100

    testing docker settings

--- UI/scripts/build.sh
+++ UI/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 npm install
-ng build --prod --deploy-url /ui/ --base-href /ui/
+ng build --prod --deploy-url / --base-href /
 cp -r ../dist/UI ../docker/ui
 docker build -t qa4sm_ui ../docker
