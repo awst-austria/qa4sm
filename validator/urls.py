719be1f37bae38c952caaab15c98d0e2cc2eac45
sheenaze <monika.tercjak@gmail.com>
2022-02-16 15:16:37 +0100

    not used import removed

--- validator/urls.py
+++ validator/urls.py
@@ -1,10 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path
-
-from . import views
 
 
 urlpatterns = [
