3dd254cc14b21c5250a862e8e36c3915ee11eeae
sheenaze <monika.tercjak@gmail.com>
2022-03-16 14:49:30 +0100

    Alert with header added

--- UI/src/app/modules/core/services/toast/toast.service.ts
+++ UI/src/app/modules/core/services/toast/toast.service.ts
@@ -10,22 +10,25 @@ export class ToastService {
   constructor(private messageService: MessageService) {
   }
 
-  showSuccess(message: string) {
+  showSuccess(message: string): void{
     this.messageService.add({key: 'global-toast', severity: 'success', detail: message});
   }
 
-  showSuccessWithHeader(header: string, message: string) {
+  showSuccessWithHeader(header: string, message: string): void {
     this.messageService.add({key: 'global-toast', severity: 'success', summary: header, detail: message});
   }
 
-  showError(message: string) {
+  showError(message: string): void {
     this.messageService.add({key: 'global-toast', severity: 'error', detail: message});
   }
 
-  showErrorWithHeader(header: string, message: string) {
+  showErrorWithHeader(header: string, message: string): void {
     this.messageService.add({key: 'global-toast', severity: 'error', summary: header, detail: message});
   }
   showAlert(message: string): void{
     this.messageService.add({key: 'global-toast', severity: 'warn', detail: message});
+  }
+  showAlertWithHeader(header: string, message: string, life = 5000): void{
+    this.messageService.add({key: 'global-toast', severity: 'warn', summary: header, detail: message, life});
   }
 }
