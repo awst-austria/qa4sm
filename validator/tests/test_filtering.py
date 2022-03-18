5f134d89f6c6927f54ae19e67b0fe47c4df9f86d
Pietro Stradiotti <pietro.stradiotti@tuwien.ac.at>
2022-03-18 14:20:38 +0100

    Fixed tests filters

--- validator/tests/test_filtering.py
+++ validator/tests/test_filtering.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test.utils import override_settings
+import django
 
 from validator.admin import User
 from validator.models import DataFilter, DataVariable
@@ -9,8 +10,6 @@ from validator.tests.auxiliary_functions import generate_default_validation_smos
 
 from validator.validation.filters import setup_filtering, get_used_variables, check_normalized_bits_array
 from validator.validation.readers import create_reader
-
-import pytest
 
 
 @override_settings(CELERY_TASK_EAGER_PROPAGATES=True,
@@ -26,7 +25,7 @@ class TestValidation(TestCase):
             'username': 'testuser',
             'password': 'secret',
             'email': 'noreply@awst.at'
-            }
+        }
 
         try:
             self.testuser = User.objects.get(username=self.user_data['username'])
@@ -58,41 +57,52 @@ class TestValidation(TestCase):
         )
 
     def test_setup_filtering(self) -> None:
-        setup_filtering(
+        filtered_reader, read_name, read_kwargs = setup_filtering(
             self.smos_reader,
             self.smos_config.filters.all(),
             ParametrisedFilter.objects.filter(dataset_config_id=self.smos_config.id),
             self.smos_config.version,
             self.smos_config.variable,
         )
+        f_list_should = [
+            ('Rfi_Prob', '<=', 0.8),
+            ('Soil_Moisture', '>=', 0.0),
+            ('Soil_Moisture', '<=', 1.0),
+            ('Science_Flags', check_normalized_bits_array, [[20], [21], [22], [23]])
+        ]
+        assert filtered_reader.filter_list == f_list_should
 
+        assert read_name == 'read'
+        assert not read_kwargs
 
-tested_data = [
-    ("FIL_ISMN_GOOD", "ISMN_soil_moisture", "soil_moisture_flag"),
-    ("FIL_C3S_MODE_ASC", "C3S_sm", "flag"),
-    ("FIL_ASCAT_METOP_A", "ASCAT_sm", "sat_id"),
-    ("FIL_ERA5_TEMP_UNFROZEN", "ERA5_sm", "t"),
-    ("FIL_SMOSL3_STRONG_TOPO", "SMOSL3_sm", "Science_Flags"),
-    ("FIL_SMOSL3_RFI", "SMOSL3_sm", "Rfi_Prob"),
-]
-tested_filters = [
-    "ISMN_good", "C3S_mode", "ASCAT_satellite",
-    "ERA_frozen", "SMOSL3_flag", "SMOSL3_parametrised"
-]
+    def test_get_used_variables(self) -> None:
+        # provide a few filters to test
+        tested_data = [
+            ("FIL_ISMN_GOOD", "ISMN_soil_moisture", "soil_moisture_flag", DataFilter),
+            ("FIL_C3S_MODE_ASC", "C3S_sm", "mode", DataFilter),
+            ("FIL_ASCAT_METOP_A", "ASCAT_sm", "sat_id", DataFilter),
+            ("FIL_ERA5_TEMP_UNFROZEN", "ERA5_sm", "stl1", DataFilter),
+            ("FIL_SMOSL3_STRONG_TOPO", "SMOSL3_sm", "Science_Flags", DataFilter),
+            ("FIL_SMOSL3_RFI", "SMOSL3_sm", "Rfi_Prob", ParametrisedFilter),
+        ]
 
+        for filtername, sm_variable, filter_variable_should, model in tested_data:
+            try:
+                filters = [model.objects.get(name=filtername), ]
+            except django.core.exceptions.FieldError:
+                datafilter = DataFilter.objects.get(name=filtername)
+                filters = [model.objects.get(filter=datafilter), ]
 
-@pytest.mark.parametrize(
-    "filtername, sm_variable, filter_variable_should",
-    tested_data, ids=tested_filters
-)
-@pytest.mark.django_db
-def test_get_used_variables(filtername, sm_variable, filter_variable_should) -> None:
-    filters = [DataFilter.objects.get(name=filtername), ]
-    variable = DataVariable.objects.get(short_name=sm_variable)
-    used_variables = get_used_variables(
-        filters, dataset=None, variable=variable
-    )
-    print(used_variables)
+            variable = DataVariable.objects.get(short_name=sm_variable)
+            used_variables = get_used_variables(
+                filters, dataset=None, variable=variable
+            )
+            used_variables_should = [variable.pretty_name, filter_variable_should]
 
-def test_check_normalized_bits_array() -> None:
-    pass
+            assert used_variables == used_variables_should
+
+# bits_tests = [
+#     ('', [[3]], ),
+# ]
+# def test_check_normalized_bits_array() -> None:
+#     pass
