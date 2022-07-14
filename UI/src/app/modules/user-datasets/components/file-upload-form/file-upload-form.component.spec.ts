bfe28b5d6690eb564d0beb41b591153f4a29ca0c
sheenaze <tercjak@awst.at>
2022-07-14 16:41:53 +0200

    file-upload-form component added

---
+++ UI/src/app/modules/user-datasets/components/file-upload-form/file-upload-form.component.spec.ts
@@ -0,0 +1,25 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {FileUploadFormComponent} from './file-upload-form.component';
+
+describe('FileUploadFormComponent', () => {
+  let component: FileUploadFormComponent;
+  let fixture: ComponentFixture<FileUploadFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FileUploadFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FileUploadFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ UI/src/app/modules/user-datasets/components/file-upload-form/file-upload-form.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'qa-file-upload-form',
+  templateUrl: './file-upload-form.component.html',
+  styleUrls: ['./file-upload-form.component.scss']
+})
+export class FileUploadFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
