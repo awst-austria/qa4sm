f35e5c3b38a1a964ed6efd7809b104167a49f722
sheenaze <tercjak@awst.at>
2022-08-18 10:52:51 +0200

    caching updated to remember if also user datasets should be taken or not

--- UI/src/app/modules/core/services/dataset/dataset.service.ts
+++ UI/src/app/modules/core/services/dataset/dataset.service.ts
@@ -8,27 +8,31 @@ import {DataCache} from '../../tools/DataCache';
 
 const datasetUrl: string = environment.API_URL + 'api/dataset';
 const CACHE_KEY_ALL_DATASETS = -1;
+const CACHE_USER_DATA_INFO = -2;
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatasetService {
 
-  //cache for dataset arrays
+  // cache for dataset arrays
   arrayRequestCache = new DataCache<Observable<DatasetDto[]>>(5);
-  //cache for single dataset dtos
+  // cache for single dataset dtos
   singleRequestCache = new DataCache<Observable<DatasetDto>>(5);
+  // cache for information if user data should be also fetched
+  userDataInfoCache = new DataCache<boolean>(5);
 
   constructor(private httpClient: HttpClient) {
   }
 
   getAllDatasets(userData = false): Observable<DatasetDto[]> {
-    if (this.arrayRequestCache.isCached(CACHE_KEY_ALL_DATASETS)) {
+    if (this.arrayRequestCache.isCached(CACHE_KEY_ALL_DATASETS) && this.userDataInfoCache.get(CACHE_USER_DATA_INFO) === userData) {
       return this.arrayRequestCache.get(CACHE_KEY_ALL_DATASETS);
     } else {
       const params = new HttpParams().set('userData', String(userData));
       const datasets$ = this.httpClient.get<DatasetDto[]>(datasetUrl, {params}).pipe(shareReplay());
       this.arrayRequestCache.push(CACHE_KEY_ALL_DATASETS, datasets$);
+      this.userDataInfoCache.push(CACHE_USER_DATA_INFO, userData);
       return datasets$;
     }
   }
