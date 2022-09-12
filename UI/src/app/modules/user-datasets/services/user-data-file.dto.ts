ead9970a04bc8e265d0cc051d991c39ddc5721af
sheenaze <tercjak@awst.at>
2022-09-12 17:31:37 +0200

    UserDataFileDto updated according to the model changes

--- UI/src/app/modules/user-datasets/services/user-data-file.dto.ts
+++ UI/src/app/modules/user-datasets/services/user-data-file.dto.ts
@@ -5,6 +5,14 @@ export class UserDataFileDto {
               public owner: number,
               public dataset: number,
               public version: number,
-              public variable: number) {
+              public variable: number,
+              public latname: string,
+              public lonname: string,
+              public timename: string,
+              public variable_choices: [{variable: string, standard_name: string, long_name: string}],
+              public lon_name_choices: [{name: string}],
+              public lat_name_choices: [{name: string}],
+              public time_name_choices: [{name: string}],
+              ) {
   }
 }
