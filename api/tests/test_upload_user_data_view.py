70d346c21a01322729d8ee4324e7f1d552ab18e1
sheenaze <tercjak@awst.at>
2022-10-17 15:12:38 +0200

    testing upload user data views improved

--- api/tests/test_upload_user_data_view.py
+++ api/tests/test_upload_user_data_view.py
@@ -1,12 +1,19 @@
 import logging
-from django.test import TestCase
-from django.utils import timezone
-
 from api.tests.test_helper import *
-from rest_framework.test import APIClient, APIRequestFactory, APITestCase
+from rest_framework.test import APIClient, APITestCase
 from django.urls import reverse
-from validator.models import UserDatasetFile, DatasetVersion, DataVariable, Dataset
+from validator.models import UserDatasetFile, DataVariable
 from django.conf import settings
+import shutil
+from pathlib import Path
+
+def _create_test_file(path):
+    test_file = open(path, 'w')
+    test_file.write('some test content of a no netcdf file\n')
+    test_file.close()
+    test_file = open(path, 'rb')
+    return test_file
+
 
 class TestUploadUserDataView(APITestCase):
     __logger = logging.getLogger(__name__)
@@ -18,17 +25,22 @@ class TestUploadUserDataView(APITestCase):
 
         self.client = APIClient()
         self.client.login(**self.auth_data)
-
-        self.file_name = 'teststack_c3s_2dcoords_min_attrs.nc'
         self.user_data_path = os.path.join(settings.BASE_DIR, 'testdata/user_data')
-        self.file = f'{self.user_data_path}/{self.file_name}'
-        # self.file = f'/testdata/user_data/{self.file_name}'
+        self.test_user_data_path = f'{self.user_data_path}/{self.test_user.username}'
+        Path(self.test_user_data_path).mkdir(exist_ok=True, parents=True)
+
+        self.netcdf_file_name = 'teststack_c3s_2dcoords_min_attrs.nc'
+        self.not_netcdf_file_name = 'test_file.txt'
+
+        self.netcdf_file = f'{self.user_data_path}/{self.netcdf_file_name}'
+        self.not_netcdf_file = f'{self.user_data_path}/{self.not_netcdf_file_name}'
+
         self.upload_data_url_name = 'Upload user data'
         self.post_metadata_url_name = 'Post User Data File Metadata'
 
-    def test_upload_user_data(self):
-        response = self.client.post(reverse(self.upload_data_url_name, kwargs={'filename': self.file_name}),
-                                    {'file': self.file}, format='multipart')
+    def test_upload_user_data_correct(self):
+        response = self.client.post(reverse(self.upload_data_url_name, kwargs={'filename': self.netcdf_file_name}),
+                                    {'file': self.netcdf_file}, format='multipart')
 
         assert response.status_code == 200
 
@@ -36,24 +48,66 @@ class TestUploadUserDataView(APITestCase):
 
         assert len(existing_files) == 1
         file_entry = existing_files[0]
-        assert file_entry.dataset is None  # metadata hasn't been posted yet
-        assert file_entry.version is None  # metadata hasn't been posted yet
-        assert file_entry.variable is None  # metadata hasn't been posted yet
-        assert file_entry.lon_name is None  # metadata hasn't been posted yet
-        assert file_entry.lat_name is None  # metadata hasn't been posted yet
-        assert file_entry.time_name is None  # metadata hasn't been posted yet
+        file_dir = file_entry.get_raw_file_path
+
+        assert len(os.listdir(self.test_user_data_path)) == 1
+        assert os.path.exists(file_dir)
+
+        # metadata hasn't been posted yet, so most of the fields should be empty
+        assert file_entry.dataset is None
+        assert file_entry.version is None
+        assert file_entry.variable is None
+        assert file_entry.lon_name is None
+        assert file_entry.lat_name is None
+        assert file_entry.time_name is None
         assert file_entry.owner == self.test_user
         file_entry.delete()
 
+        # check if the entry got removed along with the entire file directory
+        existing_files = UserDatasetFile.objects.all()
+        assert len(existing_files) == 0
+        assert not os.path.exists(file_dir)
+        assert len(os.listdir(self.test_user_data_path)) == 0
+
+    def test_upload_user_data_not_netcdf(self):
+        file_to_upload = _create_test_file(self.not_netcdf_file)
+        response = self.client.post(reverse(self.upload_data_url_name, kwargs={'filename': self.not_netcdf_file_name}),
+                                    {'file': file_to_upload}, format='multipart')
+        # assert False
+        assert response.status_code == 500
+
+        # checking if nothing got to the db and to the data path
+        existing_files = UserDatasetFile.objects.all()
+        assert len(existing_files) == 0
+        assert len(os.listdir(self.test_user_data_path)) == 0
+
+    def test_upload_user_data_with_wrong_name(self):
+            file_to_upload = _create_test_file(self.not_netcdf_file)
+            response = self.client.post(
+                reverse(self.upload_data_url_name, kwargs={'filename': 'wrong_name'}),
+                {'file': file_to_upload}, format='multipart')
+            # assert False
+            assert response.status_code == 500
+
+            # checking if nothing got to the db and to the data path
+            existing_files = UserDatasetFile.objects.all()
+            assert len(existing_files) == 0
+            assert len(os.listdir(self.test_user_data_path)) == 0
+
     def test_post_user_file_metadata_and_preprocess_file(self):
-        # new file entry -> an existing proper file
-        file_entry = UserDatasetFile(file_name=self.file_name, owner=self.test_user, upload_date=timezone.now())
-        file_entry.file.name = self.file
-        file_entry.save()
+        # I am posting the file to create the proper dataset entry
+        response = self.client.post(reverse(self.upload_data_url_name, kwargs={'filename': self.netcdf_file_name}),
+                                    {'file': self.netcdf_file}, format='multipart')
+        assert response.status_code == 200
 
         # checking if the file entry got saved
         existing_files = UserDatasetFile.objects.all()
         assert len(existing_files) == 1
+
+        file_entry = existing_files[0]
+        # I need to replace the posted file with the original one, because the api post method somehow corrupts netCDFs
+        # which is not the problem when I post them via angular and I don't want to deal with it right now.
+        shutil.copy2(self.netcdf_file, file_entry.file.path)
 
         metadata_correct = {
             'dataset_name': 'test_dataset',
@@ -61,21 +115,30 @@ class TestUploadUserDataView(APITestCase):
             'version_name': 'test_version',
             'version_pretty_name': 'test_version_pretty_name'
         }
-
+        # posting metadata as those from the metadata form and checking if it has been done
         response_metadata = self.client.post(reverse(self.post_metadata_url_name, kwargs={'file_uuid': file_entry.id}),
                                              metadata_correct, format='json')
-
-        file_entry = UserDatasetFile.objects.get(id=file_entry.id)
         assert response_metadata.status_code == 200
+
+        # re-querying file entry
+        file_entry = UserDatasetFile.objects.get(id=response.json()['id'])
+        # checking if the posted metadata is proper
         assert file_entry.dataset.short_name == metadata_correct['dataset_name']
         assert file_entry.dataset.pretty_name == metadata_correct['dataset_pretty_name']
+        assert file_entry.dataset == Dataset.objects.all().last()
         assert file_entry.version.short_name == metadata_correct['version_name']
         assert file_entry.version.pretty_name == metadata_correct['version_pretty_name']
+        assert file_entry.version == DatasetVersion.objects.all().last()
+        # checking if the proper metadata was retrieved from the file
         assert file_entry.variable == DataVariable.objects.all().last()
         assert file_entry.variable.short_name == 'soil_moisture'
         assert file_entry.time_name == 'time'
         assert file_entry.lat_name == 'lat'
         assert file_entry.lon_name == 'lon'
 
+        # check if the timeseries files were created:
+        timeseries_dir = file_entry.get_raw_file_path + '/timeseries'
+        assert os.path.exists(timeseries_dir)
+        assert len(os.listdir(timeseries_dir)) != 0
+
         file_entry.delete()
-        # assert False
