d4e5d3255b3e0448a06b760edc9940d1ab07c115
sheenaze <tercjak@awst.at>
2022-11-08 11:58:10 +0100

    a file with fields and variable names added to make it easier to update it if needed and pass names between api and front-end and between e.g. views and tests

---
+++ api/variable_and_field_names.py
@@ -0,0 +1,16 @@
+# Here I define variable names used in the frontend and in api backend,
+# so there is no problem to pass variables between frontend and backend
+
+# User Data Uploading Form
+USER_DATA_FIELD_NAME = 'field_name'
+USER_DATA_FIELD_VALUE = 'field_value'
+USER_DATA_DATASET_FIELD_NAME = 'dataset_name'
+USER_DATA_DATASET_FIELD_PRETTY_NAME = 'dataset_pretty_name'
+USER_DATA_VERSION_FIELD_NAME = 'version_name'
+USER_DATA_VERSION_FIELD_PRETTY_NAME = 'version_pretty_name'
+USER_DATA_VARIABLE_FIELD_NAME = 'variable_name'
+USER_DATA_LAT_FIELD_NAME = 'lat_name'
+USER_DATA_LON_FIELD_NAME = 'lon_name'
+USER_DATA_TIME_FIELD_NAME = 'time_name'
+URL_FILE_UUID = 'file_uuid'
+URL_FILENAME = 'filename'
