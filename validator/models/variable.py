dc010338bc183079086898b6a87c809ada2f3f53
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2023-02-22 08:56:11 +0100

    [QA4SM-275] Add unit to fixtures, model and netcdf file  (#575)
    * Add unit to fixtures, model and netcdf file
    
    * Update test data manually

--- validator/models/variable.py
+++ validator/models/variable.py
@@ -4,6 +4,7 @@ class DataVariable(models.Model):
     id = models.AutoField(primary_key=True)
     short_name = models.CharField(max_length=30)
     pretty_name = models.CharField(max_length=100)
+    unit = models.CharField(max_length=20, default='unknown')
     help_text = models.CharField(max_length=150)
 
     min_value = models.FloatField(null=True)
