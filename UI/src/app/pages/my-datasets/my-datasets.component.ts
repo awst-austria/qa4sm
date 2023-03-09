57fb3487d62d90e4e1d29abb6724556f860e5117
sheenaze <tercjak@awst.at>
2023-03-09 09:13:28 +0100

    message on the storage limit added

--- UI/src/app/pages/my-datasets/my-datasets.component.ts
+++ UI/src/app/pages/my-datasets/my-datasets.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {UserDatasetsService} from '../../modules/user-datasets/services/user-datasets.service';
 import {Observable} from 'rxjs';
 import {UserDataFileDto} from '../../modules/user-datasets/services/user-data-file.dto';
+import {AuthService} from '../../modules/core/services/auth/auth.service';
 
 @Component({
   selector: 'qa-my-datasets',
@@ -10,9 +11,12 @@ import {UserDataFileDto} from '../../modules/user-datasets/services/user-data-fi
 })
 export class MyDatasetsComponent implements OnInit {
   // userDatasets = [];
-  constructor(private userDatasetService: UserDatasetsService) { }
+  constructor(private userDatasetService: UserDatasetsService,
+              public authService: AuthService) { }
   userDatasets$: Observable<UserDataFileDto[]>;
   readMore = false;
+  hasNoSpaceLimit: boolean;
+  hasNoSpaceAssigned: boolean;
 
   ngOnInit(): void {
     this.userDatasets$ = this.userDatasetService.getUserDataList();
@@ -21,10 +25,24 @@ export class MyDatasetsComponent implements OnInit {
         this.userDatasets$ = this.userDatasetService.getUserDataList();
       }
     });
+    this.hasNoSpaceLimit = !this.authService.currentUser.space_limit_value;
+    this.hasNoSpaceAssigned = this.authService.currentUser.space_limit_value === 1;
   }
 
   toggleReadMore(): void{
     this.readMore = !this.readMore;
   }
+
+  getLimitMessage(): string{
+    let message;
+    this.hasNoSpaceLimit ?
+      message = 'You have no space limit for your data.' :
+      message = `You can use up to ${this.authService.currentUser.space_limit_value / Math.pow(10, 9)}
+      GB space for your data.`;
+
+    return message;
+  }
+
+
 
 }
