295d1b6d325d21cd7eda086212ecfd50c38722d0
sheenaze <tercjak@awst.at>
2023-03-17 09:23:17 +0100

    cleaning redundant datasets added

--- api/views/auxiliary_functions.py
+++ api/views/auxiliary_functions.py
@@ -12,14 +12,14 @@ def get_fields_as_list(model):
 def clean_redundant_datasets(user):
     user_datasets_without_file = Dataset.objects.filter(user=user).filter(user_dataset__isnull=True)
     print(user_datasets_without_file)
-    # for dataset in user_datasets_without_file:
-    #     if len(dataset.user_dataset.all()) == 0:
-    #         versions = dataset.versions.all()
-    #         variables = dataset.variables.all()
-    #         dataset.versions.clear()
-    #         dataset.variables.clear()
-    #         dataset.delete()
-    #         for version in versions:
-    #             version.delete()
-    #         for variable in variables:
-    #             variable.delete()
+    for dataset in user_datasets_without_file:
+        if len(dataset.user_dataset.all()) == 0:
+            versions = dataset.versions.all()
+            variables = dataset.variables.all()
+            dataset.versions.clear()
+            dataset.variables.clear()
+            dataset.delete()
+            for version in versions:
+                version.delete()
+            for variable in variables:
+                variable.delete()
