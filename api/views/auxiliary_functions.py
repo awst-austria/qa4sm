aabd900279818c2e9562ff873c281ff4f8c8fb4f
sheenaze <tercjak@awst.at>
2023-03-17 09:52:47 +0100

    printing added

--- api/views/auxiliary_functions.py
+++ api/views/auxiliary_functions.py
@@ -1,5 +1,6 @@
 from validator.models import Dataset
 
+
 def get_fields_as_list(model):
     fields = []
     for field in model._meta.get_fields():
@@ -13,13 +14,14 @@ def clean_redundant_datasets(user):
     user_datasets_without_file = Dataset.objects.filter(user=user).filter(user_dataset__isnull=True)
     print(user_datasets_without_file)
     for dataset in user_datasets_without_file:
-        if len(dataset.user_dataset.all()) == 0:
-            versions = dataset.versions.all()
-            variables = dataset.variables.all()
-            dataset.versions.clear()
-            dataset.variables.clear()
-            dataset.delete()
-            for version in versions:
-                version.delete()
-            for variable in variables:
-                variable.delete()
+        print(dataset)
+        versions = dataset.versions.all()
+        variables = dataset.variables.all()
+        print(versions, variables)
+        dataset.versions.clear()
+        dataset.variables.clear()
+        dataset.delete()
+        for version in versions:
+            version.delete()
+        for variable in variables:
+            variable.delete()
