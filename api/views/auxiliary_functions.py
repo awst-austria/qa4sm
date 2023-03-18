736d0a7a9a6e980798b009c612abb15d152a1cdc
sheenaze <tercjak@awst.at>
2023-03-18 17:42:38 +0100

    saving metadata and preprocessing the file moved to the same view as preprocessing

--- api/views/auxiliary_functions.py
+++ api/views/auxiliary_functions.py
@@ -10,18 +10,20 @@ def get_fields_as_list(model):
     return fields
 
 
-def clean_redundant_datasets(user):
-    user_datasets_without_file = Dataset.objects.filter(user=user).filter(user_dataset__isnull=True)
-    print(user_datasets_without_file)
-    for dataset in user_datasets_without_file:
-        print(dataset)
-        versions = dataset.versions.all()
-        variables = dataset.variables.all()
-        print(versions, variables)
-        dataset.versions.clear()
-        dataset.variables.clear()
-        dataset.delete()
-        for version in versions:
-            version.delete()
-        for variable in variables:
-            variable.delete()
+# def clean_redundant_datasets(user):
+#     user_datasets_without_file = Dataset.objects.filter(user=user).filter(user_dataset__isnull=True)
+#     print(user_datasets_without_file)
+#     for dataset in user_datasets_without_file:
+#         print(dataset)
+#         versions = dataset.versions.all()
+#         variables = dataset.variables.all()
+#         dataset.versions.clear()
+#         dataset.variables.clear()
+#         dataset.delete()
+#         print(versions, variables)
+#         for version in versions:
+#             print('version: ', version)
+#             version.delete()
+#         for variable in variables:
+#             print('variable: ', variables)
+#             variable.delete()
