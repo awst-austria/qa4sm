6c747a6b50bfb759dba20ed9e55a7fbc3052c17b
sheenaze <tercjak@awst.at>
2023-06-15 13:42:56 +0200

    not needed comments removed

--- UI/src/app/modules/comparison/components/extent-visualization/extent-visualization.component.ts
+++ UI/src/app/modules/comparison/components/extent-visualization/extent-visualization.component.ts
@@ -7,8 +7,6 @@ import {ValidationrunService} from '../../../core/services/validation-run/valida
 import {DatasetConfigurationService} from '../../../validation-result/services/dataset-configuration.service';
 import {ComparisonService} from '../../services/comparison.service';
 import {WebsiteGraphicsService} from '../../../core/services/global/website-graphics.service';
-// import {CarouselComponent} from 'angular-gallery/lib/carousel.component.d';
-// import {Gallery} from 'angular-gallery';
 
 @Component({
   selector: 'qa-extent-visualization',
