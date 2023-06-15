540509004701c15c5a2f2cdf3c211770ac1c2de2
sheenaze <tercjak@awst.at>
2023-06-15 16:16:18 +0200

    observers applied

--- UI/src/app/modules/user/user-form/user-form.component.ts
+++ UI/src/app/modules/user/user-form/user-form.component.ts
@@ -16,21 +16,32 @@ import {UserData} from '../../core/services/form-interfaces/UserDataForm';
 })
 export class UserFormComponent implements OnInit {
   userForm = this.formBuilder.group<UserData>({
-      username: ['', [Validators.required, Validators.maxLength(150)]],
-      password1: ['', [Validators.required]],
-      password2: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      first_name: [''],
-      last_name: [''],
-      organisation: [''],
-      country: [''],
-      orcid: [''],
-      terms_consent: ['', Validators.required]
+    username: ['', [Validators.required, Validators.maxLength(150)]],
+    password1: ['', [Validators.required]],
+    password2: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    first_name: [''],
+    last_name: [''],
+    organisation: [''],
+    country: [''],
+    orcid: [''],
+    terms_consent: ['', Validators.required]
   });
   countries: CountryDto[];
   countries$: Observable<CountryDto[]>;
   selectedCountry: CountryDto;
   formErrors: any;
+
+  signUpObserver = {
+    next: () => this.onSignUpNext(),
+    error: error => this.onSignupError(error)
+  }
+
+  UpdateObserver = {
+    next: data => this.onUpdateNext(data),
+    error: error => this.onUpdateError(error),
+    complete: () => this.onUpdateComplete()
+  }
 
   @Input() userData: UserDto;
   @Output() doRefresh = new EventEmitter();
@@ -39,55 +50,59 @@ export class UserFormComponent implements OnInit {
               private formBuilder: FormBuilder,
               private userService: AuthService,
               private router: Router,
-              private toastService: ToastService) { }
+              private toastService: ToastService) {
+  }
 
   ngOnInit(): void {
     // this.getListOfCountries();
     this.countries$ = this.userFormService.getCountryList();
-    if (this.userData){
+    if (this.userData) {
       this.setDefaultValues();
     }
   }
 
-  onSubmit(): void{
-    if (!this.userData){
-      this.userService.signUp(this.userForm.value).subscribe(
-        () => {
-          this.formErrors = null;
-          this.router.navigate(['/signup-complete']);
-        },
-        error => {
-          this.formErrors = error.error;
-        });
+  onSubmit(): void {
+    if (!this.userData) {
+      this.userService.signUp(this.userForm.value).subscribe(this.signUpObserver);
     } else {
-      this.userService.updateUser(this.userForm.value).subscribe(
-        data => {
-          this.userService.currentUser.email = data.email;
-          this.userService.currentUser.first_name = data.first_name;
-          this.userService.currentUser.last_name = data.last_name;
-          this.userService.currentUser.organisation = data.organisation;
-          this.userService.currentUser.country = data.country;
-          this.userService.currentUser.orcid = data.orcid;
-          this.doRefresh.emit(true);
-          this.formErrors = null;
-        },
-        error => {
-          this.formErrors = error.error;
-        },
-        () => {
-          this.toastService.showSuccessWithHeader('', 'User profile has been updated');
-        }
-      );
+      this.userService.updateUser(this.userForm.value).subscribe(this.UpdateObserver);
     }
   }
 
-  getListOfCountries(): void{
+  private onSignUpNext(): void {
+    this.formErrors = null;
+    this.router.navigate(['/signup-complete']);
+  }
+
+  private onSignupError(error): void{
+    this.formErrors = error.error;
+  }
+  private onUpdateComplete(): void {
+    this.toastService.showSuccessWithHeader('', 'User profile has been updated');
+  }
+
+  private onUpdateNext(data): void {
+    this.userService.currentUser.email = data.email;
+    this.userService.currentUser.first_name = data.first_name;
+    this.userService.currentUser.last_name = data.last_name;
+    this.userService.currentUser.organisation = data.organisation;
+    this.userService.currentUser.country = data.country;
+    this.userService.currentUser.orcid = data.orcid;
+    this.doRefresh.emit(true);
+    this.formErrors = null;
+  }
+
+  private onUpdateError(error): void {
+    this.formErrors = error.error;
+  }
+
+  getListOfCountries(): void {
     this.userFormService.getCountryList().subscribe(countries => {
       this.countries = countries;
     });
   }
 
-  setDefaultValues(): void{
+  setDefaultValues(): void {
     this.userForm.controls.username.setValue(this.userData.username);
     this.userForm.controls.username.disable();
     this.userForm.controls.email.setValue(this.userData.email);
@@ -101,7 +116,7 @@ export class UserFormComponent implements OnInit {
     this.userForm.controls.terms_consent.setValue(true);
   }
 
-  deactivateAccount(): void{
+  deactivateAccount(): void {
     const username = this.userService.currentUser.username;
     this.userService.deactivateUser(username).subscribe(
       () => {
