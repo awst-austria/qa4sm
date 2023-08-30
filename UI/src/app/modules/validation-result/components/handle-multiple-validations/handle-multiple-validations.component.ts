3d0a32ab9f736062f000bd26cbd5abefae02b7b7
sheenaze <tercjak@awst.at>
2023-08-30 07:44:11 +0200

    selecting by group removed

--- UI/src/app/modules/validation-result/components/handle-multiple-validations/handle-multiple-validations.component.ts
+++ UI/src/app/modules/validation-result/components/handle-multiple-validations/handle-multiple-validations.component.ts
@@ -2,56 +2,41 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {MenuItem} from 'primeng/api';
 
 @Component({
-    selector: 'qa-handle-multiple-validations',
-    templateUrl: './handle-multiple-validations.component.html',
-    styleUrls: ['./handle-multiple-validations.component.scss']
+  selector: 'qa-handle-multiple-validations',
+  templateUrl: './handle-multiple-validations.component.html',
+  styleUrls: ['./handle-multiple-validations.component.scss']
 })
 export class HandleMultipleValidationsComponent implements OnInit {
-    @Output() selectionActive = new EventEmitter();
+  @Output() selectionActive = new EventEmitter();
 
-    deleteItems: MenuItem[];
+  deleteItems: MenuItem[];
 
-    ngOnInit() {
-        this.deleteItems = [
-            {
-                label: 'Delete',
-                icon: 'pi pi-fw pi-trash',
-                items: [
-                    {
-                        label: 'Select one by one',
-                        items: [
-                            {
-                                label: 'Select all',
-                                icon: 'pi pi-fw pi-check-square',
-                                command: () => this.activateSelection(true)
-                            },
-                            {
-                                label: 'Select individually',
-                                icon: 'pi pi-fw pi-stop',
-                                command: () => this.activateSelection(false)
-                            }
-                        ]
-                    },
-                    {
-                        label: 'Select a group',
-                        items: [
-                            {
-                                label: 'Select by date',
-                                icon: 'pi pi-fw pi-calendar'
-                            },
-                            {
-                                label: 'Select by data set',
-                                icon: 'pi pi-fw pi-file'
-                            }
-                        ]
-                    },
-                ]
-            }
+  ngOnInit() {
+    this.deleteItems = [
+      {
+        label: 'Delete',
+        icon: 'pi pi-fw pi-trash',
+        items: [
+          {
+            label: 'Select all',
+            icon: 'pi pi-fw pi-check-square',
+            command: () => this.activateSelection(true)
+          },
+          {
+            label: 'Select individually',
+            icon: 'pi pi-fw pi-stop',
+            command: () => this.activateSelection(false)
+          }
+
         ]
+      }
+    ]
+  }
+
+    activateSelection(allSelected)
+  :
+    void {
+      this.selectionActive.emit({activate: true, selectAll: allSelected})
     }
 
-    activateSelection(allSelected): void{
-        this.selectionActive.emit({activate: true, selectAll: allSelected})
-    }
-
-}
+  }
