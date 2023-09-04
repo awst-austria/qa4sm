e2bd7d6849e5e2a5676d52895a9e1840407d2a3f
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2023-09-04 12:40:29 +0200

    Infinite scroll validations (#733)
    * cdk-virtual-scroll-viewport added
    
    * infinite scrolling first version added
    
    * not needed package removed
    
    * infinite scrolling improved
    
    * loading spinner is added
    
    * not needed import removed
    
    * coma removed
    
    * pagination module removed
    
    * pagination module removed
    
    * offset updated
    
    * setting offset and page number to onScroll; handling changing page fixed
    
    * code simplified; number of validations shown only for non-published ones; styling updated

--- UI/src/app/modules/validation-result/components/validation-page-paginated/validation-page-paginated.component.ts
+++ UI/src/app/modules/validation-result/components/validation-page-paginated/validation-page-paginated.component.ts
@@ -5,134 +5,128 @@ import {HttpParams} from '@angular/common/http';
 import {BehaviorSubject} from 'rxjs';
 
 @Component({
-  selector: 'qa-validation-page-paginated',
-  templateUrl: './validation-page-paginated.component.html',
-  styleUrls: ['./validation-page-paginated.component.scss']
+    selector: 'qa-validation-page-paginated',
+    templateUrl: './validation-page-paginated.component.html',
+    styleUrls: ['./validation-page-paginated.component.scss']
 })
 export class ValidationPagePaginatedComponent implements OnInit {
-  @Input() published: boolean;
+    @Input() published: boolean;
 
-  commonClasses = 'col-12 md:col-10 lg:col-10 xl:col-8  xl:col-offset-2 '
-  myValClasses = this.commonClasses + 'col-offset-4  md:col-offset-3  lg:col-offset-2'
-  publishedValClasses = this.commonClasses +  'md:col-offset-1 lg:col-offset-1'
+    commonClasses = 'col-12 md:col-10 lg:col-10 xl:col-8  xl:col-offset-2 '
+    myValClasses = this.commonClasses + 'col-offset-4  md:col-offset-3  lg:col-offset-2'
+    publishedValClasses = this.commonClasses + 'md:col-offset-1 lg:col-offset-1'
 
-  validations: ValidationrunDto[] = [];
-  maxNumberOfPages: number;
-  currentPage = 1;
-  limit = 10;
-  offset = 0;
-  order = '-start_time';
-  selectionActive$ = new BehaviorSubject(false);
-  selectedValidations$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+    validations: ValidationrunDto[] = [];
+    maxNumberOfPages: number;
+    currentPage = 1;
+    limit = 10;
+    offset = 0;
+    order = '-start_time';
+    selectionActive$ = new BehaviorSubject(false);
+    selectedValidations$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
 
 
-  isLoading: boolean = false;
-  endOfPage: boolean = false;
-  constructor(private validationrunService: ValidationrunService) {
-  }
+    isLoading: boolean = false;
+    endOfPage: boolean = false;
 
-  ngOnInit(): void {
-    this.getValidationsAndItsNumber(this.published);
-    this.validationrunService.doRefresh.subscribe(value => {
-      if (value && value !== 'page') {
-        this.updateData(value);
-      } else if (value && value === 'page') {
-        this.refreshPage();
-      }
-    });
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll() {
-    const windowHeight = 'innerHeight' in window ? window.innerHeight : document.documentElement.offsetHeight;
-    const body = document.body, html = document.documentElement;
-    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-    const windowBottom = windowHeight + window.pageYOffset;
-
-    if (windowBottom >= docHeight - 1 && !this.isLoading && !this.endOfPage) {
-      this.currentPage++;
-      this.getValidationsAndItsNumber(this.published);
+    constructor(private validationrunService: ValidationrunService) {
     }
-  }
 
-  getValidationsAndItsNumber(published: boolean): void {
-    this.isLoading = true;
-    const parameters = new HttpParams().set('offset', String(this.offset)).set('limit', String(this.limit))
-      .set('order', String(this.order));
-    if (!published) {
-      this.validationrunService.getMyValidationruns(parameters).subscribe(
-        response => {
-          this.handleFetchedValidations(response)
-        });
-    } else {
-      this.validationrunService.getPublishedValidationruns(parameters).subscribe(
-        response => {
-          this.handleFetchedValidations(response);
+    ngOnInit(): void {
+        this.getValidationsAndItsNumber(this.published);
+        this.validationrunService.doRefresh.subscribe(value => {
+            if (value && value !== 'page') {
+                this.updateData(value);
+            } else if (value && value === 'page') {
+                this.refreshPage();
+            }
         });
     }
-  }
 
-  handleFetchedValidations(serverResponse: { validations: ValidationrunDto[]; length: number; }): void{
-    const {validations, length} = serverResponse;
-    if (!this.maxNumberOfPages){
-      this.maxNumberOfPages = Math.ceil( length/this.limit);
-    }
-    if (validations.length){
-      this.validations = this.validations.concat(validations);
-      this.currentPage++;
+    @HostListener('window:scroll', ['$event'])
+    onScroll() {
+        const windowHeight = 'innerHeight' in window ? window.innerHeight : document.documentElement.offsetHeight;
+        const docHeight = document.body.offsetHeight
+        const windowBottom = windowHeight + window.scrollY;
 
-      if (this.currentPage > this.maxNumberOfPages ){
-        this.endOfPage = true;
-      }
-    } else {
-      this.endOfPage = true;
+        if (windowBottom >= docHeight - 1 && !this.isLoading && !this.endOfPage) {
+            this.currentPage++;
+            this.offset = (this.currentPage - 1) * this.limit;
+            this.getValidationsAndItsNumber(this.published);
+        }
     }
 
-    this.isLoading = false;
-  }
-
-  handlePageChange(event: number): void {
-    this.currentPage = event;
-    this.offset = (this.currentPage - 1) * this.limit;
-    this.getValidationsAndItsNumber(this.published);
-  }
-
-  getOrder(order): void {
-    this.order = order;
-    this.getValidationsAndItsNumber(this.published);
-  }
-
-  updateData(validationId: string): void {
-    const indexOfValidation = this.validations.findIndex(validation => validation.id === validationId);
-    this.validationrunService.getValidationRunById(validationId).subscribe(data => {
-      this.validations[indexOfValidation] = data;
-    });
-  }
-
-  refreshPage(): void {
-    const parameters = new HttpParams().set('offset', String(this.offset)).set('limit', String(this.limit))
-      .set('order', String(this.order));
-    this.validationrunService.getMyValidationruns(parameters).subscribe(
-      response => {
-        const {validations, length} = response;
-        this.validations = validations;
-        // this.numberOfValidations = length;
-      });
-  }
-
-  handleMultipleSelection(event): void {
-    this.selectionActive$.next(event.activate)
-    this.selectedValidations$.next(event.selected.value)
-  }
-
-  updateSelectedValidations(checked: string[], id: string): void {
-    let selectedValidations = this.selectedValidations$.getValue();
-    if (checked.includes(id)) {
-      selectedValidations = [...selectedValidations, id];
-    } else {
-      selectedValidations = selectedValidations.filter(selectedId => selectedId !== id);
+    getValidationsAndItsNumber(published: boolean): void {
+        this.isLoading = true;
+        const parameters = new HttpParams().set('offset', String(this.offset)).set('limit', String(this.limit))
+            .set('order', String(this.order));
+        if (!published) {
+            this.validationrunService.getMyValidationruns(parameters).subscribe(
+                response => {
+                    this.handleFetchedValidations(response)
+                });
+        } else {
+            this.validationrunService.getPublishedValidationruns(parameters).subscribe(
+                response => {
+                    this.handleFetchedValidations(response);
+                });
+        }
     }
-    this.selectedValidations$.next(selectedValidations)
-  }
+
+    handleFetchedValidations(serverResponse: { validations: ValidationrunDto[]; length: number; }): void {
+        const {validations, length} = serverResponse;
+        if (!this.maxNumberOfPages) {
+            this.maxNumberOfPages = Math.ceil(length / this.limit);
+        }
+        if (validations.length) {
+            this.validations = this.validations.concat(validations);
+
+            if (this.currentPage > this.maxNumberOfPages) {
+                this.endOfPage = true;
+            }
+        } else {
+            this.endOfPage = true;
+        }
+
+        this.isLoading = false;
+    }
+
+    getOrder(order): void {
+        this.order = order;
+        this.getValidationsAndItsNumber(this.published);
+    }
+
+    updateData(validationId: string): void {
+        const indexOfValidation = this.validations.findIndex(validation => validation.id === validationId);
+        this.validationrunService.getValidationRunById(validationId).subscribe(data => {
+            this.validations[indexOfValidation] = data;
+        });
+    }
+
+    refreshPage(): void {
+        const parameters = new HttpParams().set('offset', String(this.offset)).set('limit', String(this.limit))
+            .set('order', String(this.order));
+        this.validationrunService.getMyValidationruns(parameters).subscribe(
+            response => {
+                const {validations, length} = response;
+                this.validations = validations;
+                // this.numberOfValidations = length;
+            });
+    }
+
+    handleMultipleSelection(event): void {
+        this.selectionActive$.next(event.activate)
+        this.selectedValidations$.next(event.selected.value)
+    }
+
+    updateSelectedValidations(checked: string[], id: string): void {
+        let selectedValidations = this.selectedValidations$.getValue();
+        if (checked.includes(id)) {
+            selectedValidations = [...selectedValidations, id];
+        } else {
+            selectedValidations = selectedValidations.filter(selectedId => selectedId !== id);
+        }
+        this.selectedValidations$.next(selectedValidations)
+    }
 
 }
