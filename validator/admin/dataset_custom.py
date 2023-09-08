086f71068e8d4599f299d8e76fe44dd9fcd63058
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2023-09-08 10:35:14 +0200

    Improve sharing data (#737)
    * saving model instance updated so it set dataset storage path; deleting dataset, version and variable updated, to check if there are other relations
    
    * setting dataset storage path moved to the model instance, deleted from api view
    
    * file description updated in the admin panel
    
    * blank line added
    
    * file description updated in the admin panel
    
    * setting proper data path moved to the save method

--- validator/admin/dataset_custom.py
+++ validator/admin/dataset_custom.py
@@ -6,9 +6,11 @@ class DatasetAdmin(ModelAdmin):
     list_display = ('id', 'short_name', 'user', 'file')
 
     def file(self, obj):
-        if len(obj.user_dataset.all()):
+        if len(obj.user_dataset.all()) and obj.id >= USER_DATASET_MIN_ID:
             return obj.user_dataset.all()[0]
-        elif obj.id< USER_DATASET_MIN_ID:
+        elif not len(obj.user_dataset.all()) and obj.user is not None:
+            return "Private admin data"
+        elif obj.id < USER_DATASET_MIN_ID:
             return "Application data"
         else:
             return "No file assigned"
