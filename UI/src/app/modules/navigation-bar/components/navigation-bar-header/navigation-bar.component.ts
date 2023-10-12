d6eb9aa525e5ff3b8283415c43ea478663f720fb
sheenaze <tercjak@awst.at>
2023-10-12 11:55:11 +0200

    navigation bar updated to hide some buttons for non-authenticated user

--- UI/src/app/modules/navigation-bar/components/navigation-bar-header/navigation-bar.component.ts
+++ UI/src/app/modules/navigation-bar/components/navigation-bar-header/navigation-bar.component.ts
@@ -4,7 +4,6 @@ import {AuthService} from '../../../core/services/auth/auth.service';
 import {Router} from '@angular/router';
 import {ToastService} from '../../../core/services/toast/toast.service';
 import {SettingsService} from '../../../core/services/global/settings.service';
-import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'navigation-bar-header',
@@ -14,84 +13,135 @@ import {BehaviorSubject} from 'rxjs';
 export class NavigationBarComponent implements OnInit {
 
   items: MenuItem[];
+  homeMenuItem: MenuItem;
+  validateMenuItem: MenuItem;
+  myResultsMenuItem: MenuItem;
+  publishedResultsMenuItem: MenuItem;
+  resultsMenuItem: MenuItem;
+  compareResultsMenuItem: MenuItem;
+  uploadDataMenuItem: MenuItem;
+  uploadDataHelpMenuItem: MenuItem;
+  datasetUploadMenuItem: MenuItem;
+  helpMenuItem: MenuItem;
+  userManualMenuItem: MenuItem;
+  datasetMenuItem: MenuItem;
+  termsMenuItem: MenuItem;
+  infoMenuItem: MenuItem;
+  contactMenuItem: MenuItem;
+  userMenuItem: MenuItem;
   loginMenuItem: MenuItem;
   logoutMenuItem: MenuItem;
   userProfileMenuItem: MenuItem;
-  isFixed: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  isAuthenticated: boolean;
+
 
   constructor(private authService: AuthService,
               private router: Router,
               private toastService: ToastService,
               private settingsService: SettingsService) {
+    this.homeMenuItem = {
+      label: 'Home',
+      icon: 'pi pi-fw pi-home',
+      routerLink: ['home'],
+      command: () => this.setPreviousUrl('home')
+    };
+
+    this.validateMenuItem = {
+      label: 'Validate',
+      icon: 'pi pi-fw pi-wrench',
+      routerLink: ['validate'],
+      command: () => this.setPreviousUrl('validate')
+    };
+
+    this.myResultsMenuItem = {
+      label: 'My validations',
+      icon: 'pi pi-fw pi-folder',
+      routerLink: ['my-validations'],
+      command: () => this.setPreviousUrl('my-validations')
+    };
+
+    this.publishedResultsMenuItem = {
+      label: 'Published validations',
+      icon: 'pi pi-fw pi-globe',
+      routerLink: ['published-validations']
+    };
+
+    this.resultsMenuItem = {
+      label: 'Results',
+      icon: 'pi pi-fw pi-folder',
+      items: [
+        this.myResultsMenuItem,
+        this.publishedResultsMenuItem,
+      ]
+    };
+
+    this.compareResultsMenuItem = {
+      label: 'Compare validations',
+      icon: 'pi pi-fw pi-th-large',
+      routerLink: ['comparison'],
+      command: () => this.setPreviousUrl('comparison')
+    };
+
+    this.uploadDataMenuItem = {
+      label: 'My datasets',
+      icon: 'pi pi-fw pi-upload',
+      routerLink: ['my-datasets'],
+      command: () => this.setPreviousUrl('my-datasets')
+    };
+
+    this.uploadDataHelpMenuItem = {
+      label: 'Upload Data Help',
+      icon: 'pi pi-fw pi-question',
+      routerLink: ['user-data-guidelines']
+    };
+
+    this.datasetUploadMenuItem = {
+      label: 'Dataset upload',
+      icon: 'pi pi-fw pi-upload',
+      items: [
+        this.uploadDataMenuItem,
+        this.uploadDataHelpMenuItem,
+      ]
+    };
+
+    this.helpMenuItem = {label: 'Help', icon: 'pi pi-fw pi-question', routerLink: ['help']};
+    this.userManualMenuItem = {label: 'User Manual', icon: 'pi pi-fw pi-book', url: '', target: '_blank'}
+    this.datasetMenuItem = {label: 'Datasets', icon: 'pi pi-fw pi-save', routerLink: ['datasets']};
+    this.termsMenuItem = {label: 'Terms', icon: 'pi pi-fw pi-briefcase', routerLink: ['terms']};
+    this.infoMenuItem = {
+      label: 'Info', icon: 'pi pi-fw pi-info-circle', items: [
+        this.helpMenuItem,
+        this.userManualMenuItem,
+        this.datasetMenuItem,
+        this.termsMenuItem,
+
+      ]
+    };
+
+    this.contactMenuItem = {label: 'Contact Us', icon: 'pi pi-fw pi-envelope', routerLink: ['contact-us']};
 
     this.loginMenuItem = {
       label: 'Log in',
       icon: 'pi pi-fw pi-sign-in',
       routerLink: ['login'],
-      command: () => this.setPreviousUrl('user-profile')
+      command: () => this.setPreviousUrl('user-profile'),
+      visible: !this.authService.authenticated.value
     };
+
+
     this.userProfileMenuItem = {label: 'User profile', icon: 'pi pi-fw pi-user', routerLink: ['user-profile']};
     this.logoutMenuItem = {label: 'Log out', icon: 'pi pi-fw pi-sign-out', command: () => this.logout()};
-    this.items = [
-      {label: 'Home', icon: 'pi pi-fw pi-home', routerLink: ['home'], command: () => this.setPreviousUrl('home')},
-      {
-        label: 'Validate',
-        icon: 'pi pi-fw pi-wrench',
-        routerLink: ['validate'],
-        // styleClass: 'validate-button',
-        command: () => this.setPreviousUrl('validate')
-      },
-      {
-        label: 'Results',
-        icon: 'pi pi-fw pi-folder',
-        items: [
-          {
-            label: 'My validations',
-            icon: 'pi pi-fw pi-folder',
-            routerLink: ['my-validations'],
-            command: () => this.setPreviousUrl('my-validations')
-          },
-          {label: 'Published validations', icon: 'pi pi-fw pi-globe', routerLink: ['published-validations']},
-        ]
-      },
-      {label: 'Compare validations', icon: 'pi pi-fw pi-th-large', routerLink: ['comparison'], command: () => this.setPreviousUrl('comparison')},
-      {
-        label: 'Dataset upload',
-        icon: 'pi pi-fw pi-upload',
-        items: [
-          {label: 'My datasets',
-            icon: 'pi pi-fw pi-upload',
-            routerLink: ['my-datasets'],
-            command: () => this.setPreviousUrl('my-datasets')
-          },
-          {label: 'Upload Data Help', icon: 'pi pi-fw pi-question', routerLink: ['user-data-guidelines']},
-        ]
-      },
-
-      {
-        label: 'Info', icon: 'pi pi-fw pi-info-circle', items: [
-          // {label: 'About', icon: 'pi pi-fw pi-info', routerLink: ['about']},
-          {label: 'Help', icon: 'pi pi-fw pi-question', routerLink: ['help']},
-          {
-            label: 'User Manual',
-            icon: 'pi pi-fw pi-book',
-            url: '',
-            target: '_blank'
-          },
-          {label: 'Datasets', icon: 'pi pi-fw pi-save', routerLink: ['datasets']},
-          {label: 'Terms', icon: 'pi pi-fw pi-briefcase', routerLink: ['terms']},
-        ]
-      },
-      {label: 'Contact Us', icon: 'pi pi-fw pi-envelope', routerLink: ['contact-us']},
-      {
-        label: 'Profile', icon: 'pi pi-fw pi-user', items: [
-          this.userProfileMenuItem,
-          this.logoutMenuItem,
-          this.loginMenuItem,
-        ]
-      }
-    ];
+    this.userMenuItem = {
+      label: `My account`,
+      icon: 'pi pi-fw pi-user',
+      items: [
+        this.userProfileMenuItem,
+        this.logoutMenuItem,
+      ],
+    };
   }
+
 
   ngOnInit(): void {
     this.authService.authenticated.subscribe(authenticated => this.authStatusChanged(authenticated));
@@ -99,9 +149,27 @@ export class NavigationBarComponent implements OnInit {
   }
 
   private authStatusChanged(authenticated: boolean): void {
-    this.logoutMenuItem.disabled = !authenticated;
-    this.loginMenuItem.disabled = authenticated;
-    this.userProfileMenuItem.disabled = !authenticated;
+    this.loginMenuItem.visible = !authenticated;
+    this.userMenuItem.visible = authenticated;
+    this.myResultsMenuItem.visible = authenticated;
+    this.compareResultsMenuItem.visible = authenticated;
+    this.datasetUploadMenuItem.visible = authenticated;
+    this.setMenuItems();
+  }
+
+
+  private setMenuItems(): void {
+    this.items = [
+      this.homeMenuItem,
+      this.validateMenuItem,
+      this.resultsMenuItem,
+      this.compareResultsMenuItem,
+      this.datasetUploadMenuItem,
+      this.infoMenuItem,
+      this.contactMenuItem,
+      this.loginMenuItem,
+      this.userMenuItem
+    ];
   }
 
   logout(): void {
@@ -119,7 +187,7 @@ export class NavigationBarComponent implements OnInit {
   }
 
   setSumLink(): void {
-    const userManualItem =  this.items.find(item => item.label === 'Info')
+    const userManualItem = this.items.find(item => item.label === 'Info')
       .items.find(item => item.label === 'User Manual');
     this.settingsService.getAllSettings().subscribe(data => {
       userManualItem.url = data[0].sum_link;
