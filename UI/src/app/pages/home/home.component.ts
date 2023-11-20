fb2960b1fd421b07aab50ca3c0948fce3e4f9ac9
sheenaze <tercjak@awst.at>
2023-11-20 19:29:51 +0100

    animations updated

--- UI/src/app/pages/home/home.component.ts
+++ UI/src/app/pages/home/home.component.ts
@@ -1,9 +1,8 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit, Renderer2} from '@angular/core';
 import {AuthService} from '../../modules/core/services/auth/auth.service';
 import {SettingsService} from '../../modules/core/services/global/settings.service';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 import {DomSanitizer} from '@angular/platform-browser';
 
 const homeUrlPrefix = '/static/images/home/';
@@ -15,40 +14,8 @@ const videosPrefix = 'static/videos/'
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('showNews', [
-      state('show', style({ opacity: 1, transform: 'translateY(0)' })),
-      state('hide', style({ opacity: 0, transform: 'translateY(20px)' })),
-      transition('hide => show', animate('1000ms ease-in')),
-    ]),
-    trigger('showLine1', [
-      state('show', style({ opacity: 1, transform: 'translateY(0)' })),
-      state('hide', style({ opacity: 0, transform: 'translateY(20px)' })),
-      transition('hide => show', animate('1000ms ease-in')),
-    ]),
-    trigger('showDesc', [
-      state('show', style({ opacity: 1, transform: 'translateY(0)' })),
-      state('hide', style({ opacity: 0, transform: 'translateY(20px)' })),
-      transition('hide => show', animate('1000ms ease-in')),
-    ]),
-    trigger('showLine2', [
-      state('show', style({ opacity: 1, transform: 'translateY(0)' })),
-      state('hide', style({ opacity: 0, transform: 'translateY(20px)' })),
-      transition('hide => show', animate('1000ms ease-in')),
-    ]),
-    trigger('showFeedback', [
-      state('show', style({ opacity: 1, transform: 'translateY(0)' })),
-      state('hide', style({ opacity: 0, transform: 'translateY(20px)' })),
-      transition('hide => show', animate('1000ms ease-in')),
-    ]),
-  ],
 })
 export class HomeComponent implements OnInit {
-  showNewsState = 'hide';
-  showLine1State = 'hide';
-  showDescState = 'hide';
-  showLine2State = 'hide';
-  showFeedbackState = 'hide';
 
   logoFiles = [{
     plot: logoUrlPrefix + 'logo_awst.webp',
@@ -95,6 +62,11 @@ export class HomeComponent implements OnInit {
 
   userLoggedIn: boolean;
 
+  animationFadeInDownClass = ['fadeindown', 'animation-duration-1000', 'animation-iteration-1']
+  animationFadeInClass = ['fadein', 'animation-duration-3000', 'animation-iteration-1']
+  animationFadeInLeftClass = ['fadeinleft', 'animation-duration-1000', 'animation-iteration-1']
+  animationFadeInRightClass = ['fadeinright', 'animation-duration-1000', 'animation-iteration-1']
+
   applicationInfoClass = {
     main: 'flex flex-wrap flex-column row-gap-0 application-info',
     oddRow: 'flex flex-row flex-wrap column-gap-0 w-12 application-row',
@@ -104,31 +76,31 @@ export class HomeComponent implements OnInit {
     div: 'lg:text-3xl md:text-2xl  sm:text-base text-xs',
   }
 
-  newsSectionClass = {
-    main: 'app-news flex flex-column flex-wrap justify-content-center align-items-center',
+  qa4smNewsClass = {
+    main: 'app-news flex flex-column flex-wrap justify-content-center align-items-center fadeInOut',
     header: 'line-height-2 lg:text-5xl md:text-4xl sm:text-3xl text-2xl my-1 text-center',
     div: 'line-height-2 lg:text-2xl md:text-xl sm:text-lg text-base py-1'
   }
 
   opinionSectionClass = {
-    main: 'app-news flex flex-column flex-wrap justify-content-center align-items-center mb-2',
+    main: 'app-news flex flex-column flex-wrap justify-content-center align-items-center mb-2 fadeInOut',
     header: 'lg:text-5xl md:text-4xl sm:text-3xl text-2xl mb-2 line-height-2 text-center',
     div: 'line-height-2 lg:text-2xl md:text-xl sm:text-lg text-base py-1 text-center'
   }
 
   verticalLineClass = {
-    main: 'flex flex-column lg:h-6rem h-3rem',
+    main: 'flex flex-column lg:h-6rem h-3rem fadeIn',
   };
 
-  applicationDescriptionClass = 'application-desc flex flex-column flex-wrap h-auto';
+  applicationDescriptionClass = 'application-desc flex flex-column flex-wrap h-auto fadeIn';
   applicationIntroClass = {
-    main: 'application-intro',
+    main: 'application-intro fadeInOut',
     h1: 'lg:text-6xl md:text-5xl sm:text-4xl text-3xl',
     span: 'lg:text-3xl md:text-2xl'
   }
 
   videosClass = {
-    main: 'application-videos lg:my-5 my-2 w-12',
+    main: 'application-videos lg:my-5 my-2 w-12 fadeInOut',
     mainDiv: 'flex flex-column align-content-center justify-content-center w-12',
     rowDiv: "w-12 py-2 text-center iframe-container",
     header: "lg:text-3xl md:text-2xl text-base",
@@ -136,19 +108,20 @@ export class HomeComponent implements OnInit {
 
   applicationTeamClass = {
     main: 'application-team flex flex-column flex-wrap h-auto lg:my-6 my-4 row-gap-2',
-    h1: 'lg:text-6xl md:text-5xl sm:text-4xl text-3xl',
+    h1: 'lg:text-6xl md:text-5xl sm:text-4xl text-3xl fadeInOut',
     partner: 'flex flex-row flex-wrap w-12 gap-2 align-items-center justify-content-center',
-    partnerDesc: 'md:w-8 w-12 px-3 text-justify partner-desc gap-1 lg:text-xl md:text-base text-xs',
-    partnerLogo: 'md:w-3 w-12 partner-logo align-items-center justify-content-center text-center',
+    partnerDesc: 'md:w-8 w-12 px-3 text-justify partner-desc gap-1 lg:text-xl md:text-base text-xs fadeInLeft',
+    partnerLogo: 'md:w-3 w-12 partner-logo align-items-center justify-content-center text-center fadeInRight',
     team: 'flex flex-row flex-wrap gap-2 w-12 align-items-center justify-content-center mb-2',
-    teamDesc: 'md:w-9 w-12 text-justify team-desc md:pl-8 pl-2 lg:text-xl md:text-base text-xs',
-    teamLogo: 'md:w-2 w-12 md:ml-5 ml-3 partner-logo flex align-items-center justify-content-center'
+    teamDesc: 'md:w-9 w-12 text-justify team-desc md:pl-8 pl-2 lg:text-xl md:text-base text-xs fadeInRight',
+    teamLogo: 'md:w-2 w-12 md:ml-5 ml-3 partner-logo flex align-items-center justify-content-center fadeInLeft'
   }
 
   constructor(private authService: AuthService,
               private settingsService: SettingsService,
               private router: Router,
-              private sanitizer: DomSanitizer) {
+              private sanitizer: DomSanitizer,
+              private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
@@ -157,53 +130,54 @@ export class HomeComponent implements OnInit {
   }
 
   goToNews(): void {
-    this.router.navigate([], {fragment: "qa4sm-news"});
+    this.router.navigate([], {fragment: "qa4smNews"});
   }
 
-  // Add a method to change the animation state
-  toggleNewsAnimation() {
-    this.showNewsState = 'show';
-  }
-  toggleDescAnimation() {
-    this.showDescState = 'show';
-  }
-  toggleAnimation(element, state){
-    switch (element) {
-      case '#qa4sm-news':
-        this.showNewsState = state;
-        break;
-      case '#line-1':
-        this.showLine1State = state;
-        break;
-      case '#qa4sm-desc':
-        this.showDescState = state;
-        break;
-      case '#line-2':
-        this.showLine2State = state;
-        break;
-      case '#qa4sm-opinion':
-        this.showFeedbackState = state;
-        break;
+
+  animations(element): string[] {
+    if (element.includes('fadeInOut')) {
+      return this.animationFadeInDownClass
+    } else if (element.includes('fadeInRight')) {
+      return this.animationFadeInRightClass
+    } else if (element.includes('fadeInLeft')) {
+      return this.animationFadeInLeftClass
+    } else if (element.includes('fadeIn')) {
+      return this.animationFadeInClass
     }
+    return []
   }
+
+  toggleAnimation(element, classes: string[]) {
+    classes.forEach(className => {
+      this.renderer.addClass(element, className);
+    });
+  }
+
   @HostListener('window:scroll', [])
   onScroll() {
-    ['#qa4sm-news', '#line-1' , '#qa4sm-desc', '#line-2', '#qa4sm-opinion'].forEach(element => {
+    ['.fadeInOut',
+      '.fadeIn',
+      '.fadeInLeft',
+      '.fadeInRight'].forEach(element => {
       this.checkVisibility(element)
     })
   }
 
   checkVisibility(element) {
-    const animatedElement = document.querySelector(element);
+    const elements = document.querySelectorAll(element);
+
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          this.toggleAnimation(element, 'show')
+          const identifier = entry.target;
+          this.toggleAnimation(identifier, this.animations(identifier.className))
         }
       });
-    });
+    }, {threshold: 0.001}); // Adjust the threshold as needed
 
-    observer.observe(animatedElement);
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
   }
 
 }
