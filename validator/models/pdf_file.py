fc97685bfd0963137fef81ee490ba8d2378c0018
sheenaze <tercjak@awst.at>
2023-11-21 16:53:04 +0100

    pdf class updated to set user manual link automatically

--- validator/models/pdf_file.py
+++ validator/models/pdf_file.py
@@ -1,10 +1,11 @@
 from django.core.files.storage import FileSystemStorage
 from django.db import models
-from valentina.settings_conf import DOCS_DIR
+from valentina.settings_conf import DOCS_DIR, SITE_URL
 from django.dispatch.dispatcher import receiver
 from django.db.models.signals import post_delete
 from shutil import rmtree
 from os import path
+from validator.models import Settings
 import uuid
 
 key_store = FileSystemStorage(location=DOCS_DIR)
@@ -15,6 +16,14 @@ class PdfFiles(models.Model):
     file = models.FileField(null=True, blank=True, storage=key_store, upload_to='./files_to_serve')
     file_name = models.CharField(max_length=100, blank=True, null=True)
     upload_date = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if self.file_name == 'UserManual':
+            settings = Settings.objects.all().first()
+            settings.sum_link = SITE_URL + f'/api/get-pdf-file?file_id={self.id}'
+            settings.save()
+
+        super(PdfFiles, self).save(*args, **kwargs)
 
 
 @receiver(post_delete, sender=PdfFiles)
