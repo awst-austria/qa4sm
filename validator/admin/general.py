e5d5cbddd835183d6da3bfc248de9f5849b6d9a6
sheenaze <tercjak@awst.at>
2023-11-24 12:52:43 +0100

    celery tasks list added to the admin panel

--- validator/admin/general.py
+++ validator/admin/general.py
@@ -11,6 +11,7 @@ from validator.admin import DatasetAdmin
 from validator.admin import DatasetVersionAdmin
 from validator.admin import DatasetVariableAdmin
 from validator.admin.data_management_groups import DataManagementGroupAdmin
+from validator.admin.celery_tasks import CeleryTasksAdmin
 from validator.models import DataFilter
 from validator.models import DataVariable
 from validator.models import Dataset
@@ -22,6 +23,7 @@ from validator.models import Email
 from validator.models import UptimeReport
 from validator.models import UserDatasetFile
 from validator.models import DataManagementGroup
+from validator.models import CeleryTask
 User = get_user_model()
 
 admin.site.register(Settings, SystemSettingsAdmin)
@@ -36,3 +38,4 @@ admin.site.register(Email, EmailAdmin)
 admin.site.register(UptimeReport, UptimeMonitoringAdmin)
 admin.site.register(UserDatasetFile, UserDatasetFileAdmin)
 admin.site.register(DataManagementGroup, DataManagementGroupAdmin)
+admin.site.register(CeleryTask, CeleryTasksAdmin)
