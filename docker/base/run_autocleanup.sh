d4de265d61fe4a63e6bd7064a0bf5a96fb14a0a2
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2023-12-22 09:16:36 +0100

    not needed file removed; copying removed, as the script will be copied with the entire app (#812)

--- docker/base/run_autocleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-APP_DIR="/var/lib/qa4sm-web-val"
-MINICONDA_PATH="/opt/miniconda"
-PYTHON_ENV_DIR="$APP_DIR/virtenv"
-
-PATH="$PATH:$MINICONDA_PATH/bin/"
-source activate "$PYTHON_ENV_DIR"
-cd "$APP_DIR/valentina"
-python manage.py autocleanupvalidations
