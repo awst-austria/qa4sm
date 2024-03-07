68d2986e819cffa08243d6379d37df4c7de13513
sheenaze <tercjak@awst.at>
2024-03-07 09:15:09 +0100

    not needed model removed

--- UI/src/app/pages/home/homepage-images-model.ts
+++
@@ -1,9 +0,0 @@
-import {PlotDto} from '../../modules/core/services/global/plot.dto';
-import {Observable} from 'rxjs';
-
-export class HomepageImagesModel{
-  constructor(public plot: Observable<PlotDto>,
-              public link: string,
-              public description: string) {
-  }
-}
