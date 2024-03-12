5e2b390ace862f1f0e3913739eb958b91e6c2a52
sheenaze <tercjak@awst.at>
2024-03-12 08:30:41 +0100

    error handling function updated

--- UI/src/app/modules/core/services/global/http-error.service.ts
+++ UI/src/app/modules/core/services/global/http-error.service.ts
@@ -7,24 +7,38 @@ import {Observable, throwError} from 'rxjs';
 })
 export class HttpErrorService {
 
-  constructor() { }
+  constructor() {
+  }
 
 
-  private customHttpErrorFormatter(err: HttpErrorResponse): CustomHttpError {
-    let errorMessage = '';
+  private customHttpErrorFormatter(err: HttpErrorResponse,
+                                   message: string | undefined,
+                                   header: string | undefined): CustomHttpError {
+
+    /**
+     * Error status doesn't tell much to the user, therefore we take either a message passed when piping particular
+     * request or from the error message, if it's passed from the backend. If none exists, we pass undefined and the
+     *  message will be defined separately in each case
+     */
+
+    let errorMessage = undefined;
+    const errorHeader = header ? header : 'Something went wrong'
+
     if (err.error instanceof ErrorEvent) {
       console.error(`Client-side or network problem ${err.error.message}.`)
-      errorMessage = `An error occurred: ${err.error.message}`;
+      errorMessage = message ? message : `An error occurred: ${err.error.message}`;
     } else {
       console.error(`Problem on the server side. Server returned code: ${err.status}.
       Error message is: ${err.statusText}`)
-      errorMessage = `${err.error.message ? err.error.message : ''}`;
+        errorMessage = message ? message : `${err.error.message ? err.error.message : undefined}`;
     }
-    return {status: err.status, message: errorMessage};
+    return {status: err.status, message: errorMessage, header: errorHeader};
   }
 
-  handleError(error: HttpErrorResponse): Observable<never>{
-    return throwError(() => this.customHttpErrorFormatter(error))
+  handleError(error: HttpErrorResponse,
+              message: string | undefined = undefined,
+              header: string | undefined = undefined): Observable<never> {
+    return throwError(() => this.customHttpErrorFormatter(error, message, header))
   }
 
 
@@ -32,5 +46,6 @@ export class HttpErrorService {
 
 export interface CustomHttpError {
   status: number,
-  message: string
+  message: string,
+  header?: string,
 }
