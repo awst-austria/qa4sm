03101b22bf66c50030b6f212a144ef8fc607de84
sheenaze <tercjak@awst.at>
2024-03-14 09:44:52 +0100

    handling error added to the validation result page

--- UI/src/app/pages/validation-result/validation-result.component.ts
+++ UI/src/app/pages/validation-result/validation-result.component.ts
@@ -1,8 +1,10 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, signal} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ValidationrunService} from '../../modules/core/services/validation-run/validationrun.service';
 import {DatasetConfigurationService} from '../../modules/validation-result/services/dataset-configuration.service';
 import {ValidationResultModel} from './validation-result-model';
+import {catchError} from 'rxjs/operators';
+import {EMPTY} from 'rxjs';
 
 @Component({
   selector: 'app-validation',
@@ -13,6 +15,7 @@ import {ValidationResultModel} from './validation-result-model';
 export class ValidationResultComponent implements OnInit {
   public validationId: string;
   public validationModel: ValidationResultModel;
+  dataFetchError = signal(false);
 
   constructor(private route: ActivatedRoute,
               private validationRunService: ValidationrunService,
@@ -23,14 +26,33 @@ export class ValidationResultComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.validationId = params.validationId;
       this.validationModel = new ValidationResultModel(
-        this.validationRunService.getValidationRunById(this.validationId),
+        this.validationRunService.getValidationRunById(this.validationId)
+          .pipe(
+            catchError(() => {
+              console.log('Monika')
+              this.dataFetchError.set(true);
+              return EMPTY;
+            })
+          ),
         this.datasetConfigurationService.getConfigByValidationrun(this.validationId)
+          .pipe(
+            catchError(() => {
+              this.dataFetchError.set(true);
+              return EMPTY;
+            })
+          )
       );
     });
   }
 
   update(): void{
-    this.validationModel.validationRun = this.validationRunService.getValidationRunById(this.validationId);
+    this.validationModel.validationRun = this.validationRunService.getValidationRunById(this.validationId)
+      .pipe(
+      catchError(() => {
+        this.dataFetchError.set(true);
+          return EMPTY;
+      })
+    );
   }
 
 }
