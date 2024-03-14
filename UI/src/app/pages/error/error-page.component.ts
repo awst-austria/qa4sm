dd97db6772463026928ca4d13be521886ebd972a
sheenaze <tercjak@awst.at>
2024-03-14 19:03:25 +0100

    selector updated to be consistent with other selectors

--- UI/src/app/pages/error/error-page.component.ts
+++ UI/src/app/pages/error/error-page.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-error-page',
+  selector: 'qa-error-page',
   templateUrl: './error-page.component.html',
   styleUrls: ['./error-page.component.scss']
 })
