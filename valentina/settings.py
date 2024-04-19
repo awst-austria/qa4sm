d4e8470f05e50e63b63dd58e605b5d9acdb9d3e1
sheenaze <tercjak@awst.at>
2024-04-19 09:34:58 +0200

    authentication methods order updated

--- valentina/settings.py
+++ valentina/settings.py
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',  # make all views private
