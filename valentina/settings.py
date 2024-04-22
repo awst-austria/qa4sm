dd38d54fc85d7bdc80d0464ad58d63dcdac3cb0d
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2024-04-22 06:51:42 +0200

    Update settings.py
    duplicated token authentication removed

--- valentina/settings.py
+++ valentina/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
