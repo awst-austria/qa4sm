f7fd4abaafc3147db23db7e9ca6d105d1a1215f6
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2024-05-23 15:56:32 +0200

    Autocleanup as view (#871)
    * not needed imports removed
    
    * cleanup view added
    
    * ADMIN_ACCESS_TOKEN added to the default config file
    
    * generateautocleanup management function added
    
    * docker settings updated to be able to generate a script that triggers autocleanup
    
    * bind updated
    
    * bind removed for now
    
    * bind removed for now
    
    * not needed imports removed
    
    * binding added
    
    * binding files replaces with binding directories
    
    * generateautocleanup updated to generate a new script also when there has been token update
    
    * some code refinement done; todo comment added
    
    * function name udpated
    
    * email about validation expiration updated;

--- api/views/autocleanup_view.py
+++ api/views/autocleanup_view.py
@@ -19,16 +19,17 @@ def auto_cleanup():
     cleaned_up = []
     notified = []
     for user in users:
-        ## no, you can't filter for is_expired because it isn't in the database. but you can exclude running validations
+        # no, you can't filter for is_expired because it isn't in the database. but you can exclude running validations
         good_validations = ValidationRun.objects.filter(end_time__isnull=False).filter(user=user)
         canceled_validations = ValidationRun.objects.filter(progress=-1).filter(user=user)
         validations = good_validations.union(canceled_validations)
         validations_near_expiring = []
         for validation in validations:
-            ## notify user about upcoming expiry if not already done
-            if ((validation.is_near_expiry) and (not validation.expiry_notified)):
-                ## if already a quarter of the warning period has passed without a notification being sent (could happen when service is down),
-                ## extend the validation so that the user gets the full warning period
+            # notify user about upcoming expiry if not already done
+            if validation.is_near_expiry and not validation.expiry_notified:
+                # if already a quarter of the warning period has passed without a notification
+                # being sent (could happen when service is down),
+                # extend the validation so that the user gets the full warning period
                 if timezone.now() + timedelta(
                         days=settings.VALIDATION_EXPIRY_WARNING_DAYS / 4) > validation.expiry_date:
                     validation.last_extended = timezone.now() - timedelta(
@@ -38,8 +39,8 @@ def auto_cleanup():
                 notified.append(str(validation.id))
                 continue
 
-            ## if validation is expired and user was notified, get rid of it
-            elif (validation.is_expired and validation.expiry_notified):
+            # if validation is expired and user was notified, get rid of it
+            elif validation.is_expired and validation.expiry_notified:
                 vid = str(validation.id)
                 celery_tasks = CeleryTask.objects.filter(validation_id=validation.id)
                 for task in celery_tasks:
@@ -72,3 +73,7 @@ def run_auto_cleanup_script(request):
         except:
             return JsonResponse({'message': 'something went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return JsonResponse({'message': 'success'}, status=status.HTTP_200_OK, safe=False)
+    else:
+        # todo: add sending email to admins
+        return JsonResponse({'message': 'Provided token does not belong to the admin user.'},
+                            status=status.HTTP_401_UNAUTHORIZED)
--- validator/management/commands/generateautocleanupscript.py
+++ validator/management/commands/generateautocleanupscript.py
@@ -17,19 +17,20 @@ class Command(BaseCommand):
         file_dir = options['path']
         cleanup_api_url = f'{settings.SITE_URL}{reverse("Run Auto Cleanup")}'
         try:
-            authentication_token = User.objects.get(username='admin').auth_token.key
+            authentication_token = User.objects.get(username='admin').auth_token
         except:
             authentication_token = User.objects.filter(is_superuser=True).filter(
-                auth_token__isnull=False).first().auth_token.key
+                auth_token__isnull=False).first().auth_token
         script_filename = 'run_autocleanup_script.sh'
         script_content = f"""#!/bin/bash 
-        curl -X POST {cleanup_api_url} -H "Authorization: Token {authentication_token}"
+        curl -X POST {cleanup_api_url} -H "Authorization: Token {authentication_token.key}"
         """
 
         os.makedirs(file_dir, exist_ok=True)
         full_script_path = os.path.join(file_dir, script_filename)
 
-        if not os.path.isfile(full_script_path):
+        if not os.path.isfile(full_script_path) or authentication_token.created.timestamp() > os.stat(
+                full_script_path).st_ctime:
             print(f"Creating {script_filename} with the necessary content.")
             with open(full_script_path, 'w') as script_file:
                 script_file.write(script_content)
