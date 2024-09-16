392cb29a67a7b8fb92b6e7a69d4ccfb6e66dc584
sheenaze <tercjak@awst.at>
2024-09-16 13:53:44 +0200

    inter-annual-metrics.component created

---
+++ UI/src/app/modules/metrics/components/inter-annual-metrics/inter-annual-metrics.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'qa-inter-annual-metrics',
+  standalone: true,
+  imports: [],
+  templateUrl: './inter-annual-metrics.component.html',
+  styleUrl: './inter-annual-metrics.component.scss'
+})
+export class InterAnnualMetricsComponent {
+
+}
