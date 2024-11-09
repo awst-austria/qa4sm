c4fb06d21a231bc688556631e479dcb997cbcb83
sheenaze <tercjak@awst.at>
2024-11-09 13:16:02 +0100

    not needed code removed; proper date passed to the backend

--- UI/src/app/modules/anomalies/components/anom-climatology/anom-climatology.component.ts
+++ UI/src/app/modules/anomalies/components/anom-climatology/anom-climatology.component.ts
@@ -10,46 +10,8 @@ export class AnomClimatologyComponent  {
   public minYear = 1971;
   public maxYear = 2100;
 
-  // @Input() anomaliesModel: AnomaliesModel;
   anomaliesModel = model({} as AnomaliesModel);
 
   constructor() {
-    // this.anomaliesModel.update(
-    //   model => {
-    //     model.anomaliesFrom.set(1978);
-    //     model.anomaliesTo.set((new Date()).getFullYear());
-    //     return model;
-    //   }
-    // )
   }
-
- //  setDate(year: number): Date {
- //    console.log(year);
- //    return new Date(`${year}-01-01`);
- // }
-
- //  getYearFrom(): number {
- //    let year: number;
- //    if (this.anomaliesModel.anomaliesFrom()){
- //      year = this.anomaliesModel.anomaliesFrom().getFullYear();
- //    } else {
- //      year = this.minYear;
- //      this.anomaliesModel.anomaliesFrom.set(this.setDate(year));
- //    }
- //
- //    return year;
- // }
-
-  // getYearTo(): number {
-  //   let year: Date;
-  //
-  //   if (this.anomaliesModel.anomaliesTo()){
-  //     year = this.anomaliesModel.anomaliesTo();
-  //   } else {
-  //     year = (new Date());
-  //     this.anomaliesModel.anomaliesTo.set(year);
-  //   }
-  //   return year.getFullYear();
-  // }
-
 }
