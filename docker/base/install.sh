c377d638cab496c4355b6e76ccc714f142656d39
sheenaze <tercjak@awst.at>
2024-11-28 11:01:21 +0100

    dir fixtures excluded from copying this directory from github

--- docker/base/install.sh
+++ docker/base/install.sh
@@ -7,5 +7,5 @@ mkdir -p "$APP_DIR"
 
 wget -P /tmp/ https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash /tmp/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda && ln -s /opt/miniconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh && rm /tmp/Miniconda3-latest-Linux-x86_64.sh
-rsync -r "$WEB_VAL_GIT_DIR/" "$APP_DIR/" --exclude="tests" --exclude="db.sqlite3"
+rsync -r "$WEB_VAL_GIT_DIR/" "$APP_DIR/" --exclude="tests" --exclude="db.sqlite3" --exclude="fixtures"
 /opt/miniconda/condabin/conda env create --prefix=/var/lib/qa4sm-web-val/virtenv -f "$WEB_VAL_GIT_DIR/environment/qa4sm_env.yml"
--- docker/celery-w/install.sh
+++ docker/celery-w/install.sh
@@ -4,4 +4,4 @@ set -x
 WEB_VAL_GIT_DIR="/tmp/qa4sm-git"
 APP_DIR="/var/lib/qa4sm-web-val/valentina"
 mkdir -p "$APP_DIR"
-rsync -r "$WEB_VAL_GIT_DIR/" "$APP_DIR/" --exclude="tests" --exclude="db.sqlite3"
+rsync -r "$WEB_VAL_GIT_DIR/" "$APP_DIR/" --exclude="tests" --exclude="db.sqlite3" --exclude="fixtures"
