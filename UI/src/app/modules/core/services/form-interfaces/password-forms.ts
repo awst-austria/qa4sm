d8765fae777e218cf6685ab444932b8638e9cf09
sheenaze <tercjak@awst.at>
2025-01-08 13:51:27 +0100

    old password optional

--- UI/src/app/modules/core/services/form-interfaces/password-forms.ts
+++ UI/src/app/modules/core/services/form-interfaces/password-forms.ts
@@ -4,6 +4,7 @@ import {AbstractControl} from '@angular/forms';
 export interface PasswordForm {
   password1: AbstractControl<string>;
   password2: AbstractControl<string>;
+  oldPassword?: AbstractControl<string>;
 }
 export interface PasswordResetForm{
   email: AbstractControl<string>;
