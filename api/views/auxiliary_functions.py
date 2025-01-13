9f7cdc26d52d460352a93e2ceb08d3388c9933f0
sheenaze <tercjak@awst.at>
2025-01-13 10:22:22 +0000

    Function for pushing changes to github added

--- api/views/auxiliary_functions.py
+++ api/views/auxiliary_functions.py
@@ -1,5 +1,7 @@
-from validator.models import Dataset
 from django.db.models.fields.related import ManyToManyRel, OneToOneField
+from django.conf import settings
+from git import Repo
+
 
 def get_fields_as_list(model):
     fields = []
@@ -23,3 +25,33 @@ def clean_redundant_datasets(user_datasets_without_file):
             variable.delete()
 
         dataset.delete()
+
+
+def push_changes_to_github(file_path, commit_message, branch_name='master'):
+    """
+    Pushes changes to our github repository
+
+    Args:
+        file_path (str): relative (to the project) path of a file to be commited,
+        commit_message (str): commit message,
+        branch_name (str): Branch to push to (default: 'master'),
+
+    Parameters
+    ----------
+    file_path
+    commit_message
+    branch_name
+    """
+    # our repository is initialized here
+    repo = Repo(settings.BASE_DIR)
+    origin = repo.remote('origin')
+
+    # check for changes
+    diffs = [item.a_path for item in repo.index.diff(None)]
+    print(file_path)
+    print(diffs)
+    if file_path in diffs:
+        repo.index.add([file_path])
+        repo.index.commit(commit_message)
+        origin.push(refspec=f'{branch_name}:{branch_name}')
+
