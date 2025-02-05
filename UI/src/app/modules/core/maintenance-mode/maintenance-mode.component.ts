edb6d1f61133c17be21d41879a7dd8dbdcec73ea
sheenaze <tercjak@awst.at>
2025-02-05 12:29:32 +0100

    maintenance mode moved to a separate component

---
+++ UI/src/app/modules/core/maintenance-mode/maintenance-mode.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'qa-maintenance-mode',
+  standalone: true,
+  imports: [],
+  templateUrl: './maintenance-mode.component.html',
+  styleUrl: './maintenance-mode.component.scss'
+})
+export class MaintenanceModeComponent {
+
+}
