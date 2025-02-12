bb6c6f94a0cf52c581f1b65a19cd6b3bfb83cb0a
sheenaze <tercjak@awst.at>
2025-02-12 14:25:25 +0100

    submodule initialization updated - for test instances it will point to the test-branch

--- docker/celery-w/entrypoint.sh
+++ docker/celery-w/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 set -x
 APP_DIR="/var/lib/qa4sm-web-val/valentina"
 export C_FORCE_ROOT="true"
+QA4SM_INSTANCE="$QA4SM_INSTANCE"
 
 . /opt/miniconda/etc/profile.d/conda.sh
 conda activate /var/lib/qa4sm-web-val/virtenv
@@ -14,6 +15,11 @@ fi
 cd $APP_DIR
 git submodule update --init --remote  validator/fixtures
 git config --global --add safe.directory "$APP_DIR/validator/fixtures"
+
+if [[ "$QA4SM_INSTANCE" == "TEST" ]]; then
+    git fetch origin
+    git switch test-branch || git checkout -b test-branch origin/test-branch
+fi
 
 cd $APP_DIR
 chown www-data:www-data -R "$APP_DIR/validator/fixtures"
