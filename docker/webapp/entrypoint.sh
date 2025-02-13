4fd44c8991e2bb821193e63cc5e5b4a5e87da4b7
Monika Tercjak <57092960+sheenaze@users.noreply.github.com>
2025-02-13 11:23:32 +0100

    docker setup updated to do the db update on the test instance (#1048)

--- docker/webapp/entrypoint.sh
+++ docker/webapp/entrypoint.sh
@@ -67,18 +67,18 @@ else
   NEW_DB="TRUE"
 fi
 
-## Check if DB_UPDATE is true and upload the dump if necessary
-#if [ "$DB_UPDATE" = "TRUE" ]; then
-#    echo "Updating DB from dump..."
-#    if [ -f "/var/lib/qa4sm-web-val/valentina/db_dump/$DB_DUMP_NAME" ]; then
-#        psql -h qa4sm-db -p 5432 -U postgres -d $QA4SM_DB_NAME -q -f "/var/lib/qa4sm-web-val/valentina/db_dump/$DB_DUMP_NAME"
-#        NEW_DB="FALSE"
-#        echo "DB dump uploaded successfully."
-#    else
-#        echo "DB dump file not found: /var/lib/qa4sm-web-val/valentina/db_dump/$DB_DUMP_NAME"
-#        exit 1
-#    fi
-#fi
+# Check if DB_UPDATE is true and upload the dump if necessary
+if [ "$DB_UPDATE" = "TRUE" ]; then
+    echo "Updating DB from dump..."
+    if [ -f "/var/lib/qa4sm-web-val/valentina/db_dump/$DB_DUMP_NAME" ]; then
+        psql -h qa4sm-db -p 5432 -U postgres -d $QA4SM_DB_NAME -q -f "/var/lib/qa4sm-web-val/valentina/db_dump/$DB_DUMP_NAME"
+        NEW_DB="FALSE"
+        echo "DB dump uploaded successfully."
+    else
+        echo "DB dump file not found: /var/lib/qa4sm-web-val/valentina/db_dump/$DB_DUMP_NAME"
+        exit 1
+    fi
+fi
 
 
 
