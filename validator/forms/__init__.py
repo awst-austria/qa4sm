a9e06fc792084d3e38112e19db67ebf6c262952c
sheenaze <tercjak@awst.at>
2025-04-22 09:18:35 +0200

    not needed files removed

--- validator/forms/__init__.py
+++ validator/forms/__init__.py
@@ -1,7 +1,3 @@
-from .custom_widgets import *
-from .data_configuration import *
-from .validation_run import *
 from .signup import *
 from .user_profile import *
-from .results_sorting import *
 from .publishing import *
