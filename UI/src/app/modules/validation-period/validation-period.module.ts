4a8fb3fc677c12175d0aa74f6662eb30e54285db
sheenaze <tercjak@awst.at>
2025-05-27 13:25:59 +0200

    declarations moved to imports

--- UI/src/app/modules/validation-period/validation-period.module.ts
+++ UI/src/app/modules/validation-period/validation-period.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ValidationPeriodComponent } from './components/validation-period/validation-period.component';
-import {PanelModule} from 'primeng/panel';
-import {TooltipModule} from 'primeng/tooltip';
-import {CalendarModule} from 'primeng/calendar';
-import {FormsModule} from '@angular/forms';
-
+import { PanelModule } from 'primeng/panel';
+import { TooltipModule } from 'primeng/tooltip';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [ValidationPeriodComponent],
+  declarations: [],
   exports: [
     ValidationPeriodComponent
   ],
@@ -18,7 +17,8 @@ import {FormsModule} from '@angular/forms';
     PanelModule,
     TooltipModule,
     CalendarModule,
-    FormsModule
+    FormsModule,
+    ValidationPeriodComponent
   ]
 })
 export class ValidationPeriodModule { }
