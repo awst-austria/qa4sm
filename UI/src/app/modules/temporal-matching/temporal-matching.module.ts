94d9b6fe6906afb39c6d20db15ec8a9aede6a07a
sheenaze <tercjak@awst.at>
2025-05-27 13:55:34 +0200

    needed imports added

--- UI/src/app/modules/temporal-matching/temporal-matching.module.ts
+++ UI/src/app/modules/temporal-matching/temporal-matching.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemporalMatchingComponent } from './components/temporal-matching/temporal-matching.component';
+import { InputNumber } from 'primeng/inputnumber';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [TemporalMatchingComponent],
   exports: [TemporalMatchingComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    InputNumber,
+    FormsModule
   ]
 })
 export class TemporalMatchingModule { }
