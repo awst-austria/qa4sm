e89041cfbe53ff577db9b9bd836bdccc14574aee
sheenaze <tercjak@awst.at>
2025-05-27 13:16:32 +0200

    declarations moved to imports

--- UI/src/app/modules/validation-reference/validation-reference.module.ts
+++ UI/src/app/modules/validation-reference/validation-reference.module.ts
@@ -1,19 +1,19 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ValidationReferenceComponent} from './components/validation-reference/validation-reference.component';
-import {DropdownModule} from 'primeng/dropdown';
-import {FormsModule} from '@angular/forms';
-import {TooltipModule} from 'primeng/tooltip';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ValidationReferenceComponent } from './components/validation-reference/validation-reference.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { TooltipModule } from 'primeng/tooltip';
 
 
 @NgModule({
-  declarations: [ValidationReferenceComponent],
   exports: [ValidationReferenceComponent],
-    imports: [
-        CommonModule,
-        DropdownModule,
-        FormsModule,
-        TooltipModule
-    ]
+  imports: [
+    CommonModule,
+    DropdownModule,
+    FormsModule,
+    TooltipModule,
+    ValidationReferenceComponent
+  ]
 })
 export class ValidationReferenceModule { }
